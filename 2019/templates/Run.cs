d0f040183567040a10981b5b31ed31990b76a4a1
Arnold Filip <arnold.filip@technia.com>
2019-12-02 16:53:59 +0100

    Add script for project creation

---
+++ 2019/templates/Run.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aoc._2019._xx
+{
+    public class Run : BaseRun, IRun
+    {
+        public Run(string InputPrefix = "") : base(InputPrefix) { }
+
+        public string Job1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Job2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 2019/templates/Run_Should.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Aoc._2019._xx.Tests
+{
+    public class Run_Should
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
