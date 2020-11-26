b8925639d522c75773ccf9b192120dff4f0829d7
Arnold Filip <arnold.filip@gmail.com>
2020-11-26 21:40:51 +0100

    Prepare test structure

---
+++ 2020/csharp/Aoc2020CalendarTests/Day00Tests.cs
@@ -0,0 +1,34 @@
+using Xunit;
+
+namespace Aoc2020Calendar.Tests
+{
+    public class Day00Tests
+    {
+        [Theory]
+        [InlineData(2, 12)]
+        [InlineData(2, 14)]
+        [InlineData(654, 1969)]
+        [InlineData(33583, 100756)]
+        public void RequiredFuel_Works(int fuel, int mass)
+        {
+            Assert.Equal(fuel, Day00.RequiredFuel(mass));
+        }
+
+        [Theory]
+        [InlineData(2, 14)]
+        [InlineData(966, 1969)]
+        [InlineData(50346, 100756)]
+        public void RequiredTotalFuel_Works(int fuel, int mass)
+        {
+            Assert.Equal(fuel, Day00.RequiredTotalFuel(mass));
+        }
+
+        [Fact]
+        public void Stars()
+        {
+            var run = new Day00();
+            Assert.Equal("3421505", run.Result1());
+            Assert.Equal("5129386", run.Result2());
+        }
+    }
+}
