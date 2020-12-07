d4c3292c17b198f237a418aab0a916360cadde01
Arnold Filip <arnold.filip@gmail.com>
2020-12-07 19:19:33 +0100

    [fsharp] Day 01

---
+++ 2020/fsharp/Aoc2020.Tests/Day01.Tests.fs
@@ -0,0 +1,58 @@
+module Day01.Tests
+
+open Xunit
+open Day01
+open System.Collections.Generic
+
+[<Fact>]
+let ``combine works`` () =
+    let expected = []
+    let result = List.empty |> combine
+    Assert.Equal<IEnumerable<int * int>>(expected, result)
+
+    let expected = []
+    let result = [ 1 ] |> combine
+    Assert.Equal<IEnumerable<int * int>>(expected, result)
+
+    let expected = [ (1, 2) ]
+    let result = [ 1; 2 ] |> combine
+    Assert.Equal<IEnumerable<int * int>>(expected, result)
+
+    let expected = [ (1, 2); (1, 3); (2, 3) ]
+    let result = [ 1; 2; 3 ] |> combine
+    Assert.Equal<IEnumerable<int * int>>(expected, result)
+    ()
+
+[<Fact>]
+let ``combine2 works`` () =
+    let expected = []
+    let result = List.empty |> combine2
+    Assert.Equal<IEnumerable<int * int * int>>(expected, result)
+
+    let expected = []
+    let result = [ 1 ] |> combine2
+    Assert.Equal<IEnumerable<int * int * int>>(expected, result)
+
+    let expected = []
+    let result = [ 1; 2 ] |> combine2
+    Assert.Equal<IEnumerable<int * int * int>>(expected, result)
+
+    let expected = [ (1, 2, 3) ]
+    let result = [ 1; 2; 3 ] |> combine2
+    Assert.Equal<IEnumerable<int * int * int>>(expected, result)
+
+    let expected =
+        [ (1, 2, 3)
+          (1, 2, 4)
+          (1, 3, 4)
+          (2, 3, 4) ]
+
+    let result = [ 1; 2; 3; 4 ] |> combine2
+    Assert.Equal<IEnumerable<int * int * int>>(expected, result)
+    ()
+
+[<Fact>]
+let ``Day01 Stars`` () =
+    Assert.Equal("1018944", job1 ())
+    Assert.Equal("8446464", job2 ())
+    ()
---
+++ 2020/fsharp/Aoc2020/Day01.fs
@@ -0,0 +1,51 @@
+module Day01
+
+open Utils
+
+let pairWithItem items first =
+    seq {
+        if items |> (not << Seq.isEmpty) then
+            for second in items do
+                yield first, second
+    }
+
+let pairWithTuple tuples first =
+    seq {
+        if tuples |> (not << Seq.isEmpty) then
+            for second, third in tuples do
+                yield first, second, third
+    }
+
+let combine items' =
+    let items = items' |> Seq.toArray
+    items
+    |> Seq.indexed
+    |> Seq.collect (fun (index, first) ->
+        first
+        |> pairWithItem (items |> Seq.skip (index + 1)))
+
+let combine2 items' =
+    let items = items' |> Seq.toArray
+    items
+    |> Seq.indexed
+    |> Seq.collect (fun (index, first) ->
+        first
+        |> pairWithTuple (items |> Seq.skip (index + 1) |> combine))
+
+let job1 () =
+    readInputLines "01"
+    |> Seq.map int
+    |> combine
+    |> Seq.filter (fun (a, b) -> a + b = 2020)
+    |> Seq.head
+    |> (fun (a, b) -> a * b)
+    |> string
+
+let job2 () =
+    readInputLines "01"
+    |> Seq.map int
+    |> combine2
+    |> Seq.filter (fun (a, b, c) -> a + b + c = 2020)
+    |> Seq.head
+    |> (fun (a, b, c) -> a * b * c)
+    |> string
