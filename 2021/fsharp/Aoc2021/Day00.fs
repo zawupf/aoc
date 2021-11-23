81204313847ba184e1f735cf7ea4026ac3d9bad5
Arnold Filip <arnold.filip@gmail.com>
2021-11-23 21:53:09 +0100

    [F#] Prepare 2021 workspace

---
+++ 2021/fsharp/Aoc2021/Day00.fs
@@ -0,0 +1,29 @@
+module Day00
+
+open Utils
+
+let fuel mass =
+    let fuel = mass / 3 - 2
+    if fuel > 0 then fuel else 0
+
+let totalFuel mass =
+    let rec _totalFuel mass previousFuel =
+        let currentFuel = fuel mass
+        let currentTotalFuel = previousFuel + currentFuel
+
+        if currentFuel > 0 then
+            _totalFuel currentFuel currentTotalFuel
+        else
+            currentTotalFuel
+
+    _totalFuel mass 0
+
+let job1 () =
+    readInputLines "00"
+    |> Seq.sumBy (int >> fuel)
+    |> string
+
+let job2 () =
+    readInputLines "00"
+    |> Seq.sumBy (int >> totalFuel)
+    |> string
