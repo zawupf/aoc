29a1b726c2d5b98b140ec47c367460b4c9499c7d
Arnold Filip <arnold.filip@gmail.com>
2022-10-24 21:37:35 +0200

    F# 2015-01

--- 2015/fsharp/Aoc2015.Tests/Day01.Tests.fs
+++ 2015/fsharp/Aoc2015.Tests/Day01.Tests.fs
@@ -4,10 +4,29 @@ open Xunit
 open Utils
 open Day01
 
+[<Theory>]
+[<InlineData(0, "(())")>]
+[<InlineData(0, "()()")>]
+[<InlineData(3, "(((")>]
+[<InlineData(3, "(()(()(")>]
+[<InlineData(3, "))(((((")>]
+[<InlineData(-1, "())")>]
+[<InlineData(-1, "))(")>]
+[<InlineData(-3, ")))")>]
+[<InlineData(-3, ")())())")>]
+let ``Day01 Part 1`` (expected, input) = Assert.Equal(expected, floor input)
+
+
+[<Theory>]
+[<InlineData(1, ")")>]
+[<InlineData(5, "()())")>]
+let ``Day01 Part 2`` (expected, input) =
+    Assert.Equal(expected, enterBasementPosition input)
+
 [<Fact>]
 let ``Day01 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("74", job1 ())
+        Assert.Equal("1795", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2015/fsharp/Aoc2015/Day01.fs
+++ 2015/fsharp/Aoc2015/Day01.fs
@@ -2,12 +2,24 @@ module Day01
 
 open Utils
 
-let input = readInputLines "01"
+let private floorFolder floor =
+    function
+    | '(' -> floor + 1
+    | ')' -> floor - 1
+    | _ -> floor
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let floor (input: string) =
+    input.ToCharArray() |> Array.fold floorFolder 0
+
+let enterBasementPosition (input: string) =
+    input.ToCharArray()
+    |> Seq.ofArray
+    |> Seq.scan floorFolder 0
+    |> Seq.findIndex (fun floor -> floor = -1)
+
+let input = readInputText "01"
+
+let job1 () = input |> floor |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> enterBasementPosition |> string
