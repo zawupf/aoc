e78237454c35ef2a853ab610b42944a2ab0e5c57
Arnold Filip <arnold.filip@gmail.com>
2022-10-25 19:53:29 +0200

    F# 2015-02

--- 2015/fsharp/Aoc2015.Tests/Day02.Tests.fs
+++ 2015/fsharp/Aoc2015.Tests/Day02.Tests.fs
@@ -4,10 +4,22 @@ open Xunit
 open Utils
 open Day02
 
+[<Theory>]
+[<InlineData(58, "2x3x4")>]
+[<InlineData(43, "1x1x10")>]
+let ``Day02 required paper works`` expected input =
+    Assert.Equal(expected, input |> Box.parse |> requiredPaper)
+
+[<Theory>]
+[<InlineData(34, "2x3x4")>]
+[<InlineData(14, "1x1x10")>]
+let ``Day02 required ribbon works`` expected input =
+    Assert.Equal(expected, input |> Box.parse |> requiredRibbon)
+
 [<Fact>]
 let ``Day02 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("1586300", job1 ())
+        Assert.Equal("3737498", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2015/fsharp/Aoc2015/Day02.fs
+++ 2015/fsharp/Aoc2015/Day02.fs
@@ -2,12 +2,33 @@ module Day02
 
 open Utils
 
+type Box = int * int * int
+
+module Box =
+    let parse input =
+        match input |> String.parseInts 'x' with
+        | [ w; h; l ] -> (w, h, l)
+        | _ -> failwithf "Invalid box '%s'" input
+
+    let smallestSides (w, h, l) =
+        [| w; h; l |] |> Seq.sort |> Seq.pairwise |> Seq.head
+
+    let surface (w, h, l) = 2 * (w * h + w * l + h * l)
+
+    let volume (w, h, l) = w * h * l
+
+let requiredPaper box =
+    let a, b = Box.smallestSides box
+    Box.surface box + a * b
+
+let requiredRibbon box =
+    let a, b = Box.smallestSides box
+    Box.volume box + 2 * (a + b)
+
 let input = readInputLines "02"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> Seq.sumBy (Box.parse >> requiredPaper) |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> Seq.sumBy (Box.parse >> requiredRibbon) |> string
