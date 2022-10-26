27e58589a6b66ff76d4989c07ebb100066643442
Arnold Filip <arnold.filip@gmail.com>
2022-10-26 20:11:46 +0200

    F# 2015-04

--- 2015/fsharp/Aoc2015.Tests/Day04.Tests.fs
+++ 2015/fsharp/Aoc2015.Tests/Day04.Tests.fs
@@ -4,10 +4,16 @@ open Xunit
 open Utils
 open Day04
 
+[<Theory>]
+[<InlineData(609043, "abcdef")>]
+[<InlineData(1048970, "pqrstuv")>]
+let ``Day04 Part 1`` expected secret =
+    Assert.Equal(expected, secret |> findLowestNumberWithPrefix "00000")
+
 [<Fact>]
 let ``Day04 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("254575", job1 ())
+        Assert.Equal("1038736", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2015/fsharp/Aoc2015/Day04.fs
+++ 2015/fsharp/Aoc2015/Day04.fs
@@ -2,12 +2,25 @@ module Day04
 
 open Utils
 
-let input = readInputLines "04"
+let md5 (input: string) =
+    use md5 = System.Security.Cryptography.MD5.Create()
+
+    input
+    |> System.Text.Encoding.ASCII.GetBytes
+    |> md5.ComputeHash
+    |> System.Convert.ToHexString
+
+let findLowestNumberWithPrefix (prefix: string) secret =
+    let buildMd5 i = i + 1, secret + string (i + 1) |> md5
+
+    let hexWithPrefix (_, md5: string) = md5.StartsWith(prefix)
+
+    buildMd5 |> Seq.initInfinite |> Seq.find hexWithPrefix |> fst
+
+let input = readInputText "04"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> findLowestNumberWithPrefix "00000" |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> findLowestNumberWithPrefix "000000" |> string
