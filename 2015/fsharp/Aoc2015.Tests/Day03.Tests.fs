4f47a752c43f9d6f277735a2eebbd96f5a6af532
Arnold Filip <arnold.filip@gmail.com>
2022-10-26 16:06:23 +0200

    F# 2015-03

--- 2015/fsharp/Aoc2015.Tests/Day03.Tests.fs
+++ 2015/fsharp/Aoc2015.Tests/Day03.Tests.fs
@@ -4,10 +4,24 @@ open Xunit
 open Utils
 open Day03
 
+[<Theory>]
+[<InlineData(2, ">")>]
+[<InlineData(4, "^>v<")>]
+[<InlineData(2, "^v^v^v^v^v")>]
+let ``Day03 santa house count works`` expected input =
+    Assert.Equal(expected, visitedHousesCount input)
+
+[<Theory>]
+[<InlineData(3, "^v")>]
+[<InlineData(3, "^>v<")>]
+[<InlineData(11, "^v^v^v^v^v")>]
+let ``Day03 robo-santa house count works`` expected input =
+    Assert.Equal(expected, visitedHousesWithRoboCount input)
+
 [<Fact>]
 let ``Day03 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("2565", job1 ())
+        Assert.Equal("2639", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2015/fsharp/Aoc2015/Day03.fs
+++ 2015/fsharp/Aoc2015/Day03.fs
@@ -2,12 +2,42 @@ module Day03
 
 open Utils
 
-let input = readInputLines "03"
+type House = int * int
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let nextHouse direction (x, y) =
+    match direction with
+    | '^' -> x, y + 1
+    | 'v' -> x, y - 1
+    | '>' -> x + 1, y
+    | '<' -> x - 1, y
+    | _ -> failwithf "Invalid direction %c" direction
+
+let walk houses direction =
+    let nextHouse = houses |> List.head |> nextHouse direction
+    nextHouse :: houses
+
+let visitHouses directions = directions |> Array.fold walk [ 0, 0 ]
+
+let visitedHousesCount input =
+    input |> String.toCharArray |> visitHouses |> List.distinct |> List.length
+
+let visitedHousesWithRoboCount input =
+    let santaDirections, roboDirections =
+        input
+        |> String.toCharArray
+        |> Array.chunkBySize 2
+        |> Array.map (fun a -> a.[0], a.[1])
+        |> Array.unzip
+
+    santaDirections
+    |> visitHouses
+    |> List.append (roboDirections |> visitHouses)
+    |> List.distinct
+    |> List.length
+
+let input = readInputText "03"
+
+let job1 () = input |> visitedHousesCount |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> visitedHousesWithRoboCount |> string
