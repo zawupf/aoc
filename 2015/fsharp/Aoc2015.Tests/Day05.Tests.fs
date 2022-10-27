d6579ac7c153174a68b3cb944b23dcbbf5a3c3d6
Arnold Filip <arnold.filip@gmail.com>
2022-10-27 11:14:47 +0200

    F# 2015-05

--- 2015/fsharp/Aoc2015.Tests/Day05.Tests.fs
+++ 2015/fsharp/Aoc2015.Tests/Day05.Tests.fs
@@ -4,10 +4,27 @@ open Xunit
 open Utils
 open Day05
 
+[<Theory>]
+[<InlineData(true, "ugknbfddgicrmopn")>]
+[<InlineData(true, "aaa")>]
+[<InlineData(false, "jchzalrnumimnmhp")>]
+[<InlineData(false, "haegwjzuvuyypxyu")>]
+[<InlineData(false, "dvszwmarrgswjxmb")>]
+let ``Day05 is nice works`` expected input =
+    Assert.Equal(expected, input |> isNice)
+
+[<Theory>]
+[<InlineData(true, "qjhvhtzxzqqjkmpb")>]
+[<InlineData(true, "xxyxx")>]
+[<InlineData(false, "uurcxstgmygtbstg")>]
+[<InlineData(false, "ieodomkazucvgmuy")>]
+let ``Day05 is really nice works`` expected input =
+    Assert.Equal(expected, input |> isReallyNice)
+
 [<Fact>]
 let ``Day05 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("238", job1 ())
+        Assert.Equal("69", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2015/fsharp/Aoc2015/Day05.fs
+++ 2015/fsharp/Aoc2015/Day05.fs
@@ -2,12 +2,70 @@ module Day05
 
 open Utils
 
+
+[<Literal>]
+let vowels = "aeiou"
+
+let isNice input =
+
+    let isVowel (c: char) = vowels.Contains(c)
+
+    let hasAtLeast3Vowels input =
+        input
+        |> String.toCharArray
+        |> Seq.filter isVowel
+        |> Seq.tryItem 2
+        |> Option.isSome
+
+    let hasAtLeast1Pair input =
+        input
+        |> String.toCharArray
+        |> Seq.pairwise
+        |> Seq.exists (fun (a, b) -> a = b)
+
+    let forbiddenStrings = [ "ab"; "cd"; "pq"; "xy" ]
+
+    let hasForbiddenString (input: string) =
+        forbiddenStrings
+        |> List.exists (fun substring -> input.Contains(substring))
+
+    input |> hasAtLeast3Vowels
+    && input |> hasAtLeast1Pair
+    && input |> hasForbiddenString |> not
+
+let isReallyNice input =
+    let rec hasAtLeast1SpecialPair input =
+        let isHeadRepeated input =
+            if input |> String.length >= 4 then
+                let head = input.Substring(0, 2)
+                let tail = input.Substring(2, input.Length - 2)
+                tail.Contains(head)
+            else
+                false
+
+        if isHeadRepeated input then
+            true
+        else if input.Length > 4 then
+            input |> String.substring 1 |> hasAtLeast1SpecialPair
+        else
+            false
+
+    let hasAtLeast1SpecialLetter input =
+        input
+        |> String.toCharArray
+        |> Seq.windowed 3
+        |> Seq.exists (function
+            | [| a; _; b |] when a = b -> true
+            | _ -> false)
+
+    input |> hasAtLeast1SpecialPair && input |> hasAtLeast1SpecialLetter
+
 let input = readInputLines "05"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> Seq.sumBy (fun line -> if line |> isNice then 1 else 0) |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input
+    |> Seq.sumBy (fun line -> if line |> isReallyNice then 1 else 0)
+    |> string
