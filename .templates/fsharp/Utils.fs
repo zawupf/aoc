08d600aacfff73ebd944fb7a7858f15e4797a7a9
Arnold Filip <arnold.filip@gmail.com>
2022-10-28 20:03:49 +0200

    Add Regex active pattern

--- .templates/fsharp/Utils.fs
+++ .templates/fsharp/Utils.fs
@@ -1,8 +1,17 @@
 module Utils
 
+open System.Text.RegularExpressions
+
+let (|Regex|_|) pattern input =
+    let m = Regex.Match(input, pattern)
+
+    if m.Success then
+        Some(List.tail [ for g in m.Groups -> g.Value ])
+    else
+        None
+
 module String =
-    let join separator (chunks: Collections.seq<_>) =
-        System.String.Join(separator, chunks)
+    let join separator (chunks: Collections.seq<_>) = System.String.Join(separator, chunks)
 
     let trim (string: string) = string.Trim()
 
