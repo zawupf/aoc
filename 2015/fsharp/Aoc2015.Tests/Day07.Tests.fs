edce21afca113a8817fcb2fb43535297c504b1ef
Arnold Filip <arnold.filip@gmail.com>
2022-10-29 20:46:53 +0200

    F# 2015-07

--- 2015/fsharp/Aoc2015.Tests/Day07.Tests.fs
+++ 2015/fsharp/Aoc2015.Tests/Day07.Tests.fs
@@ -5,9 +5,32 @@ open Utils
 open Day07
 
 [<Fact>]
+let ``Day07 part 1 works`` () =
+    let circuit =
+        Circuit.empty
+        |> Circuit.add "123 -> x"
+        |> Circuit.add "456 -> y"
+        |> Circuit.add "x AND y -> d"
+        |> Circuit.add "x OR y -> e"
+        |> Circuit.add "x LSHIFT 2 -> f"
+        |> Circuit.add "y RSHIFT 2 -> g"
+        |> Circuit.add "NOT x -> h"
+        |> Circuit.add "NOT y -> i"
+        |> Circuit.run
+
+    Assert.Equal(72us, circuit |> Circuit.get "d")
+    Assert.Equal(507us, circuit |> Circuit.get "e")
+    Assert.Equal(492us, circuit |> Circuit.get "f")
+    Assert.Equal(114us, circuit |> Circuit.get "g")
+    Assert.Equal(65412us, circuit |> Circuit.get "h")
+    Assert.Equal(65079us, circuit |> Circuit.get "i")
+    Assert.Equal(123us, circuit |> Circuit.get "x")
+    Assert.Equal(456us, circuit |> Circuit.get "y")
+
+[<Fact>]
 let ``Day07 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("3176", job1 ())
+        Assert.Equal("14710", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2015/fsharp/Aoc2015/Day07.fs
+++ 2015/fsharp/Aoc2015/Day07.fs
@@ -2,12 +2,111 @@ module Day07
 
 open Utils
 
+type Signal = uint16
+type WireId = string
+
+type Input =
+    | InSignal of Signal
+    | InWireId of WireId
+
+type Gate =
+    | SIGNAL of Input
+    | AND of Input * Input
+    | OR of Input * Input
+    | NOT of Input
+    | LSHIFT of Input * int
+    | RSHIFT of Input * int
+
+type Circuit = Map<WireId, Gate>
+
+module Circuit =
+    let empty = Map.empty<WireId, Gate>
+
+    let add wire circuit =
+        let toInput s =
+            match s with
+            | Regex @"^\d+$" [] -> s |> uint16 |> InSignal
+            | _ -> s |> InWireId
+
+        let wireId, input =
+            match wire with
+            | Regex @"^(\d+|\w+) -> (\w+)$" [ signal; id ] -> id, signal |> toInput |> SIGNAL
+            | Regex @"^(\d+|\w+) AND (\d+|\w+) -> (\w+)$" [ in1; in2; id ] ->
+                id, (in1 |> toInput, in2 |> toInput) |> AND
+            | Regex @"^(\d+|\w+) OR (\d+|\w+) -> (\w+)$" [ in1; in2; id ] -> id, (in1 |> toInput, in2 |> toInput) |> OR
+            | Regex @"NOT (\d+|\w+) -> (\w+)$" [ input; id ] -> id, input |> toInput |> NOT
+            | Regex @"^(\d+|\w+) LSHIFT (\d+) -> (\w+)$" [ input; shift; id ] ->
+                id, (input |> toInput, shift |> int) |> LSHIFT
+            | Regex @"^(\d+|\w+) RSHIFT (\d+) -> (\w+)$" [ input; shift; id ] ->
+                id, (input |> toInput, shift |> int) |> RSHIFT
+            | _ -> failwithf "Invalid wire: %s" wire
+
+        circuit |> Map.add wireId input
+
+    let rec resolve id circuit =
+        let resolveSignal input circuit =
+            match input with
+            | InSignal signal -> signal, circuit
+            | InWireId wireId ->
+                let signal, circuit = resolve wireId circuit
+
+                match signal with
+                | InSignal signal -> signal, circuit
+                | InWireId wireId -> failwithf "Could not fully resolve signal of wire '%s'" wireId
+
+        match circuit |> Map.tryFind id with
+        | None -> failwithf "Unknown wire: '%s'" id
+        | Some gate ->
+            match gate with
+            | SIGNAL signal ->
+                match signal with
+                | InSignal _ -> signal, circuit
+                | InWireId id1 ->
+                    let signal, circuit = resolve id1 circuit
+                    signal, circuit |> Map.add id (signal |> SIGNAL)
+            | AND (in1, in2) ->
+                let signal1, circuit = resolveSignal in1 circuit
+                let signal2, circuit = resolveSignal in2 circuit
+                let signal = signal1 &&& signal2
+                signal |> InSignal, circuit |> Map.add id (signal |> InSignal |> SIGNAL)
+            | OR (in1, in2) ->
+                let signal1, circuit = resolveSignal in1 circuit
+                let signal2, circuit = resolveSignal in2 circuit
+                let signal = signal1 ||| signal2
+                signal |> InSignal, circuit |> Map.add id (signal |> InSignal |> SIGNAL)
+            | NOT in1 ->
+                let signal, circuit = resolveSignal in1 circuit
+                let signal = ~~~signal
+                signal |> InSignal, circuit |> Map.add id (signal |> InSignal |> SIGNAL)
+            | LSHIFT (in1, n) ->
+                let signal, circuit = resolveSignal in1 circuit
+                let signal = signal <<< n
+                signal |> InSignal, circuit |> Map.add id (signal |> InSignal |> SIGNAL)
+            | RSHIFT (in1, n) ->
+                let signal, circuit = resolveSignal in1 circuit
+                let signal = signal >>> n
+                signal |> InSignal, circuit |> Map.add id (signal |> InSignal |> SIGNAL)
+
+    let run circuit =
+        circuit
+        |> Map.keys
+        |> Seq.fold (fun circuit id -> resolve id circuit |> snd) circuit
+
+    let get id circuit =
+        match resolve id circuit |> fst with
+        | InSignal signal -> signal
+        | InWireId wireId -> failwithf "Could not fully resolve signal of wire '%s'" wireId
+
 let input = readInputLines "07"
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let circuit =
+    input
+    |> Seq.fold (fun circuit line -> circuit |> Circuit.add line) Circuit.empty
+
+
+let job1 () = circuit |> Circuit.get "a" |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    let a = circuit |> Circuit.get "a"
+
+    circuit |> Circuit.add (sprintf "%d -> b" a) |> Circuit.get "a" |> string
