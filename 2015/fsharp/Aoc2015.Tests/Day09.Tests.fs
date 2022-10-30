1ad078cbcb0608ef6fdcc6ec113f7da48f66a34b
Arnold Filip <arnold.filip@gmail.com>
2022-10-30 15:34:41 +0100

    F# 2015-09

--- 2015/fsharp/Aoc2015.Tests/Day09.Tests.fs
+++ 2015/fsharp/Aoc2015.Tests/Day09.Tests.fs
@@ -5,9 +5,25 @@ open Utils
 open Day09
 
 [<Fact>]
+let ``Day09 routes work`` () =
+    let roads =
+        [ "London to Dublin = 464"
+          "London to Belfast = 518"
+          "Dublin to Belfast = 141" ]
+        |> parseRoads
+
+    Assert.Equal(6, roads |> List.length)
+    Assert.Equal(3, roads |> cities |> List.length)
+
+    let routes = roads |> routes
+    Assert.Equal(6, routes |> Seq.length)
+    Assert.Equal(605, routes |> Seq.minBy snd |> snd)
+    Assert.Equal(982, routes |> Seq.maxBy snd |> snd)
+
+[<Fact>]
 let ``Day09 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("141", job1 ())
+        Assert.Equal("736", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2015/fsharp/Aoc2015/Day09.fs
+++ 2015/fsharp/Aoc2015/Day09.fs
@@ -2,12 +2,53 @@ module Day09
 
 open Utils
 
+let parseRoads input =
+    let insert a b d list = (a, b, d) :: (b, a, d) :: list
+
+    input
+    |> Seq.fold
+        (fun roads line ->
+            match line with
+            | Regex @"^(\w+) to (\w+) = (\d+)$" [ city1; city2; distance ] ->
+                roads |> insert city1 city2 (distance |> int)
+            | _ -> failwithf "Invalid input: %s" line)
+        []
+
+let cities roads =
+    roads
+    |> List.fold (fun cities (city, _, _) -> cities |> Set.add city) Set.empty
+    |> Set.toList
+
+let rec findRoutes (route, distance) roads =
+    let current = route |> List.head
+    let nextRoads = roads |> List.filter (fun (a, _, _) -> a = current)
+
+    seq {
+        match nextRoads with
+        | [] -> yield route, distance
+        | _ ->
+            for (_, next, d) in nextRoads do
+                yield!
+                    findRoutes
+                        (next :: route, distance + d)
+                        (roads |> List.filter (fun (a, b, _) -> a <> current && b <> current))
+    }
+
+
+let routes roads =
+    let cities = roads |> cities
+
+    seq {
+        for start in cities do
+            yield!
+                findRoutes ([ start ], 0) roads
+                |> Seq.filter (fun (route, _) -> route |> Seq.length = cities.Length)
+    }
+
 let input = readInputLines "09"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> parseRoads |> routes |> Seq.minBy snd |> snd |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> parseRoads |> routes |> Seq.maxBy snd |> snd |> string
