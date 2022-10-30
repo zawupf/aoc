fcb6cfcadc35ef1cac73eeab62b1cc91a497cdb4
Arnold Filip <arnold.filip@gmail.com>
2022-10-30 11:21:57 +0100

    F# 2015-08

--- 2015/fsharp/Aoc2015.Tests/Day08.Tests.fs
+++ 2015/fsharp/Aoc2015.Tests/Day08.Tests.fs
@@ -4,10 +4,26 @@ open Xunit
 open Utils
 open Day08
 
+[<Theory>]
+[<InlineData(0, "\"\"")>]
+[<InlineData(3, "\"abc\"")>]
+[<InlineData(7, "\"aaa\\\"aaa\"")>]
+[<InlineData(1, "\"\\x27\"")>]
+let ``Day08 decode literal works`` expected literal =
+    Assert.Equal(expected, literal |> decodeCharCount)
+
+[<Theory>]
+[<InlineData(6, "\"\"")>]
+[<InlineData(9, "\"abc\"")>]
+[<InlineData(16, "\"aaa\\\"aaa\"")>]
+[<InlineData(11, "\"\\x27\"")>]
+let ``Day08 encode string works`` expected literal =
+    Assert.Equal(expected, literal |> encodeCharCount)
+
 [<Fact>]
 let ``Day08 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("1350", job1 ())
+        Assert.Equal("2085", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2015/fsharp/Aoc2015/Day08.fs
+++ 2015/fsharp/Aoc2015/Day08.fs
@@ -2,12 +2,46 @@ module Day08
 
 open Utils
 
+let decodeCharCount (literal: string) =
+    if literal.StartsWith('"') |> not || literal.EndsWith('"') |> not then
+        failwithf "Invalid literal: %s" literal
+    else
+        let mutable n = 0
+        let iend = literal.Length - 1
+        let mutable i = 1
+
+        while i < iend do
+            n <- n + 1
+
+            i <-
+                i
+                + match literal.[i] with
+                  | '\\' ->
+                      if i + 1 = iend then
+                          failwith "Invalid escape sequence at end"
+                      else
+                          match literal.[i + 1] with
+                          | '\\'
+                          | '"' -> 2
+                          | 'x' -> 4
+                          | c -> failwithf "Invalid escape sequence \\%c" c
+                  | _ -> 1
+
+        n
+
+let decodeOverhead literal =
+    (literal |> String.length) - (literal |> decodeCharCount)
+
+let encodeCharCount (string: string) =
+    string |> Seq.fold (fun len c -> len + if c = '"' || c = '\\' then 2 else 1) 2
+
+let encodeOverhead literal =
+    (literal |> encodeCharCount) - (literal |> String.length)
+
 let input = readInputLines "08"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> Seq.sumBy decodeOverhead |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> Seq.sumBy encodeOverhead |> string
