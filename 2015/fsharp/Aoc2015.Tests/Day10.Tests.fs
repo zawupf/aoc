c404f62e25256f471307e6437a9c71836011fa02
Arnold Filip <arnold.filip@gmail.com>
2022-10-30 19:56:19 +0100

    F# 2015-10

--- 2015/fsharp/Aoc2015.Tests/Day10.Tests.fs
+++ 2015/fsharp/Aoc2015.Tests/Day10.Tests.fs
@@ -4,10 +4,22 @@ open Xunit
 open Utils
 open Day10
 
+[<Theory>]
+[<InlineData("1", "11")>]
+[<InlineData("11", "21")>]
+[<InlineData("21", "1211")>]
+[<InlineData("1211", "111221")>]
+[<InlineData("111221", "312211")>]
+let ``Day10 look and say`` look say = Assert.Equal(say, look |> lookAndSay)
+
+[<Fact>]
+let ``Day10 play works`` () =
+    Assert.Equal("312211", "1" |> play |> Seq.item 5)
+
 [<Fact>]
 let ``Day10 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("360154", job1 ())
+        Assert.Equal("5103798", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2015/fsharp/Aoc2015/Day10.fs
+++ 2015/fsharp/Aoc2015/Day10.fs
@@ -1,13 +1,32 @@
 module Day10
 
+open System.Text
+
 open Utils
 
-let input = readInputLines "10"
+let lookAndSay (s: string) =
+    let look groups c =
+        match groups with
+        | (n, prevChar) :: groups' when prevChar = c -> (n + 1, c) :: groups'
+        | _ -> (1, c) :: groups
+
+    let say (sb: StringBuilder) (n: int, c: char) = sb.Append(n).Append(c)
+
+    s |> Seq.fold look [] |> List.rev |> List.fold say (StringBuilder()) |> string
+
+let play init =
+    seq {
+        let mutable current = init
+
+        while true do
+            yield current
+            current <- current |> lookAndSay
+    }
+
+let input = readInputText "10"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> play |> Seq.item 40 |> String.length |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> play |> Seq.item 50 |> String.length |> string
