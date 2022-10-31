5f76f564668d356c4cd2ff65a9326542b22951ca
Arnold Filip <arnold.filip@gmail.com>
2022-10-31 20:57:15 +0100

    F# 2015-12

--- 2015/fsharp/Aoc2015.Tests/Day12.Tests.fs
+++ 2015/fsharp/Aoc2015.Tests/Day12.Tests.fs
@@ -4,10 +4,30 @@ open Xunit
 open Utils
 open Day12
 
+[<Theory>]
+[<InlineData(6, "[1,2,3]")>]
+[<InlineData(6, "{\"a\":2,\"b\":4}")>]
+[<InlineData(3, "[[[3]]]")>]
+[<InlineData(3, "{\"a\":{\"b\":4},\"c\":-1}")>]
+[<InlineData(0, "{\"a\":[-1,1]}")>]
+[<InlineData(0, "[-1,{\"a\":1}]")>]
+[<InlineData(0, "[]")>]
+[<InlineData(0, "{}")>]
+let ``Day12 sum of numbers works`` sum input =
+    Assert.Equal(sum, input |> findNumbers |> Seq.sum)
+
+[<Theory>]
+[<InlineData(6, "[1,2,3]")>]
+[<InlineData(4, "[1,{\"c\":\"red\",\"b\":2},3]")>]
+[<InlineData(0, "{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}")>]
+[<InlineData(6, "[1,\"red\",5]")>]
+let ``Day12 sum of numbers w/o red works`` sum input =
+    Assert.Equal(sum, input |> removeRed |> findNumbers |> Seq.sum)
+
 [<Fact>]
 let ``Day12 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("156366", job1 ())
+        Assert.Equal("96852", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2015/fsharp/Aoc2015/Day12.fs
+++ 2015/fsharp/Aoc2015/Day12.fs
@@ -2,12 +2,45 @@ module Day12
 
 open Utils
 
-let input = readInputLines "12"
+let findNumbers input =
+    // printfn "%s" input
+    let rx = System.Text.RegularExpressions.Regex(@"(\+|-)?(\d+)")
+    let matches = rx.Matches(input)
+    matches |> Seq.map (fun m -> m.Value |> int)
+
+let removeRed (input: string) =
+    let rec findLeftBracket (input: string) index nObject =
+        match input.[index] with
+        | '}' -> findLeftBracket input (index - 1) (nObject + 1)
+        | '{' ->
+            match nObject - 1 with
+            | 0 -> index
+            | nObject -> findLeftBracket input (index - 1) nObject
+        | _ -> findLeftBracket input (index - 1) nObject
+
+    let rec findRightBracket (input: string) index nObject =
+        match input.[index] with
+        | '{' -> findRightBracket input (index + 1) (nObject + 1)
+        | '}' ->
+            match nObject - 1 with
+            | 0 -> index
+            | nObject -> findRightBracket input (index + 1) nObject
+        | _ -> findRightBracket input (index + 1) nObject
+
+    let rec loop (input: string) (start: int) =
+        match input.IndexOf(":\"red\"", start) with
+        | -1 -> input
+        | index ->
+            let i = findLeftBracket input index 1
+            let j = findRightBracket input index 1
+            loop (input.Substring(0, i + 1) + input.Substring(j)) (i + 2)
+
+    loop input 0
+
+let input = readInputText "12"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> findNumbers |> Seq.sum |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> removeRed |> findNumbers |> Seq.sum |> string
