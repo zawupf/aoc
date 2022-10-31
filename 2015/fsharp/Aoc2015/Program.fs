fe1b4efa4578590940e9e911ade727ee9b85ad94
Arnold Filip <arnold.filip@gmail.com>
2022-10-31 20:56:05 +0100

    Handle argv to run only given days

--- 2015/fsharp/Aoc2015/Program.fs
+++ 2015/fsharp/Aoc2015/Program.fs
@@ -27,31 +27,62 @@ let run day job1 job2 =
     | Some (value, time) -> printfn "Result 2: %s (%.3fms)" value time
     | _ -> ()
 
+let runDay =
+    [| (fun () -> run 1 Day01.job1 Day01.job2)
+       (fun () -> run 2 Day02.job1 Day02.job2)
+       (fun () -> run 3 Day03.job1 Day03.job2)
+       (fun () -> run 4 Day04.job1 Day04.job2)
+       (fun () -> run 5 Day05.job1 Day05.job2)
+       (fun () -> run 6 Day06.job1 Day06.job2)
+       (fun () -> run 7 Day07.job1 Day07.job2)
+       (fun () -> run 8 Day08.job1 Day08.job2)
+       (fun () -> run 9 Day09.job1 Day09.job2)
+       (fun () -> run 10 Day10.job1 Day10.job2)
+       (fun () -> run 11 Day11.job1 Day11.job2)
+       (fun () -> run 12 Day12.job1 Day12.job2)
+       (fun () -> run 13 Day13.job1 Day13.job2)
+       (fun () -> run 14 Day14.job1 Day14.job2)
+       (fun () -> run 15 Day15.job1 Day15.job2)
+       (fun () -> run 16 Day16.job1 Day16.job2)
+       (fun () -> run 17 Day17.job1 Day17.job2)
+       (fun () -> run 18 Day18.job1 Day18.job2)
+       (fun () -> run 19 Day19.job1 Day19.job2)
+       (fun () -> run 20 Day20.job1 Day20.job2)
+       (fun () -> run 21 Day21.job1 Day21.job2)
+       (fun () -> run 22 Day22.job1 Day22.job2)
+       (fun () -> run 23 Day22.job1 Day23.job2)
+       (fun () -> run 24 Day22.job1 Day24.job2)
+       (fun () -> run 25 Day22.job1 Day25.job2) |]
+
 [<EntryPoint>]
-let main _argv =
-    run 1 Day01.job1 Day01.job2
-    run 2 Day02.job1 Day02.job2
-    run 3 Day03.job1 Day03.job2
-    run 4 Day04.job1 Day04.job2
-    run 5 Day05.job1 Day05.job2
-    run 6 Day06.job1 Day06.job2
-    run 7 Day07.job1 Day07.job2
-    run 8 Day08.job1 Day08.job2
-    run 9 Day09.job1 Day09.job2
-    run 10 Day10.job1 Day10.job2
-    run 11 Day11.job1 Day11.job2
-    run 12 Day12.job1 Day12.job2
-    run 13 Day13.job1 Day13.job2
-    run 14 Day14.job1 Day14.job2
-    run 15 Day15.job1 Day15.job2
-    run 16 Day16.job1 Day16.job2
-    run 17 Day17.job1 Day17.job2
-    run 18 Day18.job1 Day18.job2
-    run 19 Day19.job1 Day19.job2
-    run 20 Day20.job1 Day20.job2
-    run 21 Day21.job1 Day21.job2
-    run 22 Day22.job1 Day22.job2
-    run 23 Day22.job1 Day23.job2
-    run 24 Day22.job1 Day24.job2
-    run 25 Day22.job1 Day25.job2
+let main argv =
+    match argv with
+    | [||] ->
+        run 1 Day01.job1 Day01.job2
+        run 2 Day02.job1 Day02.job2
+        run 3 Day03.job1 Day03.job2
+        run 4 Day04.job1 Day04.job2
+        run 5 Day05.job1 Day05.job2
+        run 6 Day06.job1 Day06.job2
+        run 7 Day07.job1 Day07.job2
+        run 8 Day08.job1 Day08.job2
+        run 9 Day09.job1 Day09.job2
+        run 10 Day10.job1 Day10.job2
+        run 11 Day11.job1 Day11.job2
+        run 12 Day12.job1 Day12.job2
+        run 13 Day13.job1 Day13.job2
+        run 14 Day14.job1 Day14.job2
+        run 15 Day15.job1 Day15.job2
+        run 16 Day16.job1 Day16.job2
+        run 17 Day17.job1 Day17.job2
+        run 18 Day18.job1 Day18.job2
+        run 19 Day19.job1 Day19.job2
+        run 20 Day20.job1 Day20.job2
+        run 21 Day21.job1 Day21.job2
+        run 22 Day22.job1 Day22.job2
+        run 23 Day22.job1 Day23.job2
+        run 24 Day22.job1 Day24.job2
+        run 25 Day22.job1 Day25.job2
+    | _ -> argv |> Seq.iter (fun arg -> runDay.[(arg |> int) - 1] ())
+
     0
