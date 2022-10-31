e4ea4f7639e86bea09384dc67bb106a4ed46e3ee
Arnold Filip <arnold.filip@gmail.com>
2022-10-31 14:28:08 +0100

    F# 2015-11

--- 2015/fsharp/Aoc2015.Tests/Day11.Tests.fs
+++ 2015/fsharp/Aoc2015.Tests/Day11.Tests.fs
@@ -4,10 +4,29 @@ open Xunit
 open Utils
 open Day11
 
+[<Theory>]
+[<InlineData("xx", "xy")>]
+[<InlineData("xy", "xz")>]
+[<InlineData("xz", "ya")>]
+[<InlineData("ya", "yb")>]
+let ``Day11 increment works`` current next =
+    Assert.Equal(next, current |> encode |> increment |> decode)
+
+[<Theory>]
+[<InlineData("abcdffaa")>]
+[<InlineData("ghjaabcc")>]
+let ``Day11 has straight works`` valid = Assert.True(valid |> hasStraight)
+
+[<Theory>]
+[<InlineData("abcdefgh", "abcdffaa")>]
+[<InlineData("ghijklmn", "ghjaabcc")>]
+let ``Day11 next valid password works`` current next =
+    Assert.Equal(next, current |> nextValidPassword)
+
 [<Fact>]
 let ``Day11 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("vzbxxyzz", job1 ())
+        Assert.Equal("vzcaabcc", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2015/fsharp/Aoc2015/Day11.fs
+++ 2015/fsharp/Aoc2015/Day11.fs
@@ -2,12 +2,70 @@ module Day11
 
 open Utils
 
-let input = readInputLines "11"
+let encode password = password |> String.toCharArray
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let decode (pw: char[]) = pw |> System.String |> string
+
+let inc c = c + (1 |> char)
+
+let hasStraight pw =
+    pw
+    |> Seq.windowed 3
+    |> Seq.exists (function
+        | [| a; b; c |] -> inc a = b && inc b = c
+        | _ -> false)
+
+let hasPairs pw =
+    pw
+    |> Seq.pairwise
+    |> Seq.filter (fun (a, b) -> a = b)
+    |> Seq.distinct
+    |> Seq.length
+    |> (fun l -> l >= 2)
+
+let isInvalidChar c = c = 'i' || c = 'o' || c = 'l'
+
+let hasInvalidChar pw =
+    pw |> Seq.tryFind isInvalidChar |> Option.isSome
+
+let isValid pw =
+    hasStraight pw && hasPairs pw && (hasInvalidChar pw |> not)
+
+let increment (pw: char[]) =
+    let rec loop i (pw: char[]) =
+        if i < 0 then
+            ()
+        else
+            match pw.[i] |> inc with
+            | c when isInvalidChar c -> pw.[i] <- c |> inc
+            | c when c <= 'z' -> pw.[i] <- c
+            | _ ->
+                pw.[i] <- 'a'
+                loop (i - 1) pw
+
+    match pw |> Seq.tryFindIndex isInvalidChar with
+    | Some i ->
+        pw.[i] <- pw.[i] |> inc
+
+        for j in (i + 1) .. (Array.length pw - 1) do
+            pw.[j] <- 'a'
+    | None -> loop (Array.length pw - 1) pw
+
+    pw
+
+let passwords init =
+    Seq.unfold
+        (fun pw ->
+            increment pw |> ignore
+            Some(pw, pw))
+        init
+
+let nextValidPassword current =
+    current |> encode |> passwords |> Seq.filter isValid |> Seq.head |> decode
+
+let input = readInputText "11"
+
+let job1 () = input |> nextValidPassword
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> nextValidPassword |> nextValidPassword
