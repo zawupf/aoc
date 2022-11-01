1d003c961870149dd1c5195f9970c4febcdbe1ab
Arnold Filip <arnold.filip@gmail.com>
2022-11-01 19:48:24 +0100

    F# 2015-13

--- 2015/fsharp/Aoc2015.Tests/Day13.Tests.fs
+++ 2015/fsharp/Aoc2015.Tests/Day13.Tests.fs
@@ -4,10 +4,37 @@ open Xunit
 open Utils
 open Day13
 
+[<Literal>]
+let sample =
+    """
+Alice would gain 54 happiness units by sitting next to Bob.
+Alice would lose 79 happiness units by sitting next to Carol.
+Alice would lose 2 happiness units by sitting next to David.
+Bob would gain 83 happiness units by sitting next to Alice.
+Bob would lose 7 happiness units by sitting next to Carol.
+Bob would lose 63 happiness units by sitting next to David.
+Carol would lose 62 happiness units by sitting next to Alice.
+Carol would gain 60 happiness units by sitting next to Bob.
+Carol would gain 55 happiness units by sitting next to David.
+David would gain 46 happiness units by sitting next to Alice.
+David would lose 7 happiness units by sitting next to Bob.
+David would gain 41 happiness units by sitting next to Carol.
+"""
+
+[<Fact>]
+let ``Day13 total happiness works`` () =
+    let map = sample |> String.trim |> String.split '\n' |> parse
+    Assert.Equal(330, [| "David"; "Alice"; "Bob"; "Carol" |] |> totalHappiness map)
+
+[<Fact>]
+let ``Day13 max total happiness works`` () =
+    let map = sample |> String.trim |> String.split '\n' |> parse
+    Assert.Equal(330, map |> maxTotalHappiness)
+
 [<Fact>]
 let ``Day13 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("709", job1 ())
+        Assert.Equal("668", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2015/fsharp/Aoc2015/Day13.fs
+++ 2015/fsharp/Aoc2015/Day13.fs
@@ -2,12 +2,62 @@ module Day13
 
 open Utils
 
+let parse input =
+    let parseLine line =
+        match line with
+        | Regex (@"^(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+)\.$")
+                [ person1; sympathy; happiness; person2 ] ->
+            let h =
+                match sympathy with
+                | "gain" -> happiness |> int
+                | "lose" -> (happiness |> int) * -1
+                | _ -> failwithf "Invalid input: %s" line
+
+            (person1, person2), h
+        | _ -> failwithf "Invalid input: %s" line
+
+    input
+    |> Seq.fold
+        (fun map line ->
+            let persons, happiness = line |> parseLine
+            map |> Map.add persons happiness)
+        Map.empty<string * string, int>
+
+let persons map =
+    map |> Map.keys |> Seq.map (fun (person, _) -> person) |> Seq.distinct
+
+let happiness persons map = map |> Map.find persons
+
+let totalHappiness map seats =
+    let person = seats |> Seq.head
+
+    Seq.append seats (Seq.singleton person)
+    |> Seq.pairwise
+    |> Seq.sumBy (fun persons ->
+        let p1, p2 = persons
+        (happiness persons map) + (happiness (p2, p1) map))
+
+let maxTotalHappiness map =
+    let persons = map |> persons |> Seq.toList
+
+    let first, others =
+        match persons with
+        | first :: others -> first, others
+        | _ -> failwith "No guests?"
+
+    let happiness seats = (first :: seats) |> totalHappiness map
+
+    others |> Math.permutations |> Seq.maxBy happiness |> happiness
+
+let insertMe map =
+    map
+    |> persons
+    |> Seq.fold (fun map person -> map |> Map.add ("Me", person) 0 |> Map.add (person, "Me") 0) map
+
 let input = readInputLines "13"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> parse |> maxTotalHappiness |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> parse |> insertMe |> maxTotalHappiness |> string
