de5322ee7f6e172d0648722cb819a85b6e039b71
Arnold Filip <arnold.filip@gmail.com>
2022-11-02 11:44:04 +0100

    F# 2015-14

--- 2015/fsharp/Aoc2015.Tests/Day14.Tests.fs
+++ 2015/fsharp/Aoc2015.Tests/Day14.Tests.fs
@@ -4,10 +4,44 @@ open Xunit
 open Utils
 open Day14
 
+[<Literal>]
+let comet =
+    "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds."
+
+[<Literal>]
+let dancer =
+    "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."
+
+[<Theory>]
+[<InlineData(14, comet, 1)>]
+[<InlineData(140, comet, 10)>]
+[<InlineData(140, comet, 11)>]
+[<InlineData(140, comet, 12)>]
+[<InlineData(16, dancer, 1)>]
+[<InlineData(160, dancer, 10)>]
+[<InlineData(176, dancer, 11)>]
+[<InlineData(176, dancer, 12)>]
+[<InlineData(1120, comet, 1000)>]
+[<InlineData(1056, dancer, 1000)>]
+let ``Day14 distanceAfter works`` distance reindeer seconds =
+    Assert.Equal(
+        distance,
+        reindeer |> Reindeer.parse |> Reindeer.distanceAfter seconds
+    )
+
+[<Fact>]
+let ``Day14 maxScoreAfter works`` () =
+    Assert.Equal(
+        689,
+        [ comet; dancer ]
+        |> Seq.map Reindeer.parse
+        |> Reindeer.maxScoreAfter 1000
+    )
+
 [<Fact>]
 let ``Day14 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("2696", job1 ())
+        Assert.Equal("1084", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2015/fsharp/Aoc2015/Day14.fs
+++ 2015/fsharp/Aoc2015/Day14.fs
@@ -2,12 +2,63 @@ module Day14
 
 open Utils
 
+type Reindeer =
+    { Name: string
+      Speed: int
+      FlyTime: int
+      RestTime: int }
+
+    member this.FullTime = this.FlyTime + this.RestTime
+
+module Reindeer =
+    let distanceAfter seconds (reindeer: Reindeer) =
+        let fulltimeCount = seconds / reindeer.FullTime
+        let remainingTime = seconds % reindeer.FullTime
+
+        reindeer.Speed
+        * (fulltimeCount * reindeer.FlyTime
+           + (min remainingTime reindeer.FlyTime))
+
+    let maxScoreAfter seconds (reindeers: Reindeer seq) =
+        let race = reindeers |> Seq.map (fun reindeer -> reindeer, 0)
+
+        { 1..seconds }
+        |> Seq.fold
+            (fun race seconds ->
+                let distances =
+                    race
+                    |> Seq.map (fun (reindeer, score) ->
+                        (reindeer |> distanceAfter seconds), (reindeer, score))
+
+                let maxDistance = distances |> Seq.maxBy fst |> fst
+
+                distances
+                |> Seq.map (fun (distance, (reindeer, score)) ->
+                    if distance = maxDistance then
+                        reindeer, score + 1
+                    else
+                        reindeer, score))
+            race
+        |> Seq.maxBy snd
+        |> snd
+
+    let parse input =
+        match input with
+        | Regex @"^(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds\.$"
+                [ name; speed; flyTime; restTime ] ->
+            { Name = name
+              Speed = speed |> int
+              FlyTime = flyTime |> int
+              RestTime = restTime |> int }
+        | _ -> failwithf "Invalid input: %s" input
+
 let input = readInputLines "14"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input
+    |> Seq.map (Reindeer.parse >> Reindeer.distanceAfter 2503)
+    |> Seq.max
+    |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> Seq.map Reindeer.parse |> Reindeer.maxScoreAfter 2503 |> string
