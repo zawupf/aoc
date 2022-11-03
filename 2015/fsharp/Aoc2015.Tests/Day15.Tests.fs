0bdf757576aa000cbc3557c483f356003521bec1
Arnold Filip <arnold.filip@gmail.com>
2022-11-03 15:59:14 +0100

    F# 2015-15

--- 2015/fsharp/Aoc2015.Tests/Day15.Tests.fs
+++ 2015/fsharp/Aoc2015.Tests/Day15.Tests.fs
@@ -4,10 +4,28 @@ open Xunit
 open Utils
 open Day15
 
+let ingredients =
+    [ "Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8"
+      "Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3" ]
+    |> List.map Ingredient.parse
+
+[<Fact>]
+let ``Day15 total score works`` () =
+    Assert.Equal(62_842_880L, ingredients |> List.zip [ 44; 56 ] |> totalScore)
+
+[<Fact>]
+let ``Day15 max total score works`` () =
+    Assert.Equal(62_842_880L, ingredients |> maxTotalScore)
+
+[<Fact>]
+let ``Day15 amounts works`` () =
+    amounts (ingredients |> List.length)
+    |> List.iter (fun l -> Assert.Equal(100, l |> List.sum))
+
 [<Fact>]
 let ``Day15 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("13882464", job1 ())
+        Assert.Equal("11171160", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2015/fsharp/Aoc2015/Day15.fs
+++ 2015/fsharp/Aoc2015/Day15.fs
@@ -2,12 +2,100 @@ module Day15
 
 open Utils
 
-let input = readInputLines "15"
+type Ingredient =
+    { Name: string
+      Capacity: int
+      Durability: int
+      Flavor: int
+      Texture: int
+      Calories: int }
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+module Ingredient =
+    let parse input =
+        let pattern =
+            @"(\w+): capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)"
 
-let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+        match input with
+        | Regex pattern [ name; capacity; durability; flavor; texture; calories ] ->
+            { Name = name
+              Capacity = capacity |> int
+              Durability = durability |> int
+              Flavor = flavor |> int
+              Texture = texture |> int
+              Calories = calories |> int }
+        | _ -> failwith $"Invalid input: %s{input}"
+
+    let properties (amount, ingredient) =
+        let { Capacity = capacity
+              Durability = durability
+              Flavor = flavor
+              Texture = texture } =
+            ingredient
+
+        [| capacity; durability; flavor; texture |]
+        |> Array.map (fun value -> amount * value)
+
+    let calories (amount, ingredient) =
+        let { Calories = calories } = ingredient
+        amount * calories
+
+let totalScore recipe =
+    recipe
+    |> List.map (fun amount -> amount |> Ingredient.properties)
+    |> List.fold
+        (fun sum properties ->
+            Array.zip sum properties |> Array.map (fun (a, b) -> a + b))
+        [| 0; 0; 0; 0 |]
+    |> Array.fold (fun product value -> product * (max 0L value)) 1L
+
+let totalCalories recipe =
+    recipe |> List.map (fun amount -> amount |> Ingredient.calories) |> List.sum
+
+let amounts n =
+    let rec loop n max result =
+        match n, result with
+        | 0, _ -> failwith "No ingredients, no cookies!"
+        | 1, [] -> [ max, [ max ] ]
+        | 1, _ ->
+            result
+            |> List.map (fun (current, amounts) ->
+                let missing = max - current
+                max, missing :: amounts)
+        | _, [] -> loop (n - 1) max [ for i in 0..max -> i, [ i ] ]
+        | _, _ ->
+            loop
+                (n - 1)
+                max
+                (result
+                 |> List.fold
+                     (fun result (current, amounts) ->
+                         [ 0 .. max - current ]
+                         |> List.fold
+                             (fun result i ->
+                                 (i + current, i :: amounts) :: result)
+                             result)
+                     [])
+
+    loop n 100 [] |> List.map snd
+
+let maxTotalScore ingredients =
+    let n = ingredients |> List.length
+
+    amounts n
+    |> List.map (fun amounts -> ingredients |> List.zip amounts |> totalScore)
+    |> List.max
+
+let maxTotalScore500 ingredients =
+    let n = ingredients |> List.length
+
+    amounts n
+    |> List.filter (fun amounts ->
+        ingredients |> List.zip amounts |> totalCalories = 500)
+    |> List.map (fun amounts -> ingredients |> List.zip amounts |> totalScore)
+    |> List.max
+
+let input = readInputLines "15" |> Seq.map Ingredient.parse |> Seq.toList
+
+let job1 () = input |> maxTotalScore |> string
+
+let job2 () = input |> maxTotalScore500 |> string
