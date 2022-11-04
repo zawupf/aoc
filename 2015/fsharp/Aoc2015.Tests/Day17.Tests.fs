7d4fd45cbde30b1b02e5a8eedccacaabd82c8db6
Arnold Filip <arnold.filip@gmail.com>
2022-11-04 16:30:35 +0100

    F# 2015-17

--- 2015/fsharp/Aoc2015.Tests/Day17.Tests.fs
+++ 2015/fsharp/Aoc2015.Tests/Day17.Tests.fs
@@ -4,10 +4,16 @@ open Xunit
 open Utils
 open Day17
 
+let sizes = [ 20; 15; 10; 5; 5 ]
+
+[<Fact>]
+let ``Day17 count combinations works`` () =
+    Assert.Equal(4, sizes |> countCombinations 25)
+
 [<Fact>]
 let ``Day17 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("654", job1 ())
+        Assert.Equal("57", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2015/fsharp/Aoc2015/Day17.fs
+++ 2015/fsharp/Aoc2015/Day17.fs
@@ -2,12 +2,44 @@ module Day17
 
 open Utils
 
-let input = readInputLines "17"
+let combinations store sizes =
+    let rec loop stack results =
+        match stack with
+        | [] -> results
+        | (missing, combinations, sizes) :: remaining ->
+            match missing with
+            | 0 -> loop remaining (combinations @ results)
+            | _ ->
+                match sizes with
+                | [] -> loop remaining results
+                | n :: sizes when n <= missing ->
+                    let combinations' =
+                        match combinations with
+                        | [] -> [ [ n ] ]
+                        | _ -> combinations |> List.map (fun c -> n :: c)
+
+                    loop
+                        ((missing - n, combinations', sizes)
+                         :: ((missing, combinations, sizes) :: remaining))
+                        results
+                | _ :: sizes ->
+                    loop ((missing, combinations, sizes) :: remaining) results
+
+    loop [ store, [], sizes ] []
+
+let countCombinations store sizes = combinations store sizes |> List.length
+
+let countMinimalCombinations store sizes =
+    combinations store sizes
+    |> List.countBy List.length
+    |> List.minBy fst
+    |> snd
+
+
+let input = readInputLines "17" |> List.ofSeq |> List.map int
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> countCombinations 150 |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> countMinimalCombinations 150 |> string
