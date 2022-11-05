648310e0372e79974c6e6c0f5a0fb0bb713cce91
Arnold Filip <arnold.filip@gmail.com>
2022-11-05 20:27:59 +0100

    F# 2015-18

--- 2015/fsharp/Aoc2015.Tests/Day18.Tests.fs
+++ 2015/fsharp/Aoc2015.Tests/Day18.Tests.fs
@@ -4,10 +4,62 @@ open Xunit
 open Utils
 open Day18
 
+let input =
+    ".#.#.#
+...##.
+#....#
+..#...
+#.#..#
+####.."
+
+[<Fact>]
+let ``Day18 parse works`` () =
+    Assert.Equal(input, input |> Grid.parse id |> Grid.toString)
+
+[<Theory>]
+[<InlineData(0, -1)>]
+[<InlineData(0, 0)>]
+[<InlineData(1, 5)>]
+[<InlineData(1, 30)>]
+[<InlineData(0, 35)>]
+[<InlineData(0, 36)>]
+let ``Day18 item works`` count index =
+    let grid = input |> Grid.parse id
+    Assert.Equal(count, grid |> Grid.item index)
+
+[<Theory>]
+[<InlineData(0, -1)>]
+[<InlineData(1, 0)>]
+[<InlineData(1, 5)>]
+[<InlineData(2, 30)>]
+[<InlineData(1, 35)>]
+// [<InlineData(0, 36)>]
+let ``Day18 countAdjacentLights works`` count index =
+    let grid = input |> Grid.parse id
+    Assert.Equal(count, grid |> Grid.countAdjacentLights index)
+
+[<Fact>]
+let ``Day18 next grid works`` () =
+    Assert.Equal(
+        4,
+        input |> Grid.parse id |> grids id |> Seq.item 4 |> Grid.countLights
+    )
+
+[<Fact>]
+let ``Day18 next grid with overrides works`` () =
+    Assert.Equal(
+        17,
+        input
+        |> Grid.parse cornerLightsStuckOn
+        |> grids cornerLightsStuckOn
+        |> Seq.item 5
+        |> Grid.countLights
+    )
+
 [<Fact>]
 let ``Day18 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("821", job1 ())
+        Assert.Equal("886", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2015/fsharp/Aoc2015/Day18.fs
+++ 2015/fsharp/Aoc2015/Day18.fs
@@ -2,12 +2,93 @@ module Day18
 
 open Utils
 
-let input = readInputLines "18"
+type Grid = { Data: int[]; Size: int }
+
+module Grid =
+    let parse overrides input =
+        let lines = input |> String.trim |> String.split '\n'
+        let size = lines |> Seq.length
+
+        let data =
+            lines
+            |> String.join ""
+            |> Seq.map (fun c -> if c = '#' then 1 else 0)
+            |> Seq.toArray
+
+        { Data = data; Size = size } |> overrides
+
+    let toString grid =
+        grid.Data
+        |> Array.map (fun on -> if on = 0 then "." else "#")
+        |> String.join ""
+        |> Seq.chunkBySize grid.Size
+        |> Seq.map (fun o -> System.String(o) |> string)
+        |> String.join "\n"
+
+    let item index { Data = data } =
+        if index >= 0 && index < data.Length then
+            data.[index]
+        else
+            0
+
+    let countAdjacentLights i grid =
+        let { Size = size } = grid
+
+        let toPos index = index % size, index / size
+
+        let toIndex pos =
+            match pos with
+            | x, y when x >= 0 && x < size && y >= 0 && y < size -> y * size + x
+            | _ -> -1
+
+        let x, y = i |> toPos
+
+        [| x - 1, y - 1
+           x, y - 1
+           x + 1, y - 1
+           x - 1, y
+           x + 1, y
+           x - 1, y + 1
+           x, y + 1
+           x + 1, y + 1 |]
+        |> Array.sumBy (fun pos -> grid |> item (pos |> toIndex))
+
+    let countLights { Data = data } = data |> Array.sum
+
+    let next overrides grid =
+        let nextState index on =
+            match on, countAdjacentLights index grid with
+            | 1, 2
+            | 1, 3
+            | 0, 3 -> 1
+            | _, _ -> 0
+
+        { grid with Data = grid.Data |> Array.mapi nextState } |> overrides
+
+let grids overrides init =
+    Seq.unfold (fun grid -> Some(grid, Grid.next overrides grid)) init
+
+let cornerLightsStuckOn grid =
+    grid.Data.[0] <- 1
+    grid.Data.[grid.Size - 1] <- 1
+    grid.Data.[grid.Data.Length - grid.Size] <- 1
+    grid.Data.[grid.Data.Length - 1] <- 1
+    grid
+
+let input = readInputText "18"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input
+    |> Grid.parse id
+    |> grids id
+    |> Seq.item 100
+    |> Grid.countLights
+    |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input
+    |> Grid.parse cornerLightsStuckOn
+    |> grids cornerLightsStuckOn
+    |> Seq.item 100
+    |> Grid.countLights
+    |> string
