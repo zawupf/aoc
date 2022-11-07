ef201b33e8450492949644a7e612bebf894c0195
Arnold Filip <arnold.filip@gmail.com>
2022-11-07 19:19:43 +0100

    F# 2015-20

--- 2015/fsharp/Aoc2015.Tests/Day20.Tests.fs
+++ 2015/fsharp/Aoc2015.Tests/Day20.Tests.fs
@@ -4,10 +4,36 @@ open Xunit
 open Utils
 open Day20
 
+[<Theory>]
+[<InlineData(10, 1)>]
+[<InlineData(30, 2)>]
+[<InlineData(40, 3)>]
+[<InlineData(70, 4)>]
+[<InlineData(60, 5)>]
+[<InlineData(120, 6)>]
+[<InlineData(80, 7)>]
+[<InlineData(150, 8)>]
+[<InlineData(130, 9)>]
+let ``Day20 present count works`` expected house =
+    Assert.Equal(expected, presentCount1 () |> Seq.item (house - 1))
+
+[<Theory>]
+[<InlineData(10, 1)>]
+[<InlineData(30, 2)>]
+[<InlineData(40, 3)>]
+[<InlineData(70, 4)>]
+[<InlineData(60, 4)>]
+[<InlineData(120, 6)>]
+[<InlineData(80, 6)>]
+[<InlineData(150, 8)>]
+[<InlineData(130, 8)>]
+let ``Day20 findFirstHouseWithAtLeast works`` limit house =
+    Assert.Equal(house, limit |> findFirstHouseWithAtLeast presentCount1)
+
 [<Fact>]
 let ``Day20 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("665280", job1 ())
+        Assert.Equal("705600", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2015/fsharp/Aoc2015/Day20.fs
+++ 2015/fsharp/Aoc2015/Day20.fs
@@ -2,12 +2,32 @@ module Day20
 
 open Utils
 
-let input = readInputLines "20"
+let presentCount1 () =
+    Seq.unfold
+        (fun house ->
+            let singleSum = (house |> Math.divisors |> List.sum)
+            Some(singleSum * 10, house + 1))
+        1
+
+let presentCount2 () =
+    Seq.unfold
+        (fun house ->
+            let singleSum =
+                (house
+                 |> Math.divisors
+                 |> List.filter (fun d -> d > (house - 1) / 50)
+                 |> List.sum)
+
+            Some(singleSum * 11, house + 1))
+        1
+
+let findFirstHouseWithAtLeast counting limit =
+    1 + (counting () |> Seq.findIndex (fun n -> n >= limit))
+
+let input = readInputText "20" |> int
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> findFirstHouseWithAtLeast presentCount1 |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> findFirstHouseWithAtLeast presentCount2 |> string
