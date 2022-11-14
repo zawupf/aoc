da0e953cfc45c18b98734b660abe5755b9e6247f
Arnold Filip <arnold.filip@gmail.com>
2022-11-14 14:35:41 +0100

    F# 2015-24

--- 2015/fsharp/Aoc2015.Tests/Day24.Tests.fs
+++ 2015/fsharp/Aoc2015.Tests/Day24.Tests.fs
@@ -4,10 +4,20 @@ open Xunit
 open Utils
 open Day24
 
+let input = [ 1..5 ] @ [ 7..11 ]
+
+[<Fact>]
+let ``Day24 part 1 works`` () =
+    Assert.Equal(99L, input |> findMinimalEntanglement 3)
+
+[<Fact>]
+let ``Day24 part 2 works`` () =
+    Assert.Equal(44L, input |> findMinimalEntanglement 4)
+
 [<Fact>]
 let ``Day24 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("11846773891", job1 ())
+        Assert.Equal("80393059", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2015/fsharp/Aoc2015/Day24.fs
+++ 2015/fsharp/Aoc2015/Day24.fs
@@ -2,12 +2,46 @@ module Day24
 
 open Utils
 
-let input = readInputLines "24"
+let distributePackages n packages =
+    let sum = List.sum packages / n
+
+    let rec loop results stack targetSum =
+        match stack with
+        | [] -> results
+        | (sum, used, remaining) :: stack ->
+            match remaining with
+            | [] -> loop results stack targetSum
+            | n :: ns ->
+                match sum + n with
+                | s when s = targetSum ->
+                    let results' = (n :: used) :: results
+                    loop results' stack targetSum
+                | s when s < targetSum ->
+                    let stack' = (s, n :: used, ns) :: (sum, used, ns) :: stack
+                    loop results stack' targetSum
+                | _ -> loop results stack targetSum
+
+    loop [] [ 0, [], packages |> List.sort ] sum
+
+let findMinimalConfigurations n packages =
+    let configs = packages |> distributePackages n
+    let minLength = configs |> List.map List.length |> List.min
+
+    configs |> List.filter (fun list -> list |> List.length = minLength)
+
+let quantumEntanglement group =
+    group |> List.fold (fun product value -> product * int64 value) 1L
+
+let findMinimalEntanglement n packages =
+    packages
+    |> findMinimalConfigurations n
+    |> List.map quantumEntanglement
+    |> List.min
+
+let input = readInputLines "24" |> Seq.map int |> Seq.toList
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> findMinimalEntanglement 3 |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> findMinimalEntanglement 4 |> string
