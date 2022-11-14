c9543aa34e154b0419c9f08505d679cc90208dd8
Arnold Filip <arnold.filip@gmail.com>
2022-11-14 15:49:13 +0100

    F# 2015-25

--- 2015/fsharp/Aoc2015.Tests/Day25.Tests.fs
+++ 2015/fsharp/Aoc2015.Tests/Day25.Tests.fs
@@ -5,9 +5,21 @@ open Utils
 open Day25
 
 [<Fact>]
+let ``Day25 parse works`` () =
+    Assert.Equal((2981, 3075), readInputText "25" |> parse)
+
+[<Theory>]
+[<InlineData(1, 1, 20151125UL)>]
+[<InlineData(5, 1, 77061)>]
+[<InlineData(6, 3, 25397450UL)>]
+[<InlineData(3, 6, 16474243UL)>]
+[<InlineData(6, 6, 27995004)>]
+let ``Day25 codeAt works`` row col code = Assert.Equal(code, codeAt row col)
+
+[<Fact>]
 let ``Day25 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("9132360", job1 ())
+        Assert.Equal("Ho Ho Ho!", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2015/fsharp/Aoc2015/Day25.fs
+++ 2015/fsharp/Aoc2015/Day25.fs
@@ -2,12 +2,26 @@ module Day25
 
 open Utils
 
-let input = readInputLines "25"
+let codes () =
+    Seq.unfold
+        (fun (row, col, code) ->
+            let row', col' = if row = 1 then col + 1, 1 else row - 1, col + 1
+            let code' = code * 252533UL % 33554393UL
+            Some((row, col, code), (row', col', code')))
+        (1, 1, 20151125UL)
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let codeAt row col =
+    codes ()
+    |> Seq.pick (fun (r, c, code) ->
+        if r = row && c = col then Some code else None)
 
-let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let parse input =
+    match input with
+    | Regex @"row (\d+), column (\d+)" [ row; col ] -> int row, int col
+    | _ -> failwith "Invalid input"
+
+let input = readInputText "25" |> parse
+
+let job1 () = input ||> codeAt |> string
+
+let job2 () = "Ho Ho Ho!"
