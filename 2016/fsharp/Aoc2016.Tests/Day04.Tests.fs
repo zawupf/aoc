c811b56293015574c3b9d4e249be1bf13f3a861c
Arnold Filip <arnold.filip@gmail.com>
2022-11-15 20:38:07 +0100

    F# 2016-04

--- 2016/fsharp/Aoc2016.Tests/Day04.Tests.fs
+++ 2016/fsharp/Aoc2016.Tests/Day04.Tests.fs
@@ -4,10 +4,38 @@ open Xunit
 open Utils
 open Day04
 
+[<Theory>]
+[<InlineData(true, "aaaaa-bbb-z-y-x-123[abxyz]")>]
+[<InlineData(true, "a-b-c-d-e-f-g-h-987[abcde]")>]
+[<InlineData(true, "not-a-real-room-404[oarel]")>]
+[<InlineData(false, "totally-real-room-200[decoy]")>]
+let ``Day04 is real room works`` expected room =
+    Assert.Equal(expected, room |> Room.parse |> Room.isReal)
+
+[<Fact>]
+let ``Day04 sum of real sector ids works`` () =
+    Assert.Equal(
+        1514,
+        [ "aaaaa-bbb-z-y-x-123[abxyz]"
+          "a-b-c-d-e-f-g-h-987[abcde]"
+          "not-a-real-room-404[oarel]"
+          "totally-real-room-200[decoy]" ]
+        |> List.map Room.parse
+        |> List.filter Room.isReal
+        |> List.sumBy (fun room -> room.SectorID)
+    )
+
+[<Fact>]
+let ``Day04 real name works`` () =
+    Assert.Equal(
+        "very encrypted name",
+        "qzmt-zixmtkozy-ivhz-343[decoy]" |> Room.parse |> Room.realName
+    )
+
 [<Fact>]
 let ``Day04 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("158835", job1 ())
+        Assert.Equal("993", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2016/fsharp/Aoc2016/Day04.fs
+++ 2016/fsharp/Aoc2016/Day04.fs
@@ -2,12 +2,65 @@ module Day04
 
 open Utils
 
-let input = readInputLines "04"
+type Room =
+    { Name: string
+      SectorID: int
+      Checksum: string }
+
+module Room =
+    let parse input =
+        match input with
+        | Regex @"^([a-z\-]+)-(\d+)\[([a-z]+)\]$" [ name; Int sectorId; checksum ] ->
+            { Name = name
+              SectorID = sectorId
+              Checksum = checksum }
+        | _ -> failwith $"Invalid room: %s{input}"
+
+    let isReal room =
+        let { Name = name; Checksum = checksum } = room
+
+        name
+        |> String.toCharArray
+        |> Seq.filter (fun c -> c <> '-')
+        |> Seq.countBy id
+        |> Seq.groupBy snd
+        |> Seq.sortByDescending fst
+        |> Seq.collect (fun (_, chars) ->
+            chars |> Seq.map fst |> Seq.sort |> Seq.toArray)
+        |> Seq.take 5
+        |> Seq.toArray
+        |> System.String
+        |> string
+        |> (=) checksum
+
+    let realName room =
+        room.Name
+        |> String.toCharArray
+        |> Array.map (function
+            | '-' -> ' '
+            | c ->
+                let offset = room.SectorID % 26
+                let i = int c - int 'a'
+                let j = (i + offset) % 26
+                let c' = int 'a' + j
+                c' |> char)
+        |> System.String
+        |> string
+
+let input = readInputLines "04" |> Seq.map Room.parse |> Seq.toArray
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input
+    |> Seq.filter Room.isReal
+    |> Seq.sumBy (fun room -> room.SectorID)
+    |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input
+    |> Seq.filter Room.isReal
+    |> Seq.pick (fun room ->
+        if room |> Room.realName = "northpole object storage" then
+            Some room.SectorID
+        else
+            None)
+    |> string
