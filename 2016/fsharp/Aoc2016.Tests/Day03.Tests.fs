9d19921c5644b581c7451d10d95937940ba3ff82
Arnold Filip <arnold.filip@gmail.com>
2022-11-15 18:43:37 +0100

    F# 2016-03

--- 2016/fsharp/Aoc2016.Tests/Day03.Tests.fs
+++ 2016/fsharp/Aoc2016.Tests/Day03.Tests.fs
@@ -4,10 +4,19 @@ open Xunit
 open Utils
 open Day03
 
+[<Theory>]
+[<InlineData(false, "5 10 25")>]
+[<InlineData(true, "3 4 5")>]
+let ``Day03 isMaybeTriangle works`` expected triangle =
+    Assert.Equal(
+        expected,
+        triangle |> Triangle.parse |> Triangle.isMaybeTriangle
+    )
+
 [<Fact>]
 let ``Day03 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("869", job1 ())
+        Assert.Equal("1544", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2016/fsharp/Aoc2016/Day03.fs
+++ 2016/fsharp/Aoc2016/Day03.fs
@@ -2,12 +2,40 @@ module Day03
 
 open Utils
 
-let input = readInputLines "03"
+type Triangle = int * int * int
+
+module Triangle =
+    let isMaybeTriangle (a, b, c) = a + b > c && a + c > b && b + c > a
+
+    let parse string =
+        string
+        |> String.parseInts ' '
+        |> function
+            | [ a; b; c ] -> a, b, c
+            | _ -> failwith $"Invalid triangle: %s{string}"
+
+    let parseColums lines =
+        lines
+        |> List.map (String.parseInts ' ')
+        |> List.transpose
+        |> List.concat
+        |> List.chunkBySize 3
+        |> List.map (function
+            | [ a; b; c ] -> a, b, c
+            | _ -> failwith "Invalid columns")
+
+let input = readInputLines "03" |> Seq.toList
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input
+    |> List.map Triangle.parse
+    |> List.filter Triangle.isMaybeTriangle
+    |> List.length
+    |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input
+    |> Triangle.parseColums
+    |> List.filter Triangle.isMaybeTriangle
+    |> List.length
+    |> string
