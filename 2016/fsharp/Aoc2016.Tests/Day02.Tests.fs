e40c9e455a0878be94fda3629d921d7bdee8d4cb
Arnold Filip <arnold.filip@gmail.com>
2022-11-15 12:54:40 +0100

    F# 2016-02

--- 2016/fsharp/Aoc2016.Tests/Day02.Tests.fs
+++ 2016/fsharp/Aoc2016.Tests/Day02.Tests.fs
@@ -5,9 +5,70 @@ open Utils
 open Day02
 
 [<Fact>]
+let ``Day02 moveInSquare works`` () =
+    let test direction keys expected =
+        keys
+        |> List.map (moveInSquare direction)
+        |> List.zip expected
+        |> List.iter Assert.Equal
+
+    test "U" [ 1; 2; 3; 4; 5; 6; 7; 8; 9 ] [ 1; 2; 3; 1; 2; 3; 4; 5; 6 ]
+    test "D" [ 1; 2; 3; 4; 5; 6; 7; 8; 9 ] [ 4; 5; 6; 7; 8; 9; 7; 8; 9 ]
+    test "L" [ 1; 2; 3; 4; 5; 6; 7; 8; 9 ] [ 1; 1; 2; 4; 4; 5; 7; 7; 8 ]
+    test "R" [ 1; 2; 3; 4; 5; 6; 7; 8; 9 ] [ 2; 3; 3; 5; 6; 6; 8; 9; 9 ]
+
+[<Fact>]
+let ``Day02 moveInDiamond works`` () =
+    let test direction keys expected =
+        keys
+        |> List.map (moveInDiamond direction)
+        |> List.zip expected
+        |> List.iter Assert.Equal
+
+    test
+        "U"
+        [ 1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12; 13 ]
+        [ 1; 2; 1; 4; 5; 2; 3; 4; 9; 6; 7; 8; 11 ]
+
+    test
+        "D"
+        [ 1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12; 13 ]
+        [ 3; 6; 7; 8; 5; 10; 11; 12; 9; 10; 13; 12; 13 ]
+
+    test
+        "L"
+        [ 1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12; 13 ]
+        [ 1; 2; 2; 3; 5; 5; 6; 7; 8; 10; 10; 11; 13 ]
+
+    test
+        "R"
+        [ 1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12; 13 ]
+        [ 1; 3; 4; 4; 6; 7; 8; 9; 9; 11; 12; 12; 13 ]
+
+[<Fact>]
+let ``Day02 square code works`` () =
+    Assert.Equal(
+        "1985",
+        "ULL\nRRDDD\nLURDL\nUUUUD"
+        |> String.split '\n'
+        |> Array.toList
+        |> code moveInSquare
+    )
+
+[<Fact>]
+let ``Day02 diamond code works`` () =
+    Assert.Equal(
+        "5DB3",
+        "ULL\nRRDDD\nLURDL\nUUUUD"
+        |> String.split '\n'
+        |> Array.toList
+        |> code moveInDiamond
+    )
+
+[<Fact>]
 let ``Day02 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("84452", job1 ())
+        Assert.Equal("D65C3", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2016/fsharp/Aoc2016/Day02.fs
+++ 2016/fsharp/Aoc2016/Day02.fs
@@ -2,12 +2,53 @@ module Day02
 
 open Utils
 
-let input = readInputLines "02"
+let moveInSquare moves key =
+    moves
+    |> Seq.fold
+        (fun key direction ->
+            match direction with
+            | 'U' -> if key < 4 then key else (key - 3)
+            | 'D' -> if key > 6 then key else (key + 3)
+            | 'L' -> if (key - 1) % 3 = 0 then key else (key - 1)
+            | 'R' -> if (key - 1) % 3 = 2 then key else (key + 1)
+            | c -> failwithf $"Invalid direction '%c{c}'")
+        key
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let moveInDiamond moves key =
+    moves
+    |> Seq.fold
+        (fun key direction ->
+            match direction with
+            | 'U' ->
+                match key with
+                | k when [| 1; 2; 4; 5; 9 |] |> Array.contains k -> k
+                | k when [| 3; 13 |] |> Array.contains k -> k - 2
+                | k -> k - 4
+            | 'D' ->
+                match key with
+                | k when [| 5; 9; 10; 12; 13 |] |> Array.contains k -> k
+                | k when [| 1; 11 |] |> Array.contains k -> k + 2
+                | k -> k + 4
+            | 'L' ->
+                match key with
+                | k when [| 1; 2; 5; 10; 13 |] |> Array.contains k -> k
+                | k -> k - 1
+            | 'R' ->
+                match key with
+                | k when [| 1; 4; 9; 12; 13 |] |> Array.contains k -> k
+                | k -> k + 1
+            | c -> failwithf $"Invalid direction '%c{c}'")
+        key
 
-let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let code move input =
+    input
+    |> List.scan (fun key moves -> move moves key) 5
+    |> List.tail
+    |> List.map (sprintf "%X")
+    |> String.join ""
+
+let input = readInputLines "02" |> Seq.toList
+
+let job1 () = input |> code moveInSquare
+
+let job2 () = input |> code moveInDiamond
