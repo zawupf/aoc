82fcde0f420f3cdb5e1fa74ddcafdc1a7f9e3915
Arnold Filip <arnold.filip@gmail.com>
2022-11-15 22:10:31 +0100

    F# 2016-05

--- 2016/fsharp/Aoc2016.Tests/Day05.Tests.fs
+++ 2016/fsharp/Aoc2016.Tests/Day05.Tests.fs
@@ -5,9 +5,17 @@ open Utils
 open Day05
 
 [<Fact>]
+let ``Day05 computePassword works`` () =
+    Assert.Equal("18f47a30", "abc" |> computePassword)
+
+[<Fact>]
+let ``Day05 computePassword2 works`` () =
+    Assert.Equal("05ace8e3", "abc" |> computePassword2)
+
+[<Fact>]
 let ``Day05 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("d4cd2ee1", job1 ())
+        Assert.Equal("f2c730e5", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2016/fsharp/Aoc2016/Day05.fs
+++ 2016/fsharp/Aoc2016/Day05.fs
@@ -2,12 +2,40 @@ module Day05
 
 open Utils
 
-let input = readInputLines "05"
+let computePassword id =
+    Seq.unfold (fun i -> Some(Md5.ofString (id + string i), i + 1)) 0
+    |> Seq.filter (String.startsWith "00000")
+    |> Seq.take 8
+    |> Seq.map (fun md5 -> md5.[5])
+    |> Seq.toArray
+    |> System.String
+    |> (fun s -> s.ToLowerInvariant())
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let computePassword2 id =
+    let handled = System.Collections.Generic.HashSet<char>()
 
-let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    let isValid pos =
+        pos >= '0' && pos <= '7' && (handled.Contains pos |> not)
+
+    Seq.unfold (fun i -> Some(Md5.ofString (id + string i), i + 1)) 0
+    |> Seq.choose (fun md5 ->
+        match md5.StartsWith("00000") with
+        | true ->
+            match md5.[5] with
+            | pos when pos |> isValid ->
+                handled.Add(pos) |> ignore
+                Some(pos, md5.[6])
+            | _ -> None
+        | false -> None)
+    |> Seq.take 8
+    |> Seq.sortBy fst
+    |> Seq.map snd
+    |> Seq.toArray
+    |> System.String
+    |> (fun s -> s.ToLowerInvariant())
+
+let input = readInputText "05"
+
+let job1 () = input |> computePassword
+
+let job2 () = input |> computePassword2
