f2ebf51bd0ff9e4ead3da2f5f916e0dc2f53fafa
Arnold Filip <arnold.filip@gmail.com>
2022-11-16 09:29:31 +0100

    F# 2016-06

--- 2016/fsharp/Aoc2016.Tests/Day06.Tests.fs
+++ 2016/fsharp/Aoc2016.Tests/Day06.Tests.fs
@@ -4,10 +4,38 @@ open Xunit
 open Utils
 open Day06
 
+let input =
+    """
+    eedadn
+    drvtee
+    eandsr
+    raavrd
+    atevrs
+    tsrnev
+    sdttsa
+    rasrtv
+    nssdts
+    ntnada
+    svetve
+    tesnvt
+    vntsnd
+    vrdear
+    dvrsen
+    enarar
+    """
+    |> String.trim
+    |> String.split '\n'
+    |> Array.map String.trim
+
+[<Fact>]
+let ``Day06 corrected works`` () =
+    Assert.Equal("easter", input |> corrected Seq.maxBy)
+    Assert.Equal("advent", input |> corrected Seq.minBy)
+
 [<Fact>]
 let ``Day06 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("qoclwvah", job1 ())
+        Assert.Equal("ryrgviuv", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2016/fsharp/Aoc2016/Day06.fs
+++ 2016/fsharp/Aoc2016/Day06.fs
@@ -2,12 +2,15 @@ module Day06
 
 open Utils
 
-let input = readInputLines "06"
+let corrected chooser lines =
+    lines
+    |> Array.map String.toCharArray
+    |> Array.transpose
+    |> Array.map (fun chars -> chars |> Seq.countBy id |> chooser snd |> fst)
+    |> String.ofChars
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let input = readInputLines "06" |> Seq.toArray
 
-let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let job1 () = input |> corrected Seq.maxBy
+
+let job2 () = input |> corrected Seq.minBy
