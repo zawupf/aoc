bd370634e91fe082c4078131c9bb5b315f73508b
Arnold Filip <arnold.filip@gmail.com>
2022-11-16 20:31:19 +0100

    F# 2016-09

--- 2016/fsharp/Aoc2016.Tests/Day09.Tests.fs
+++ 2016/fsharp/Aoc2016.Tests/Day09.Tests.fs
@@ -4,10 +4,30 @@ open Xunit
 open Utils
 open Day09
 
+[<Theory>]
+[<InlineData("ADVENT", "ADVENT")>]
+[<InlineData("ABBBBBC", "A(1x5)BC")>]
+[<InlineData("XYZXYZXYZ", "(3x3)XYZ")>]
+[<InlineData("ABCBCDEFEFG", "A(2x2)BCD(2x2)EFG")>]
+[<InlineData("(1x3)A", "(6x1)(1x3)A")>]
+[<InlineData("X(3x3)ABC(3x3)ABCY", "X(8x2)(3x3)ABCY")>]
+let ``Day09 decompressed length 1 works`` expected data =
+    let result = data |> decompressedLength1
+    let expected = expected |> String.length |> int64
+    Assert.Equal(expected, result)
+
+[<Theory>]
+[<InlineData(9, "(3x3)XYZ")>]
+[<InlineData(20, "X(8x2)(3x3)ABCY")>]
+[<InlineData(241920, "(27x12)(20x12)(13x14)(7x10)(1x12)A")>]
+[<InlineData(445, "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN")>]
+let ``Day09 decompressed length 2 works`` expected data =
+    Assert.Equal(expected, data |> decompressedLength2)
+
 [<Fact>]
 let ``Day09 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("152851", job1 ())
+        Assert.Equal("11797310782", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2016/fsharp/Aoc2016/Day09.fs
+++ 2016/fsharp/Aoc2016/Day09.fs
@@ -2,12 +2,39 @@ module Day09
 
 open Utils
 
-let input = readInputLines "09"
+let decompressedLength len data =
+    let end' = data |> String.length
+    let mutable i = 0
+    let mutable length = 0L
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    while i < end' do
+        match data.[i] with
+        | '(' ->
+            let j = data.IndexOf(')', i + 1)
 
-let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+            match data.[i + 1 .. j - 1] with
+            | Regex @"^(\d+)x(\d+)$" [ Int n; Int repeat ] ->
+                let k = j + n
+                let s = data.[j + 1 .. k]
+                let l = s |> len
+                length <- length + l * int64 repeat
+                i <- k
+            | marker -> failwith $"Invalid marker: %s{marker}"
+        | ' ' -> ()
+        | _c -> length <- length + 1L
+
+        i <- i + 1
+
+    length
+
+let decompressedLength1 =
+    decompressedLength (Seq.sumBy (fun c -> if c = ' ' then 0L else 1L))
+
+let rec decompressedLength2 data =
+    decompressedLength decompressedLength2 data
+
+let input = readInputText "09"
+
+let job1 () = input |> decompressedLength1 |> string
+
+let job2 () = input |> decompressedLength2 |> string
