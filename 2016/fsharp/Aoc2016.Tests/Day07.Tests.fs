c17539ea8b36920f1e82e111b62309eaf26d0243
Arnold Filip <arnold.filip@gmail.com>
2022-11-16 10:50:24 +0100

    F# 2016-07

--- 2016/fsharp/Aoc2016.Tests/Day07.Tests.fs
+++ 2016/fsharp/Aoc2016.Tests/Day07.Tests.fs
@@ -4,10 +4,26 @@ open Xunit
 open Utils
 open Day07
 
+[<Theory>]
+[<InlineData(true, "abba[mnop]qrst")>]
+[<InlineData(false, "abcd[bddb]xyyx")>]
+[<InlineData(false, "aaaa[qwer]tyui")>]
+[<InlineData(true, "ioxxoj[asdfgh]zxcvbn")>]
+let ``Day07 isTlsSupported works`` expected ip =
+    Assert.Equal(expected, ip |> isTlsSupported)
+
+[<Theory>]
+[<InlineData(true, "aba[bab]xyz")>]
+[<InlineData(false, "xyx[xyx]xyx")>]
+[<InlineData(true, "aaa[kek]eke")>]
+[<InlineData(true, "zazbz[bzb]cdb")>]
+let ``Day07 isSslSupported works`` expected ip =
+    Assert.Equal(expected, ip |> isSslSupported)
+
 [<Fact>]
 let ``Day07 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("110", job1 ())
+        Assert.Equal("242", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2016/fsharp/Aoc2016/Day07.fs
+++ 2016/fsharp/Aoc2016/Day07.fs
@@ -2,12 +2,60 @@ module Day07
 
 open Utils
 
-let input = readInputLines "07"
+let isAbba a b c d = a = d && b = c && a <> b
+let isAba a b c = a = c && a <> b
+
+let containsAbba string =
+    string
+    |> Seq.windowed 4
+    |> Seq.exists (function
+        | [| a; b; c; d |] when isAbba a b c d -> true
+        | _ -> false)
+
+let findAllAbas string =
+    string
+    |> Seq.windowed 3
+    |> Seq.fold
+        (fun abs ->
+            function
+            | [| a; b; c |] when isAba a b c -> abs |> Set.add (a, b)
+            | _ -> abs)
+        Set.empty
+
+let findAllBabs = findAllAbas
+
+let splitParts (ip: string) =
+    ip.Split([| '['; ']' |])
+    |> Array.chunkBySize 2
+    |> Array.fold
+        (fun (supernetParts, hypernetParts) chunk ->
+            match chunk with
+            | [| supernet; hypernet |] ->
+                supernet :: supernetParts, hypernet :: hypernetParts
+            | [| supernet |] -> supernet :: supernetParts, hypernetParts
+            | _ -> failwith "Internal error")
+        ([], [])
+
+let isTlsSupported ip =
+    let supernetParts, hypernetParts = ip |> splitParts
+
+    supernetParts |> List.exists containsAbba
+    && hypernetParts |> List.forall (containsAbba >> not)
+
+let isSslSupported ip =
+    let supernetParts, hypernetParts = ip |> splitParts
+
+    let abas = supernetParts |> List.map findAllAbas |> Set.unionMany
+    let babs = hypernetParts |> List.map findAllBabs |> Set.unionMany
+
+    Set.intersect abas (babs |> Set.map (fun (b, a) -> a, b))
+    |> Set.isEmpty
+    |> not
+
+let input = readInputLines "07" |> Seq.toArray
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> Seq.filter isTlsSupported |> Seq.length |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> Seq.filter isSslSupported |> Seq.length |> string
