3f14bb3ea424a143b515cf2256e425c32df278c4
Arnold Filip <arnold.filip@gmail.com>
2022-11-16 15:57:21 +0100

    F# 2016-08

--- 2016/fsharp/Aoc2016.Tests/Day08.Tests.fs
+++ 2016/fsharp/Aoc2016.Tests/Day08.Tests.fs
@@ -5,9 +5,49 @@ open Utils
 open Day08
 
 [<Fact>]
+let ``Day08 screen works`` () =
+    let n = 4
+
+    let screens =
+        [ "rect 3x2"
+          "rotate column x=1 by 1"
+          "rotate row y=0 by 4"
+          "rotate column x=1 by 1" ]
+        |> List.take n
+        |> List.scan
+            (fun screen instruction -> screen |> Screen.next instruction)
+            (Screen.empty 7 3)
+        |> List.tail
+
+    let result = screens |> List.map Screen.toString
+    let litCountsResult = screens |> List.map Screen.litCount
+
+    let expected =
+        [ "###....\n###....\n......."
+          "#.#....\n###....\n.#....."
+          "....#.#\n###....\n.#....."
+          ".#..#.#\n#.#....\n.#....." ]
+        |> List.take n
+
+    let litCountsExpected = [ 6; 6; 6; 6 ] |> List.take n
+
+    Assert.Equal<string list>(expected, result)
+    Assert.Equal<int list>(litCountsExpected, litCountsResult)
+
+[<Fact>]
 let ``Day08 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("115", job1 ())
+
+        Assert.Equal(
+            """
+####.####.####.#...##..#.####.###..####..###...##.
+#....#....#....#...##.#..#....#..#.#......#.....#.
+###..###..###...#.#.##...###..#..#.###....#.....#.
+#....#....#......#..#.#..#....###..#......#.....#.
+#....#....#......#..#.#..#....#.#..#......#..#..#.
+####.#....####...#..#..#.#....#..#.#.....###..##..""",
+            job2 ()
+        )
     with :? System.NotImplementedException ->
         ()
--- 2016/fsharp/Aoc2016/Day08.fs
+++ 2016/fsharp/Aoc2016/Day08.fs
@@ -2,12 +2,98 @@ module Day08
 
 open Utils
 
-let input = readInputLines "08"
+type Screen =
+    { Grid: char[][]
+      Width: int
+      Height: int }
+
+module Screen =
+    let empty width height =
+        { Grid =
+            [| for _ in 1..height do
+                   Array.replicate width '.' |]
+          Width = width
+          Height = height }
+
+    let normalize row col screen = row % screen.Height, col % screen.Width
+
+    let copy screen =
+        { screen with Grid = screen.Grid |> Array.map Array.copy }
+
+    let get row col screen =
+        let row, col = normalize row col screen
+        screen.Grid.[row].[col]
+
+    let set value row col screen =
+        let row, col = normalize row col screen
+        screen.Grid.[row].[col] <- value
+        screen
+
+    let setRow value row screen =
+        let row, _ = normalize row 0 screen
+        screen.Grid.[row] <- value
+        screen
+
+    let setCol value col screen =
+        let _, col = normalize 0 col screen
+
+        value
+        |> Array.iteri (fun row char -> screen |> set char row col |> ignore)
+
+        screen
+
+    let rect w h screen =
+        for row in 0 .. h - 1 do
+            for col in 0 .. w - 1 do
+                (set '#' row col screen) |> ignore
+
+        screen
+
+    let rotateRow row by screen =
+        let first = screen.Width - (by % screen.Width)
+        let last = first + screen.Width - 1
+        let row' = [| for col in first..last -> screen |> get row col |]
+        setRow row' row screen
+
+    let rotateCol col by screen =
+        let first = screen.Height - (by % screen.Height)
+        let last = first + screen.Height - 1
+        let col' = [| for row in first..last -> screen |> get row col |]
+        setCol col' col screen
+
+    let toString screen =
+        screen.Grid |> Array.map String.ofChars |> String.join "\n"
+
+    let next instruction screen =
+        match instruction with
+        | Regex @"^rect (\d+)x(\d+)$" [ Int w; Int h ] ->
+            screen |> copy |> rect w h
+        | Regex @"^rotate row y=(\d+) by (\d+)$" [ Int row; Int by ] ->
+            screen |> copy |> rotateRow row by
+        | Regex @"^rotate column x=(\d+) by (\d+)$" [ Int col; Int by ] ->
+            screen |> copy |> rotateCol col by
+        | _ -> failwith $"Invalid instruction: %s{instruction}"
+
+    let litCount screen =
+        screen.Grid
+        |> Array.map (Array.sumBy (fun c -> if c = '#' then 1 else 0))
+        |> Array.sum
+
+
+let input = readInputLines "08" |> Seq.toList
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input
+    |> List.fold
+        (fun screen instruction -> screen |> Screen.next instruction)
+        (Screen.empty 50 6)
+    |> Screen.litCount
+    |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    "\n"
+    + (input
+       |> List.fold
+           (fun screen instruction -> screen |> Screen.next instruction)
+           (Screen.empty 50 6)
+       |> Screen.toString)
