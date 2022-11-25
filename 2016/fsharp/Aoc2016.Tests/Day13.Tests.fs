6025f6d0841c008ebacc5721dfab594e4fcea9fd
Arnold Filip <arnold.filip@gmail.com>
2022-11-25 12:15:56 +0100

    F# 2016-13

--- 2016/fsharp/Aoc2016.Tests/Day13.Tests.fs
+++ 2016/fsharp/Aoc2016.Tests/Day13.Tests.fs
@@ -5,9 +5,75 @@ open Utils
 open Day13
 
 [<Fact>]
+let ``Day13 Area works`` () =
+    let area = Area.empty 10u
+
+    let row y =
+        [| for x in 0u .. 9u -> area |> Area.space (x, y) |> Space.toChar |]
+        |> String.ofChars
+
+    Assert.Equal(".#.####.##", row 0u)
+    Assert.Equal("..#..#...#", row 1u)
+    Assert.Equal("#....##...", row 2u)
+    Assert.Equal("###.#.###.", row 3u)
+    Assert.Equal(".##..#..#.", row 4u)
+    Assert.Equal("..##....#.", row 5u)
+    Assert.Equal("#...##.###", row 6u)
+
+[<Fact>]
+let ``Day13 PriorityQueue works`` () =
+    let q =
+        PriorityQueue.empty
+        |> PriorityQueue.enqueue PriorityQueue.Low "low"
+        |> PriorityQueue.enqueue PriorityQueue.High "high"
+        |> PriorityQueue.enqueue PriorityQueue.Normal "normal"
+        |> PriorityQueue.enqueue PriorityQueue.Low "low2"
+
+    let item, q = q |> PriorityQueue.dequeue
+    Assert.Equal("high", item)
+    let item, q = q |> PriorityQueue.dequeue
+    Assert.Equal("normal", item)
+    let item, q = q |> PriorityQueue.dequeue
+    Assert.Equal("low", item)
+    let item, q = q |> PriorityQueue.dequeue
+    Assert.Equal("low2", item)
+
+[<Fact>]
+let ``Day13 PrioList works`` () =
+    let l =
+        PriorityList.empty
+        |> PriorityList.push PriorityList.Low "low"
+        |> PriorityList.push PriorityList.High "high"
+        |> PriorityList.push PriorityList.Low "low2"
+
+    let item, l = l |> PriorityList.pop
+    Assert.Equal("high", item)
+    let item, l = l |> PriorityList.pop
+    Assert.Equal("low2", item)
+    let item, l = l |> PriorityList.pop
+    Assert.Equal("low", item)
+
+[<Theory>]
+[<InlineData(0u, 1u, 1u)>]
+[<InlineData(1u, 1u, 2u)>]
+[<InlineData(4u, 3u, 1u)>]
+[<InlineData(5u, 4u, 1u)>]
+[<InlineData(11u, 7u, 4u)>]
+let ``Day13 minSteps works`` expected x y =
+    Assert.Equal(expected, Area.empty 10u |> minSteps (1u, 1u) (x, y))
+
+[<Theory>]
+[<InlineData(1u, 0u)>]
+[<InlineData(3u, 1u)>]
+[<InlineData(5u, 2u)>]
+[<InlineData(11u, 5u)>]
+let ``Day13 maxPlaces works`` expected maxSteps =
+    Assert.Equal(expected, Area.empty 10u |> maxPlaces maxSteps (1u, 1u))
+
+[<Fact>]
 let ``Day13 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("90", job1 ())
+        Assert.Equal("135", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2016/fsharp/Aoc2016/Day13.fs
+++ 2016/fsharp/Aoc2016/Day13.fs
@@ -1,13 +1,147 @@
 module Day13
 
+open System.Collections.Generic
+
 open Utils
 
-let input = readInputLines "13"
+let (|Even|Odd|) n =
+    match n % 2u with
+    | 0u -> Even
+    | _ -> Odd
+
+type Pos = uint * uint
+
+type Space =
+    | Wall
+    | Open
+
+module Space =
+    let toChar =
+        function
+        | Wall -> '#'
+        | Open -> '.'
+
+type Area =
+    { MagicNumber: uint
+      Area: Dictionary<Pos, Space> }
+
+module Area =
+    let empty number =
+        { MagicNumber = number
+          Area = Dictionary<Pos, Space>() }
+
+    let private countBits (number: uint) =
+        let rec loop bits number =
+            match number with
+            | 0u -> bits
+            | _ -> loop (bits + (number &&& 1u)) (number >>> 1)
+
+        loop 0u number
+
+    let space pos { MagicNumber = n; Area = area } =
+        match area.TryGetValue(pos) with
+        | true, space -> space
+        | false, _ ->
+            let x, y = pos
+
+            let space =
+                match
+                    (x * x + 3u * x + 2u * x * y + y + y * y + n) |> countBits
+                with
+                | Even -> Open
+                | Odd -> Wall
+
+            area.Add(pos, space)
+            space
+
+    let nextPos (x, y) area =
+        seq {
+            yield x + 1u, y
+
+            if x > 0u then
+                yield x - 1u, y
+
+            yield x, y + 1u
+
+            if y > 0u then
+                yield x, y - 1u
+        }
+        |> Seq.filter (fun pos -> area |> space pos = Open)
+
+
+let keepWalking n pos (steps: Dictionary<Pos, uint>) =
+    match steps.TryGetValue(pos) with
+    | false, _ ->
+        steps.Add(pos, n)
+        true
+    | true, n' when n < n' ->
+        steps.[pos] <- n
+        true
+    | true, _ -> false
+
+let minSteps from' to' area =
+    let x', y' = to'
+
+    let globalStepCount = Dictionary<Pos, uint>()
+
+    let pushHigh = PriorityList.push PriorityList.High
+    let pushLow = PriorityList.push PriorityList.Low
+
+    let dist (x, y) =
+        let diff a b = if a < b then b - a else a - b
+        diff x x' + diff y y'
+
+    let push n pos previous ways =
+        if dist pos > dist previous then
+            pushLow (n, pos) ways
+        else
+            pushHigh (n, pos) ways
+
+    let rec loop result ways =
+        match ways |> PriorityList.tryPop with
+        | None, _ -> result
+        | Some(n, pos), ways ->
+            match keepWalking n pos globalStepCount with
+            | false -> loop result ways
+            | true ->
+                match pos = to' with
+                | true -> loop n ways
+                | false ->
+                    let ways' =
+                        area
+                        |> Area.nextPos pos
+                        |> Seq.filter (fun next -> n + 1u + dist next < result)
+                        |> Seq.fold
+                            (fun ways next -> ways |> push (n + 1u) next pos)
+                            ways
+
+                    loop result ways'
+
+    loop System.UInt32.MaxValue (PriorityList.empty |> pushLow (0u, from'))
+
+let maxPlaces maxSteps from' area =
+    let globalStepCount = Dictionary<Pos, uint>()
+
+    let rec loop ways =
+        match ways with
+        | [] -> globalStepCount.Keys |> Seq.length
+        | (n, pos) :: ways ->
+            match keepWalking n pos globalStepCount && n < maxSteps with
+            | false -> loop ways
+            | true ->
+                let ways' =
+                    area
+                    |> Area.nextPos pos
+                    |> Seq.fold (fun ways next -> (n + 1u, next) :: ways) ways
+
+                loop ways'
+
+    loop [ 0u, from' ]
+
+let input = readInputText "13" |> uint
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    Area.empty input |> minSteps (1u, 1u) (31u, 39u) |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    Area.empty input |> maxPlaces 50u (1u, 1u) |> string
