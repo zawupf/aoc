0449e2f19cde7ef6419f26e2f82f9fd6390d5960
Arnold Filip <arnold.filip@gmail.com>
2022-11-25 21:29:06 +0100

    F# 2016-14

--- 2016/fsharp/Aoc2016.Tests/Day14.Tests.fs
+++ 2016/fsharp/Aoc2016.Tests/Day14.Tests.fs
@@ -4,10 +4,23 @@ open Xunit
 open Utils
 open Day14
 
+[<Theory>]
+[<InlineData(39, 0)>]
+[<InlineData(92, 1)>]
+[<InlineData(22728, 63)>]
+let ``Day14 keys with hashes1 works`` expected index =
+    Assert.Equal(expected, "abc" |> hashes1 |> keys |> Seq.item index |> fst)
+
+[<Theory>]
+[<InlineData(10, 0)>]
+[<InlineData(22551, 63)>]
+let ``Day14 keys with hashes2 works`` expected index =
+    Assert.Equal(expected, "abc" |> hashes2 |> keys |> Seq.item index |> fst)
+
 [<Fact>]
 let ``Day14 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("35186", job1 ())
+        Assert.Equal("22429", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2016/fsharp/Aoc2016/Day14.fs
+++ 2016/fsharp/Aoc2016/Day14.fs
@@ -2,12 +2,51 @@ module Day14
 
 open Utils
 
-let input = readInputLines "14"
+let hashes1 salt =
+    Seq.initInfinite (fun i -> i, Md5.ofString (salt + (string i)))
+
+let hashes2 salt =
+    Seq.initInfinite (fun i ->
+        let hash =
+            { 1..2016 }
+            |> Seq.fold
+                (fun hash _ -> Md5.ofString (hash.ToLowerInvariant()))
+                (Md5.ofString (salt + (string i)))
+
+        i, hash)
+
+let keys (hashes: (int * string) seq) =
+    let tryFindThree string =
+        string
+        |> Seq.windowed 3
+        |> Seq.tryPick (function
+            | [| a; b; c |] ->
+                match a = b && b = c with
+                | true -> Some a
+                | false -> None
+            | _ -> failwith "Invalid chunk")
+
+    seq {
+        for chunk in hashes |> Seq.windowed 1001 do
+            let hash = chunk |> Array.head
+
+            match hash |> snd |> tryFindThree with
+            | Some c ->
+                let s = System.String(c, 5)
+
+                if
+                    chunk
+                    |> Seq.skip 1
+                    |> Seq.exists (fun (_, hash) -> hash.Contains(s))
+                then
+                    yield hash
+            | _ -> ()
+    }
+
+let input = readInputText "14"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> hashes1 |> keys |> Seq.item 63 |> fst |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> hashes2 |> keys |> Seq.item 63 |> fst |> string
