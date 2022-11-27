34184011da2123135c8f13bd3709d7157f22375c
Arnold Filip <arnold.filip@gmail.com>
2022-11-27 16:53:52 +0100

    F# 2016-16

--- 2016/fsharp/Aoc2016.Tests/Day16.Tests.fs
+++ 2016/fsharp/Aoc2016.Tests/Day16.Tests.fs
@@ -4,10 +4,32 @@ open Xunit
 open Utils
 open Day16
 
+[<Theory>]
+[<InlineData("0", "")>]
+[<InlineData("100", "1")>]
+[<InlineData("001", "0")>]
+[<InlineData("11111000000", "11111")>]
+[<InlineData("1111000010100101011110000", "111100001010")>]
+let ``Day16 randomBits works`` (expected: string) salt =
+    Assert.Equal(
+        expected,
+        salt
+        |> randomBits expected.Length (salt |> Seq.length)
+        |> String.ofChars
+    )
+
+[<Theory>]
+[<InlineData("100", "110010110100")>]
+let ``Day16 checksum works`` expected bits =
+    Assert.Equal(
+        expected,
+        bits |> checksum (bits |> Seq.length) |> String.ofChars
+    )
+
 [<Fact>]
 let ``Day16 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("10011010010010010", job1 ())
+        Assert.Equal("10101011110100011", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2016/fsharp/Aoc2016/Day16.fs
+++ 2016/fsharp/Aoc2016/Day16.fs
@@ -2,12 +2,50 @@ module Day16
 
 open Utils
 
-let input = readInputLines "16"
+let randomBits length count chars =
+    let invert chars =
+        chars
+        |> Seq.rev
+        |> Seq.map (function
+            | '0' -> '1'
+            | _ -> '0')
+
+    let rec loop count chars =
+        if length <= count then
+            chars |> Seq.take length
+        else
+            loop
+                (count * 2 + 1)
+                (seq {
+                    yield! chars
+                    yield '0'
+                    yield! chars |> invert
+                })
+
+    loop count chars
+
+let checksum count chars =
+    let calc chars =
+        chars
+        |> Seq.chunkBySize 2
+        |> Seq.map (function
+            | [| a; b |] when a = b -> '1'
+            | _ -> '0')
+
+    let rec loop count chars =
+        match count with
+        | Odd -> chars
+        | Even -> loop (count / 2) (chars |> calc)
+
+    loop (count / 2) (chars |> calc)
+
+let input = readInputText "16"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> randomBits 272 input.Length |> checksum 272 |> String.ofChars
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input
+    |> randomBits 35651584 input.Length
+    |> checksum 35651584
+    |> String.ofChars
