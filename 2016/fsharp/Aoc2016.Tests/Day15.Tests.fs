218dbfc90a6a322ca4412b1e9e83e70986da0ab8
Arnold Filip <arnold.filip@gmail.com>
2022-11-27 14:26:19 +0100

    F# 2016-15

--- 2016/fsharp/Aoc2016.Tests/Day15.Tests.fs
+++ 2016/fsharp/Aoc2016.Tests/Day15.Tests.fs
@@ -4,10 +4,28 @@ open Xunit
 open Utils
 open Day15
 
+[<Theory>]
+[<InlineData(0, 0, 3, 0)>]
+[<InlineData(1, 0, 3, 1)>]
+[<InlineData(2, 0, 3, 2)>]
+[<InlineData(0, 0, 3, 3)>]
+[<InlineData(2, 0, 3, -1)>]
+[<InlineData(2, 0, 3, -10)>]
+let ``Day15 Disc.tick works`` expectedPos pos count seconds =
+    Assert.Equal(
+        Disc(expectedPos, count),
+        Disc(pos, count) |> Disc.tick seconds
+    )
+
+[<Fact>]
+let ``Day15 findDropTime works`` () =
+    let discs = [| Disc(4, 5); Disc(1, 2) |]
+    Assert.Equal(5, discs |> findDropTime)
+
 [<Fact>]
 let ``Day15 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("148737", job1 ())
+        Assert.Equal("2353212", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2016/fsharp/Aoc2016/Day15.fs
+++ 2016/fsharp/Aoc2016/Day15.fs
@@ -2,12 +2,45 @@ module Day15
 
 open Utils
 
+type Disc = Disc of pos: int * count: int
+
+module Disc =
+    let modulo a b =
+        match a % b with
+        | m when m >= 0 -> m
+        | m -> b + m
+
+    let withPos pos (Disc(_, count)) = Disc(modulo pos count, count)
+
+    let tick seconds (Disc(pos, count)) =
+        Disc(modulo (pos + seconds) count, count)
+
+    let parse line =
+        let pattern =
+            @"Disc #(?:\d+) has (\d+) positions; at time=0, it is at position (\d+)."
+
+        match line with
+        | Regex pattern [ Int count; Int pos ] -> Disc(pos, count)
+        | _ -> failwith $"Invalid disc: %s{line}"
+
+let findDropTime discs =
+    let target =
+        discs |> Array.mapi (fun i disc -> disc |> Disc.withPos -(i + 1))
+
+    let rec loop index discs =
+        if discs = target then
+            index
+        else
+            loop (index + 1) (discs |> Array.map (Disc.tick 1))
+
+    loop 0 discs
+
 let input = readInputLines "15"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> Array.map Disc.parse |> findDropTime |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    [| yield! (input |> Array.map Disc.parse); Disc(0, 11) |]
+    |> findDropTime
+    |> string
