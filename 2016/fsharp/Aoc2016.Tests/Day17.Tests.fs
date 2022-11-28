7d48e967ae1d831104a2e3291a695a95d44231a5
Arnold Filip <arnold.filip@gmail.com>
2022-11-28 20:11:57 +0100

    F# 2016-17

--- 2016/fsharp/Aoc2016.Tests/Day17.Tests.fs
+++ 2016/fsharp/Aoc2016.Tests/Day17.Tests.fs
@@ -4,10 +4,22 @@ open Xunit
 open Utils
 open Day17
 
+[<Theory>]
+[<InlineData("DDRRRD", "ihgpwlah")>]
+[<InlineData("DDUDRLRRUDRD", "kglvqrro")>]
+[<InlineData("DRURDRUDDLLDLUURRDULRLDUUDDDRR", "ulqzkmiv")>]
+let ``Day17 walk works`` path salt = Assert.Equal(path, salt |> walk)
+
+[<Theory>]
+[<InlineData(370, "ihgpwlah")>]
+[<InlineData(492, "kglvqrro")>]
+[<InlineData(830, "ulqzkmiv")>]
+let ``Day17 explore works`` path salt = Assert.Equal(path, salt |> explore)
+
 [<Fact>]
 let ``Day17 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("DDRLRRUDDR", job1 ())
+        Assert.Equal("556", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2016/fsharp/Aoc2016/Day17.fs
+++ 2016/fsharp/Aoc2016/Day17.fs
@@ -2,12 +2,67 @@ module Day17
 
 open Utils
 
-let input = readInputLines "17"
+let hash salt steps = Md5.ofString (salt + steps)
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let move direction (x, y) =
+    match direction with
+    | 'U' -> (x, y - 1)
+    | 'D' -> (x, y + 1)
+    | 'L' -> (x - 1, y)
+    | 'R' -> (x + 1, y)
+    | _ -> failwith "Invalid direction"
+    |> function
+        | x, y when x >= 0 && x < 4 && y >= 0 && y < 4 -> Some(x, y)
+        | _ -> None
 
-let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let nextSteps salt path =
+    let pos, steps = path
+
+    hash salt steps
+    |> Seq.take 4
+    |> Seq.zip "UDLR"
+    |> Seq.choose (fun (step, hash) ->
+        if hash < 'B' then
+            None
+        else
+            move step pos |> Option.map (fun pos -> pos, steps + string step))
+
+let walk salt =
+    let rec loop result ways =
+        let max = fst result
+
+        match ways with
+        | [] -> result |> snd |> snd
+        | (length, _) :: ways when length >= max -> loop result ways
+        | (length, (pos, steps)) :: ways when pos = (3, 3) ->
+            loop (length, (pos, steps)) ways
+        | (length, path) :: ways ->
+            let ways' =
+                nextSteps salt path
+                |> Seq.fold (fun ways path -> (length + 1, path) :: ways) ways
+
+            loop result ways'
+
+    loop (System.Int32.MaxValue, ((0, 0), "")) [ 0, ((0, 0), "") ]
+
+let explore salt =
+
+    let rec loop result ways =
+        match ways with
+        | [] -> result
+        | (length, (pos, _)) :: ways when pos = (3, 3) ->
+            loop (max result length) ways
+        | (length, path) :: ways ->
+            let ways' =
+                nextSteps salt path
+                |> Seq.fold (fun ways path -> (length + 1, path) :: ways) ways
+
+            loop result ways'
+
+    loop 0 [ 0, ((0, 0), "") ]
+
+let input = readInputText "17"
+
+let job1 () = input |> walk
+
+let job2 () = input |> explore |> string
