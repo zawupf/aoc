51616eb06aa857359f44574966424fc9bad0fc73
Arnold Filip <arnold.filip@gmail.com>
2022-11-28 22:45:06 +0100

    F# 2016-19

--- 2016/fsharp/Aoc2016.Tests/Day19.Tests.fs
+++ 2016/fsharp/Aoc2016.Tests/Day19.Tests.fs
@@ -5,9 +5,15 @@ open Utils
 open Day19
 
 [<Fact>]
+let ``Day19 play1 works`` () = Assert.Equal(3, play1 5)
+
+[<Fact>]
+let ``Day19 play2 works`` () = Assert.Equal(2, play2 5)
+
+[<Fact>]
 let ``Day19 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("1834471", job1 ())
+        Assert.Equal("1420064", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2016/fsharp/Aoc2016/Day19.fs
+++ 2016/fsharp/Aoc2016/Day19.fs
@@ -2,12 +2,47 @@ module Day19
 
 open Utils
 
-let input = readInputLines "19"
+module Ring =
+    let init n = [| yield! [ 1 .. n - 1 ]; 0 |]
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    let next cursor ring = ring |> Array.item cursor
 
-let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    let removeAtOffset offset cursor (ring: int[]) =
+        assert (offset > 0)
+
+        let rec loop cursor prevCursor offset =
+            match offset with
+            | 0 ->
+                ring.[prevCursor] <- ring.[cursor]
+                prevCursor
+            | _ -> loop (next cursor ring) cursor (offset - 1)
+
+        loop (next cursor ring) cursor (offset - 1)
+
+let play1 n =
+    let ring = Ring.init n
+
+    let rec loop cursor =
+        ring |> Ring.removeAtOffset 1 cursor |> ignore
+
+        match ring |> Ring.next cursor with
+        | cursor' when cursor' = cursor -> cursor + 1
+        | cursor' -> loop cursor'
+
+    loop 0
+
+let play2 n =
+    let ring = Ring.init n
+
+    let rec loop cursor offset =
+        match ring |> Ring.next cursor with
+        | cursor' when cursor' = cursor -> cursor + 1
+        | _ -> loop (ring |> Ring.removeAtOffset offset cursor) (3 - offset)
+
+    loop (n / 2 - 1) (if n % 2 = 0 then 2 else 1)
+
+let input = readInputText "19"
+
+let job1 () = input |> int |> play1 |> string
+
+let job2 () = input |> int |> play2 |> string
