9f8eae2275bb7082edf84ae581535b070342585b
Arnold Filip <arnold.filip@gmail.com>
2022-11-28 20:49:46 +0100

    F# 2016-18

--- 2016/fsharp/Aoc2016.Tests/Day18.Tests.fs
+++ 2016/fsharp/Aoc2016.Tests/Day18.Tests.fs
@@ -4,10 +4,16 @@ open Xunit
 open Utils
 open Day18
 
+[<Theory>]
+[<InlineData(6, "..^^.", 3)>]
+[<InlineData(38, ".^^.^.^^^^", 10)>]
+let ``Day18 countSafeTiles works`` expected row rowCount =
+    Assert.Equal(expected, row |> countSafeTiles rowCount)
+
 [<Fact>]
 let ``Day18 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("2013", job1 ())
+        Assert.Equal("20006289", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2016/fsharp/Aoc2016/Day18.fs
+++ 2016/fsharp/Aoc2016/Day18.fs
@@ -2,12 +2,32 @@ module Day18
 
 open Utils
 
-let input = readInputLines "18"
+let next row =
+    "." + row + "."
+    |> Seq.windowed 3
+    |> Seq.map (function
+        | [| '^'; '^'; '.' |]
+        | [| '.'; '^'; '^' |]
+        | [| '^'; '.'; '.' |]
+        | [| '.'; '.'; '^' |] -> '^'
+        | _ -> '.')
+    |> String.ofChars
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let countSafeTiles rowCount row =
+    let rec loop result rowCount row =
+        match rowCount with
+        | 0 -> result
+        | _ ->
+            loop
+                (result + Seq.sumBy (fun c -> if c = '.' then 1 else 0) row)
+                (rowCount - 1)
+                (next row)
+
+    loop 0 rowCount row
+
+let input = readInputText "18"
+
+let job1 () = input |> countSafeTiles 40 |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> countSafeTiles 400000 |> string
