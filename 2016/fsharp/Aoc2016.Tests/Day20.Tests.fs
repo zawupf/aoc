a385a02a5e17a037c24291492bc661fe867c9d1d
Arnold Filip <arnold.filip@gmail.com>
2022-11-29 20:39:50 +0100

    F# 2016-20

--- 2016/fsharp/Aoc2016.Tests/Day20.Tests.fs
+++ 2016/fsharp/Aoc2016.Tests/Day20.Tests.fs
@@ -5,9 +5,40 @@ open Utils
 open Day20
 
 [<Fact>]
+let ``Day20 findFirstOutOfRange works`` () =
+    Assert.Equal(
+        3u,
+        [ "5-8"; "0-2"; "4-7" ] |> List.map Range.parse |> findFirstOutOfRange
+    )
+
+[<Fact>]
+let ``Day20 tryMerge works`` () =
+    Assert.Equal(Some(1u, 3u), Range.tryMerge (1u, 2u) (2u, 3u))
+    Assert.Equal(Some(1u, 4u), Range.tryMerge (1u, 2u) (3u, 4u))
+    Assert.Equal(None, Range.tryMerge (1u, 2u) (4u, 5u))
+    Assert.Equal(Some(1u, 4u), Range.tryMerge (1u, 4u) (2u, 3u))
+
+[<Fact>]
+let ``Day20 combine works`` () =
+    Assert.Equal(
+        ((1u, 8u), []),
+        Range.combine (1u, 2u) [ (3u, 4u); (5u, 6u); (7u, 8u) ]
+    )
+
+    Assert.Equal(
+        ((1u, 8u), []),
+        Range.combine (1u, 2u) [ (3u, 4u); (7u, 8u); (5u, 6u) ]
+    )
+
+    Assert.Equal(
+        ((1u, 8u), [ 10u, 11u ]),
+        Range.combine (1u, 2u) [ (10u, 11u); (3u, 4u); (7u, 8u); (5u, 6u) ]
+    )
+
+[<Fact>]
 let ``Day20 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("31053880", job1 ())
+        Assert.Equal("117", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2016/fsharp/Aoc2016/Day20.fs
+++ 2016/fsharp/Aoc2016/Day20.fs
@@ -2,12 +2,72 @@ module Day20
 
 open Utils
 
-let input = readInputLines "20"
+type Range = uint * uint
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+module Range =
+    let init a b =
+        assert (a < b)
+        if a < b then a, b else b, a
 
-let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    let length (a: uint32, b: uint32) = uint64 b - uint64 a + 1UL
+
+    let contains value (a, b) = value >= a && value <= b
+
+    let tryMerge (a, b) (c, d) =
+        let range = init (min a c) (max b d)
+        let newLength = length range
+        let oldLength = length (a, b) + length (c, d)
+        if newLength <= oldLength then Some range else None
+
+    let rec combine range ranges =
+        let resultRange, resultRanges =
+            ranges
+            |> List.fold
+                (fun (newRange, rs) r ->
+                    match tryMerge newRange r with
+                    | Some mergedRange -> mergedRange, rs
+                    | None -> newRange, r :: rs)
+                (range, [])
+
+        if range = resultRange && ranges = (List.rev resultRanges) then
+            range, ranges
+        else
+            combine resultRange resultRanges
+
+
+    let parse =
+        function
+        | Regex @"^(\d+)-(\d+)$" [ UInt a; UInt b ] -> init a b
+        | line -> failwith $"Invalid range: %s{line}"
+
+let findFirstOutOfRange ranges =
+    let rec loop value ranges =
+        match ranges with
+        | [] -> value
+        | _ ->
+            match ranges |> List.partition (Range.contains value) with
+            | [], _ -> value
+            | containing, remaining ->
+                let nextValue = containing |> List.maxBy snd |> snd |> (+) 1u
+                loop nextValue remaining
+
+    loop 0u ranges
+
+let allowedIpsCount ranges =
+    let rec loop result ranges =
+        match ranges with
+        | [] -> result
+        | range :: ranges ->
+            let range, ranges = Range.combine range ranges
+            loop (range :: result) ranges
+
+    loop [] ranges
+    |> List.sumBy (Range.length >> uint32)
+    |> (-) System.UInt32.MaxValue
+    |> (+) 1u
+
+let input = readInputLines "20" |> Array.toList |> List.map Range.parse
+
+let job1 () = input |> findFirstOutOfRange |> string
+
+let job2 () = input |> allowedIpsCount |> string
