36a1992ae013794c3c3043e7a9cb6bc7455f5a7f
Arnold Filip <arnold.filip@gmail.com>
2022-11-30 21:58:09 +0100

    F# 2016-21

--- 2016/fsharp/Aoc2016.Tests/Day21.Tests.fs
+++ 2016/fsharp/Aoc2016.Tests/Day21.Tests.fs
@@ -4,10 +4,28 @@ open Xunit
 open Utils
 open Day21
 
+let input =
+    [ "swap position 4 with position 0"
+      "swap letter d with letter b"
+      "reverse positions 0 through 4"
+      "rotate left 1 step"
+      "move position 1 to position 4"
+      "move position 3 to position 0"
+      "rotate based on position of letter b"
+      "rotate based on position of letter d" ]
+
+[<Fact>]
+let ``Day21 scramble works`` () =
+    Assert.Equal("decab", scramble "abcde" input)
+
+[<Fact>]
+let ``Day21 unscramble works`` () =
+    Assert.Equal("abcde", unscramble "decab" input)
+
 [<Fact>]
 let ``Day21 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("dbfgaehc", job1 ())
+        Assert.Equal("aghfcdeb", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2016/fsharp/Aoc2016/Day21.fs
+++ 2016/fsharp/Aoc2016/Day21.fs
@@ -2,12 +2,107 @@ module Day21
 
 open Utils
 
-let input = readInputLines "21"
+let SWAP_POS = @"^swap position (\d+) with position (\d+)$"
+let SWAP_LETTER = @"^swap letter (.) with letter (.)$"
+let ROT_LEFT = @"^rotate left (\d+) steps?$"
+let ROT_RIGHT = @"^rotate right (\d+) steps?$"
+let ROT_POS = @"^rotate based on position of letter (.)$"
+let REV_POS = @"^reverse positions (\d+) through (\d+)$"
+let MOV_POS = @"^move position (\d+) to position (\d+)$"
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let rotateLeft x (chars: char[]) =
+    let i =
+        match x % chars.Length with
+        | i when i < 0 -> chars.Length + i
+        | i -> i
 
-let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    Array.concat [ chars.[i..]; chars.[.. i - 1] ]
+
+let rotateRight x (chars: char[]) = rotateLeft (chars.Length - x) chars
+
+let scramble string instructions =
+    let rec loop (chars: char[]) instructions =
+        match instructions with
+        | [] -> chars |> String.ofChars
+        | instruction :: instructions ->
+            let chars' =
+                match instruction with
+                | Regex SWAP_POS [ Int x; Int y ] ->
+                    let c = chars.[x]
+                    chars.[x] <- chars.[y]
+                    chars.[y] <- c
+                    chars
+                | Regex SWAP_LETTER [ Char x; Char y ] ->
+                    chars
+                    |> Array.map (fun c ->
+                        if c = x then y
+                        else if c = y then x
+                        else c)
+                | Regex ROT_LEFT [ Int x ] -> chars |> rotateLeft x
+                | Regex ROT_RIGHT [ Int x ] -> chars |> rotateRight x
+                | Regex ROT_POS [ Char c ] ->
+                    let i = chars |> Array.findIndex ((=) c)
+                    let x = 1 + i + (if i >= 4 then 1 else 0)
+                    chars |> rotateRight x
+                | Regex REV_POS [ Int x; Int y ] ->
+                    Array.concat
+                        [ chars.[.. x - 1]
+                          chars.[x..y] |> Array.rev
+                          chars.[y + 1 ..] ]
+                | Regex MOV_POS [ Int x; Int y ] ->
+                    let c = chars.[x]
+                    chars |> Array.removeAt x |> Array.insertAt y c
+                | _ -> failwith $"Invalid instruction: %s{instruction}"
+
+            loop chars' instructions
+
+    loop (string |> String.toCharArray) instructions
+
+let unscramble string instructions =
+    let rec loop (chars: char[]) instructions =
+        match instructions with
+        | [] -> chars |> String.ofChars
+        | instruction :: instructions ->
+            let chars' =
+                match instruction with
+                | Regex SWAP_POS [ Int x; Int y ] ->
+                    let c = chars.[x]
+                    chars.[x] <- chars.[y]
+                    chars.[y] <- c
+                    chars
+                | Regex SWAP_LETTER [ Char x; Char y ] ->
+                    chars
+                    |> Array.map (fun c ->
+                        if c = x then y
+                        else if c = y then x
+                        else c)
+                | Regex ROT_LEFT [ Int x ] -> chars |> rotateRight x
+                | Regex ROT_RIGHT [ Int x ] -> chars |> rotateLeft x
+                | Regex ROT_POS [ Char c ] ->
+                    let scramble c chars =
+                        let i = chars |> Array.findIndex ((=) c)
+                        let x = 1 + i + (if i >= 4 then 1 else 0)
+                        chars |> rotateRight x
+                    // FIXME: brute force
+                    { 1 .. chars.Length }
+                    |> Seq.map (fun x -> chars |> rotateLeft x)
+                    |> Seq.find (fun result -> (result |> scramble c) = chars)
+                | Regex REV_POS [ Int x; Int y ] ->
+                    Array.concat
+                        [ chars.[.. x - 1]
+                          chars.[x..y] |> Array.rev
+                          chars.[y + 1 ..] ]
+                | Regex MOV_POS [ Int y; Int x ] ->
+                    let c = chars.[x]
+                    chars |> Array.removeAt x |> Array.insertAt y c
+                | _ -> failwith $"Invalid instruction: %s{instruction}"
+
+            loop chars' instructions
+
+    loop (string |> String.toCharArray) (instructions |> List.rev)
+
+let input = readInputLines "21" |> Array.toList
+
+let job1 () = input |> scramble "abcdefgh"
+
+let job2 () = input |> unscramble "fbgdceah"
