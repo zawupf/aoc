17d4e8c5b18021207e689f802dd5e68e199293b4
Arnold Filip <arnold.filip@gmail.com>
2022-12-01 11:22:21 +0100

    F# 2016-22 (wip)

--- 2016/fsharp/Aoc2016.Tests/Day22.Tests.fs
+++ 2016/fsharp/Aoc2016.Tests/Day22.Tests.fs
@@ -7,7 +7,7 @@ open Day22
 [<Fact>]
 let ``Day22 Stars`` () =
     try
-        Assert.Equal("", job1 ())
+        Assert.Equal("985", job1 ())
         Assert.Equal("", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2016/fsharp/Aoc2016/Day22.fs
+++ 2016/fsharp/Aoc2016/Day22.fs
@@ -2,11 +2,66 @@ module Day22
 
 open Utils
 
+type Node =
+    { X: int
+      Y: int
+      Size: int
+      Used: int }
+
+    member this.Id = this.X, this.Y
+    member this.Avail = this.Size - this.Used
+
+    member this.UsePercent = this.Used * 100 / this.Size
+
+module Node =
+    let isEmpty node = node.Used = 0
+
+    let parse line =
+        let rx =
+            @"^/dev/grid/node-x(\d+)-y(\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T\s+(\d+)\%$"
+
+        match line with
+        | Regex rx [ Int x; Int y; Int size; Int used; Int avail; Int usep ] ->
+            let node =
+                { X = x
+                  Y = y
+                  Size = size
+                  Used = used }
+
+            assert (node.Id = (x, y))
+            assert (node.Avail = avail)
+            assert (node.UsePercent = usep)
+            node
+        | _ -> failwith $"Invalid node: %s{line}"
+
+    let parseInput lines =
+        lines |> Seq.skip 2 |> Seq.map parse |> Seq.toList
+
+let viablePairsCount nodes =
+    let isViable n1 (n2: Node) =
+        n1 |> (Node.isEmpty >> not) && n1.Id <> n2.Id && n1.Used <= n2.Avail
+
+    let rec loop count nodes =
+        match nodes with
+        | [] -> count
+        | node :: nodes ->
+            let count' =
+                nodes
+                |> List.fold
+                    (fun count node2 ->
+                        count
+                        + (if isViable node node2 then 1 else 0)
+                        + (if isViable node2 node then 1 else 0))
+                    count
+
+            loop count' nodes
+
+    loop 0 nodes
+
 let input = readInputLines "22"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> Node.parseInput |> viablePairsCount |> string
 
 let job2 () =
     // input |> String.join "" |> string
