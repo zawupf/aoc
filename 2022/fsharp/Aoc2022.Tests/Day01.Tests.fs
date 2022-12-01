30eb751cbd6587fd48e25e15f0e1eb37a301c8db
Arnold Filip <arnold.filip@gmail.com>
2022-12-01 08:59:13 +0100

    F# 2022-01

--- 2022/fsharp/Aoc2022.Tests/Day01.Tests.fs
+++ 2022/fsharp/Aoc2022.Tests/Day01.Tests.fs
@@ -4,10 +4,40 @@ open Xunit
 open Utils
 open Day01
 
+let input =
+    """
+        1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000
+    """
+    |> String.trim
+    |> String.split '\n'
+    |> Array.map String.trim
+    |> Array.toList
+
+[<Fact>]
+let ``Day01 findMostCalories works`` () =
+    Assert.Equal(24000, input |> parse |> findMostCalories)
+
+[<Fact>]
+let ``Day01 findTop3Calories works`` () =
+    Assert.Equal(45000, input |> parse |> findTop3Calories)
+
 [<Fact>]
 let ``Day01 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("67658", job1 ())
+        Assert.Equal("200158", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2022/fsharp/Aoc2022/Day01.fs
+++ 2022/fsharp/Aoc2022/Day01.fs
@@ -2,12 +2,29 @@ module Day01
 
 open Utils
 
-let input = readInputLines "01"
+let parse lines =
+    let rec loop result current lines =
+        match lines with
+        | [] ->
+            match current with
+            | [] -> result
+            | _ -> current :: result
+        | line :: lines ->
+            match line with
+            | Int n -> loop result (n :: current) lines
+            | _ -> loop (current :: result) [] lines
+
+    loop [] [] lines
+
+let findMostCalories cals = cals |> List.map List.sum |> List.max
+
+let findTop3Calories cals =
+    cals |> List.map List.sum |> List.sortDescending |> List.take 3 |> List.sum
+
+let input = readInputLines "01" |> Array.toList
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> parse |> findMostCalories |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> parse |> findTop3Calories |> string
