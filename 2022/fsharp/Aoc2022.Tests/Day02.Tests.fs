df6dd09aadebb23753c6160d8393a0b5bf5f8b09
Arnold Filip <arnold.filip@gmail.com>
2022-12-02 10:21:36 +0100

    F# 2022-02

--- 2022/fsharp/Aoc2022.Tests/Day02.Tests.fs
+++ 2022/fsharp/Aoc2022.Tests/Day02.Tests.fs
@@ -5,9 +5,17 @@ open Utils
 open Day02
 
 [<Fact>]
+let ``Day02 totalScore1 works`` () =
+    Assert.Equal(15, [| "A Y"; "B X"; "C Z" |] |> totalScore1)
+
+[<Fact>]
+let ``Day02 totalScore2 works`` () =
+    Assert.Equal(12, [| "A Y"; "B X"; "C Z" |] |> totalScore2)
+
+[<Fact>]
 let ``Day02 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("8933", job1 ())
+        Assert.Equal("11998", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2022/fsharp/Aoc2022/Day02.fs
+++ 2022/fsharp/Aoc2022/Day02.fs
@@ -2,12 +2,94 @@ module Day02
 
 open Utils
 
+type Shape =
+    | Rock
+    | Paper
+    | Scissors
+
+module Shape =
+    let parse =
+        function
+        | 'A'
+        | 'X' -> Rock
+        | 'B'
+        | 'Y' -> Paper
+        | 'C'
+        | 'Z' -> Scissors
+        | c -> failwith $"Invalid shape: %c{c}"
+
+type Winner =
+    | Me
+    | Opponent
+    | Draw
+
+module Winner =
+    let parse =
+        function
+        | 'X' -> Opponent
+        | 'Y' -> Draw
+        | 'Z' -> Me
+        | c -> failwith $"Invalid winning result: %c{c}"
+
+let winner me opponent =
+    match me, opponent with
+    | myShape, oppShape when myShape = oppShape -> Draw
+    | Rock, Scissors
+    | Scissors, Paper
+    | Paper, Rock -> Me
+    | _ -> Opponent
+
+let score (myResult, myShape) =
+    let scoreShape =
+        match myShape with
+        | Rock -> 1
+        | Paper -> 2
+        | Scissors -> 3
+
+    let winningScore =
+        match myResult with
+        | Opponent -> 0
+        | Draw -> 3
+        | Me -> 6
+
+    scoreShape + winningScore
+
+let score1 (me, opponent) = score ((winner me opponent), me)
+
+let parse1 input =
+    match input with
+    | Regex @"^(.) (.)$" [ Char opp; Char me ] ->
+        Shape.parse me, Shape.parse opp
+    | _ -> failwith $"Invalid input: %s{input}"
+
+let parse2 input =
+    let chooseShapeTo winner oppShape =
+        match winner, oppShape with
+        | Draw, _ -> oppShape
+        | Me, Rock -> Paper
+        | Me, Paper -> Scissors
+        | Me, Scissors -> Rock
+        | Opponent, Rock -> Scissors
+        | Opponent, Paper -> Rock
+        | Opponent, Scissors -> Paper
+
+    match input with
+    | Regex @"^(.) (.)$" [ Char opp; Char result ] ->
+        let oppShape = Shape.parse opp
+        let myResult = Winner.parse result
+        let myShape = chooseShapeTo myResult oppShape
+        myResult, myShape
+    | _ -> failwith $"Invalid input: %s{input}"
+
+let totalScore mapping input = input |> Seq.map mapping |> Seq.sum
+
+let totalScore1 = totalScore (parse1 >> score1)
+
+let totalScore2 = totalScore (parse2 >> score)
+
+
 let input = readInputLines "02"
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let job1 () = input |> totalScore1 |> string
 
-let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let job2 () = input |> totalScore2 |> string
