9def23d71e7ff80518218b884249c169814e3f80
Arnold Filip <arnold.filip@gmail.com>
2022-12-03 20:46:53 +0100

    F# 2022-03

--- 2022/fsharp/Aoc2022.Tests/Day03.Tests.fs
+++ 2022/fsharp/Aoc2022.Tests/Day03.Tests.fs
@@ -4,10 +4,26 @@ open Xunit
 open Utils
 open Day03
 
+let input =
+    [| "vJrwpWtwJgWrhcsFMMfFFhFp"
+       "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
+       "PmmdzqPrVvPwwTWBwg"
+       "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
+       "ttgJtRGJQctTZtZT"
+       "CrZsJsPPZsGzwwsLwLmpwMDw" |]
+
+[<Fact>]
+let ``Day03 sumOfDoubleItemPrios works`` () =
+    Assert.Equal(157, input |> sumOfDoubleItemPrios)
+
+[<Fact>]
+let ``Day03 sumOfBadgeItemPrios works`` () =
+    Assert.Equal(70, input |> sumOfBadgeItemPrios)
+
 [<Fact>]
 let ``Day03 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("8109", job1 ())
+        Assert.Equal("2738", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2022/fsharp/Aoc2022/Day03.fs
+++ 2022/fsharp/Aoc2022/Day03.fs
@@ -2,12 +2,29 @@ module Day03
 
 open Utils
 
+let findDoubleItem rucksack =
+    rucksack
+    |> Seq.splitInto 2
+    |> Seq.map Set.ofSeq
+    |> Set.intersectMany
+    |> Seq.exactlyOne
+
+let findBadgeItem rucksacks =
+    rucksacks |> Seq.map Set.ofSeq |> Set.intersectMany |> Seq.exactlyOne
+
+let priority c =
+    [| yield! [| 'a' .. 'z' |]; yield! [| 'A' .. 'Z' |] |]
+    |> Array.findIndex ((=) c)
+    |> (+) 1
+
+let sumOfDoubleItemPrios rucksacks =
+    rucksacks |> Array.sumBy (findDoubleItem >> priority)
+
+let sumOfBadgeItemPrios rucksacks =
+    rucksacks |> Array.chunkBySize 3 |> Array.sumBy (findBadgeItem >> priority)
+
 let input = readInputLines "03"
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let job1 () = input |> sumOfDoubleItemPrios |> string
 
-let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let job2 () = input |> sumOfBadgeItemPrios |> string
