0cff2192b1c1c57a96a89de67d1a4cad997b7006
Arnold Filip <arnold.filip@gmail.com>
2022-12-04 09:05:05 +0100

    F# 2022-04

--- 2022/fsharp/Aoc2022.Tests/Day04.Tests.fs
+++ 2022/fsharp/Aoc2022.Tests/Day04.Tests.fs
@@ -4,10 +4,32 @@ open Xunit
 open Utils
 open Day04
 
+let input =
+    """
+    2-4,6-8
+    2-3,4-5
+    5-7,7-9
+    2-8,3-7
+    6-6,4-6
+    2-6,4-8
+    """
+    |> String.trim
+    |> String.split '\n'
+    |> Array.map (String.trim >> Range.parsePair)
+    |> Array.toList
+
+[<Fact>]
+let ``Day04 countFullyContainedPairs works`` () =
+    Assert.Equal(2, input |> countFullyContainedPairs)
+
+[<Fact>]
+let ``Day04 countOverlappingPairs works`` () =
+    Assert.Equal(4, input |> countOverlappingPairs)
+
 [<Fact>]
 let ``Day04 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("305", job1 ())
+        Assert.Equal("811", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2022/fsharp/Aoc2022/Day04.fs
+++ 2022/fsharp/Aoc2022/Day04.fs
@@ -2,12 +2,44 @@ module Day04
 
 open Utils
 
-let input = readInputLines "04"
+type Range = int * int
+
+module Range =
+    let init a b = if a < b then (a, b) else (b, a)
+
+    let length (a, b) = b - a + 1
+
+    let isOneFullyContained (a, b) (c, d) = c >= a && d <= b || a >= c && b <= d
+
+    let isOverlapping (a, b) (c, d) = c >= a && c <= b || a >= c && a <= d
+
+    let parse =
+        function
+        | Regex @"^(\d+)-(\d+)$" [ Int a; Int b ] -> init a b
+        | range -> failwith $"Invalid range: %s{range}"
+
+    let parsePair line =
+        line
+        |> String.split ','
+        |> Array.map parse
+        |> function
+            | [| range1; range2 |] -> range1, range2
+            | _ -> failwith $"Invalid range pair: %s{line}"
+
+let countFullyContainedPairs pairs =
+    pairs
+    |> List.filter (fun pair -> pair ||> Range.isOneFullyContained)
+    |> List.length
+
+let countOverlappingPairs pairs =
+    pairs
+    |> List.filter (fun pair -> pair ||> Range.isOverlapping)
+    |> List.length
+
+let input = readInputLines "04" |> Seq.map Range.parsePair |> Seq.toList
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> countFullyContainedPairs |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> countOverlappingPairs |> string
