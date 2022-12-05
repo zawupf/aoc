12bce28a80206f38860cc7a89573ee597bc3e295
Arnold Filip <arnold.filip@gmail.com>
2022-12-05 09:46:45 +0100

    F# 2022-05

--- 2022/fsharp/Aoc2022.Tests/Day05.Tests.fs
+++ 2022/fsharp/Aoc2022.Tests/Day05.Tests.fs
@@ -4,10 +4,29 @@ open Xunit
 open Utils
 open Day05
 
+let moves =
+    [ "move 1 from 2 to 1"
+      "move 3 from 1 to 3"
+      "move 2 from 2 to 1"
+      "move 1 from 1 to 2" ]
+
+let stacks =
+    [| "NZ" |> String.toCharArray |> Array.toList
+       "DCM" |> String.toCharArray |> Array.toList
+       "P" |> String.toCharArray |> Array.toList |]
+
+[<Fact>]
+let ``Day05 topCrates 1 works`` () =
+    Assert.Equal("CMZ", stacks |> move craneOneByOne moves |> topCrates)
+
+[<Fact>]
+let ``Day05 topCrates 2 works`` () =
+    Assert.Equal("MCD", stacks |> move craneAllAtOnce moves |> topCrates)
+
 [<Fact>]
 let ``Day05 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("GFTNRBZPF", job1 ())
+        Assert.Equal("VRQWPDSGP", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2022/fsharp/Aoc2022/Day05.fs
+++ 2022/fsharp/Aoc2022/Day05.fs
@@ -2,12 +2,78 @@ module Day05
 
 open Utils
 
-let input = readInputLines "05"
+type Crane =
+    | Reverse
+    | KeepOrder
+
+let crane order n i j stacks =
+    let movedCrates, remainingCrates = stacks |> Array.item i |> List.splitAt n
+
+    let crates =
+        match order with
+        | Reverse -> movedCrates |> List.rev
+        | KeepOrder -> movedCrates
+
+    stacks[i] <- remainingCrates
+    stacks[j] <- crates @ stacks[j]
+    stacks
+
+let craneOneByOne = crane Reverse
+
+let craneAllAtOnce = crane KeepOrder
+
+let move crane moves stacks =
+    let rec loop stacks moves =
+        match moves with
+        | [] -> stacks
+        | move :: moves ->
+            match move with
+            | Regex @"^move (\d+) from (\d+) to (\d+)$" [ Int n; Int i; Int j ] ->
+                loop (crane n (i - 1) (j - 1) stacks) moves
+            | _ -> failwith $"Invalid move: %s{move}"
+
+    loop (stacks |> Array.copy) moves
+
+let topCrates (stacks: char list array) =
+    stacks |> Array.map List.head |> String.ofChars
+
+let parse (input: string) =
+    match input.Split("\n\n") with
+    | [| stackInput; movesInput |] ->
+        let stackInput = stackInput |> String.split '\n' |> Array.rev
+
+        let indexes =
+            stackInput
+            |> Array.head
+            |> Seq.indexed
+            |> Seq.fold
+                (fun indexes (i, c) ->
+                    match c with
+                    | ' ' -> indexes
+                    | _ -> i :: indexes)
+                []
+            |> Seq.rev
+            |> Seq.toArray
+
+        let stacks = Array.replicate indexes.Length []
+
+        stackInput
+        |> Array.tail
+        |> Array.iter (fun line ->
+            indexes
+            |> Array.iteri (fun iStack iChar ->
+                if iChar < line.Length && line[iChar] <> ' ' then
+                    stacks[iStack] <- line[iChar] :: stacks[iStack]))
+
+        stacks, movesInput |> String.split '\n' |> Array.toList
+    | _ -> failwith "Invalid input"
+
+let input = readInputText "05"
+
+let stacks, moves = input |> parse
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    stacks |> move craneOneByOne moves |> topCrates
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    stacks |> move craneAllAtOnce moves |> topCrates
