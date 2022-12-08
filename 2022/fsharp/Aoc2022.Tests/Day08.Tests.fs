65b54d5d7f142e48722731971652cb5ac9ac82cb
Arnold Filip <arnold.filip@gmail.com>
2022-12-08 12:48:26 +0100

    F# 2022-08

--- 2022/fsharp/Aoc2022.Tests/Day08.Tests.fs
+++ 2022/fsharp/Aoc2022.Tests/Day08.Tests.fs
@@ -4,10 +4,31 @@ open Xunit
 open Utils
 open Day08
 
+let input =
+    """
+    30373
+    25512
+    65332
+    33549
+    35390
+    """
+    |> String.trim
+    |> String.split '\n'
+    |> Array.map String.trim
+    |> Grid.parse
+
+[<Fact>]
+let ``Day08 countVisibleTrees works`` () =
+    Assert.Equal(21, input |> countVisibleTrees)
+
+[<Fact>]
+let ``Day08 maxScenicScore works`` () =
+    Assert.Equal(8, input |> maxScenicScore)
+
 [<Fact>]
 let ``Day08 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("1789", job1 ())
+        Assert.Equal("314820", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2022/fsharp/Aoc2022/Day08.fs
+++ 2022/fsharp/Aoc2022/Day08.fs
@@ -2,12 +2,91 @@ module Day08
 
 open Utils
 
-let input = readInputLines "08"
+type Grid = int[,]
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+module Grid =
+    let parse lines =
+        lines
+        |> Array.map (
+            String.toByteArray >> Array.map (fun b -> int b - int '0')
+        )
+        |> array2D
 
-let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    let rowCount (grid: Grid) = grid |> Array2D.length1
+
+    let columnCount (grid: Grid) = grid |> Array2D.length2
+
+let countVisibleTrees (grid: Grid) =
+    let w, h = Grid.columnCount grid, Grid.rowCount grid
+
+    let rec lookAt index line height result =
+        match line |> Array.length = index with
+        | true -> result
+        | false ->
+            match line[index] with
+            | h when h <= height -> lookAt (index + 1) line height result
+            | h -> lookAt (index + 1) line h (index :: result)
+
+    let handle line mapIndex set =
+        lookAt 1 line line[0] []
+        |> List.fold (fun set i -> set |> Set.add (mapIndex i)) set
+
+    let handleRow set rowIndex =
+        let row = grid[rowIndex, *]
+
+        set
+        |> handle row (fun i -> rowIndex, i)
+        |> handle (row |> Array.rev) (fun i -> rowIndex, w - 1 - i)
+
+    let handleRows set =
+        [ 1 .. h - 2 ] |> List.fold handleRow set
+
+    let handleCol set colIndex =
+        let col = grid[*, colIndex]
+
+        set
+        |> handle col (fun i -> i, colIndex)
+        |> handle (col |> Array.rev) (fun i -> h - 1 - i, colIndex)
+
+    let handleCols set =
+        [ 1 .. w - 2 ] |> List.fold handleCol set
+
+    seq {
+        for col in 0 .. w - 1 -> (0, col)
+        for col in 0 .. w - 1 -> (h - 1, col)
+        for row in 0 .. h - 1 -> (row, 0)
+        for row in 0 .. h - 1 -> (row, w - 1)
+    }
+    |> Set.ofSeq
+    |> handleRows
+    |> handleCols
+    |> Set.count
+
+let maxScenicScore (grid: Grid) =
+    let w, h = Grid.columnCount grid, Grid.rowCount grid
+
+    let scenicScore (irow, icol) =
+        let height = grid[irow, icol]
+
+        [ grid[irow, icol + 1 ..]
+          grid[irow, .. icol - 1] |> Array.rev
+          grid[irow + 1 .., icol]
+          grid[.. irow - 1, icol] |> Array.rev ]
+        |> List.map (fun line ->
+            match line |> Array.tryFindIndex (fun h -> h >= height) with
+            | Some i -> i + 1
+            | None -> line |> Array.length)
+        |> List.fold (fun score dist -> score * dist) 1
+
+    seq {
+        for row in 1 .. h - 2 do
+            for col in 1 .. w - 2 -> row, col
+    }
+    |> Seq.map scenicScore
+    |> Seq.max
+
+let input = readInputLines "08" |> Grid.parse
+
+let job1 () = input |> countVisibleTrees |> string
+
+let job2 () = input |> maxScenicScore |> string
