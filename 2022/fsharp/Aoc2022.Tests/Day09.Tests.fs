72c507e1d3f1027525d83efb2e2e12e5fd1ea19f
Arnold Filip <arnold.filip@gmail.com>
2022-12-09 10:05:01 +0100

    F# 2022-09

--- 2022/fsharp/Aoc2022.Tests/Day09.Tests.fs
+++ 2022/fsharp/Aoc2022.Tests/Day09.Tests.fs
@@ -4,10 +4,42 @@ open Xunit
 open Utils
 open Day09
 
+let input =
+    """
+    R 4
+    U 4
+    L 3
+    D 1
+    R 4
+    D 1
+    L 5
+    R 2
+    """
+    |> String.toLines
+
+let input2 =
+    """
+    R 5
+    U 8
+    L 8
+    D 3
+    R 17
+    D 10
+    L 25
+    U 20
+    """
+    |> String.toLines
+
+[<Fact>]
+let ``Day09 countTailPositions works`` () =
+    Assert.Equal(13, input |> countTailPositions 2)
+    Assert.Equal(1, input |> countTailPositions 10)
+    Assert.Equal(36, input2 |> countTailPositions 10)
+
 [<Fact>]
 let ``Day09 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("6098", job1 ())
+        Assert.Equal("2597", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2022/fsharp/Aoc2022/Day09.fs
+++ 2022/fsharp/Aoc2022/Day09.fs
@@ -2,12 +2,69 @@ module Day09
 
 open Utils
 
+type Pos = int * int
+type Rope = Pos list
+
+module Rope =
+    let init length = List.replicate length (0, 0)
+
+    let head = List.head
+
+    let tail = List.last
+
+    let step dir rope =
+        let moveHead (hx, hy) =
+            match dir with
+            | 'U' -> hx, hy + 1
+            | 'D' -> hx, hy - 1
+            | 'R' -> hx + 1, hy
+            | 'L' -> hx - 1, hy
+            | _ -> failwith $"Invalid direction: %c{dir}"
+
+        let rec moveTails moved tails =
+            match tails with
+            | [] -> moved |> List.rev
+            | (tx, ty) :: tails ->
+                let hx, hy = moved |> List.head
+                let dx, dy = hx - tx, hy - ty
+
+                let dtx, dty =
+                    if abs dx > 1 || abs dy > 1 then sign dx, sign dy else 0, 0
+
+                let tail = tx + dtx, ty + dty
+                moveTails (tail :: moved) tails
+
+        moveTails [ rope |> head |> moveHead ] (rope |> List.tail)
+
+    let move steps rope =
+        seq {
+            yield rope
+
+            let mutable r = rope
+
+            for step' in steps do
+                match step' with
+                | Regex @"^(.) (\d+)$" [ Char dir; Int count ] ->
+                    for _ in 1..count do
+                        r <- r |> step dir
+                        yield r
+                | _ -> failwith $"Invalid step: %s{step'}"
+        }
+
+let countTailPositions n steps =
+    let rope = Rope.init n
+
+    rope
+    |> Rope.move steps
+    |> Seq.fold
+        (fun tailPositions rope -> tailPositions |> Set.add (rope |> Rope.tail))
+        Set.empty
+    |> Set.count
+
+
 let input = readInputLines "09"
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let job1 () = input |> countTailPositions 2 |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> countTailPositions 10 |> string
