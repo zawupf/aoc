26cf041e870a0c3a83b757d7747dec72967de1e8
Arnold Filip <arnold.filip@gmail.com>
2022-12-10 08:51:12 +0100

    F# 2022-10

--- 2022/fsharp/Aoc2022.Tests/Day10.Tests.fs
+++ 2022/fsharp/Aoc2022.Tests/Day10.Tests.fs
@@ -4,10 +4,200 @@ open Xunit
 open Utils
 open Day10
 
+let input =
+    """
+    addx 15
+    addx -11
+    addx 6
+    addx -3
+    addx 5
+    addx -1
+    addx -8
+    addx 13
+    addx 4
+    noop
+    addx -1
+    addx 5
+    addx -1
+    addx 5
+    addx -1
+    addx 5
+    addx -1
+    addx 5
+    addx -1
+    addx -35
+    addx 1
+    addx 24
+    addx -19
+    addx 1
+    addx 16
+    addx -11
+    noop
+    noop
+    addx 21
+    addx -15
+    noop
+    noop
+    addx -3
+    addx 9
+    addx 1
+    addx -3
+    addx 8
+    addx 1
+    addx 5
+    noop
+    noop
+    noop
+    noop
+    noop
+    addx -36
+    noop
+    addx 1
+    addx 7
+    noop
+    noop
+    noop
+    addx 2
+    addx 6
+    noop
+    noop
+    noop
+    noop
+    noop
+    addx 1
+    noop
+    noop
+    addx 7
+    addx 1
+    noop
+    addx -13
+    addx 13
+    addx 7
+    noop
+    addx 1
+    addx -33
+    noop
+    noop
+    noop
+    addx 2
+    noop
+    noop
+    noop
+    addx 8
+    noop
+    addx -1
+    addx 2
+    addx 1
+    noop
+    addx 17
+    addx -9
+    addx 1
+    addx 1
+    addx -3
+    addx 11
+    noop
+    noop
+    addx 1
+    noop
+    addx 1
+    noop
+    noop
+    addx -13
+    addx -19
+    addx 1
+    addx 3
+    addx 26
+    addx -30
+    addx 12
+    addx -1
+    addx 3
+    addx 1
+    noop
+    noop
+    noop
+    addx -9
+    addx 18
+    addx 1
+    addx 2
+    noop
+    noop
+    addx 9
+    noop
+    noop
+    noop
+    addx -1
+    addx 2
+    addx -37
+    addx 1
+    addx 3
+    noop
+    addx 15
+    addx -21
+    addx 22
+    addx -6
+    addx 1
+    noop
+    addx 2
+    addx 1
+    noop
+    addx -10
+    noop
+    noop
+    addx 20
+    addx 1
+    addx 2
+    addx 2
+    addx -6
+    addx -11
+    noop
+    noop
+    noop
+    """
+    |> String.toLines
+
+let output =
+    """
+    ##..##..##..##..##..##..##..##..##..##..
+    ###...###...###...###...###...###...###.
+    ####....####....####....####....####....
+    #####.....#####.....#####.....#####.....
+    ######......######......######......####
+    #######.......#######.......#######.....
+    """
+    |> String.toLines
+
+[<Fact>]
+let ``Day10 cycles works`` () =
+    Assert.Equal<int list>(
+        [ 1; 1; 1; 4; 4 ],
+        "noop\naddx 3\naddx -5" |> String.toLines |> cycles |> Seq.toList
+    )
+
+[<Fact>]
+let ``Day10 signalStrengthSum works`` () =
+    Assert.Equal(13140, input |> cycles |> signalStrengthSum)
+
+[<Fact>]
+let ``Day10 render works`` () =
+    Assert.Equal<string[]>(output, input |> cycles |> render)
+
 [<Fact>]
 let ``Day10 Stars`` () =
+    let result2 =
+        """
+        ###...##..#..#.####..##..#....#..#..##..
+        #..#.#..#.#..#.#....#..#.#....#..#.#..#.
+        #..#.#....####.###..#....#....#..#.#....
+        ###..#.##.#..#.#....#.##.#....#..#.#.##.
+        #....#..#.#..#.#....#..#.#....#..#.#..#.
+        #.....###.#..#.#.....###.####..##...###.
+        """
+        |> String.toLines
+        |> Array.append [| "" |]
+        |> String.join "\n"
+
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("15260", job1 ())
+        Assert.Equal(result2, job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2022/fsharp/Aoc2022/Day10.fs
+++ 2022/fsharp/Aoc2022/Day10.fs
@@ -2,12 +2,46 @@ module Day10
 
 open Utils
 
+let cycles input =
+    seq {
+        let mutable x = 1
+
+        for line in input do
+            match line with
+            | Regex @"^noop$" [] -> yield x
+            | Regex @"^addx (-?\d+)$" [ Int dx ] ->
+                yield x
+                yield x
+                x <- x + dx
+            | _ -> failwith $"Invalid instruction: %s{line}"
+    }
+
+let signalStrengthSum cycles =
+    let signals = cycles |> Seq.toArray
+
+    [ 20; 60; 100; 140; 180; 220 ]
+    |> Seq.map (fun i -> signals[i - 1] * i)
+    |> Seq.sum
+
+let render cycles =
+    cycles
+    |> Seq.indexed
+    |> Seq.map (fun (i, signal) ->
+        let index = i % 40
+        let spriteLeft, spriteRight = (signal - 1, signal + 1)
+
+        if index >= spriteLeft && index <= spriteRight then
+            '#'
+        else
+            '.')
+    |> Seq.chunkBySize 40
+    |> Seq.map String.ofChars
+    |> Seq.toArray
+
 let input = readInputLines "10"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> cycles |> signalStrengthSum |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    sprintf "\n%s" (input |> cycles |> render |> String.join "\n")
