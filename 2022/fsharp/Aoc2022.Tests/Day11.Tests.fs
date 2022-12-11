49bdf31b744607005f490ca9c96ec179eccbe3c9
Arnold Filip <arnold.filip@gmail.com>
2022-12-11 14:55:07 +0100

    F# 2022-11

--- 2022/fsharp/Aoc2022.Tests/Day11.Tests.fs
+++ 2022/fsharp/Aoc2022.Tests/Day11.Tests.fs
@@ -4,10 +4,53 @@ open Xunit
 open Utils
 open Day11
 
+let input =
+    """
+Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
+"""
+
+[<Fact>]
+let ``Day11 monkeyBusiness works`` () =
+    Assert.Equal(
+        10605UL,
+        input |> Monkey.parseMany |> monkeyBusiness WithDiv 20
+    )
+
+    Assert.Equal(
+        2713310158UL,
+        input |> Monkey.parseMany |> monkeyBusiness WithoutDiv 10000
+    )
+
 [<Fact>]
 let ``Day11 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("57838", job1 ())
+        Assert.Equal("15050382231", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2022/fsharp/Aoc2022/Day11.fs
+++ 2022/fsharp/Aoc2022/Day11.fs
@@ -2,12 +2,119 @@ module Day11
 
 open Utils
 
-let input = readInputLines "11"
+type Operation =
+    | Add of uint64
+    | Mul of uint64
+    | Sq
+
+type Mode =
+    | WithDiv
+    | WithoutDiv
+
+type Monkey =
+    { items: uint64 list
+      operation: Operation
+      test: uint64
+      onTrueFalse: int * int }
+
+module Monkey =
+    let parse input =
+        let lines = input |> String.toLines
+
+        let items =
+            match lines[1] with
+            | Regex @"^Starting items: ([\d, ]+)$" [ items ] ->
+                items
+                |> String.split ','
+                |> Array.map (String.trim >> uint64)
+                |> Array.toList
+            | line -> failwith $"Invalid input[1]: %s{line}"
+
+        let operation =
+            match lines[2] with
+            | Regex @"^Operation: new = old (\+|\*) (\d+|old)$" [ op; value ] ->
+                match op, value with
+                | "+", UInt64 v -> Add v
+                | "*", UInt64 v -> Mul v
+                | "*", "old" -> Sq
+                | _ -> failwith $"Invalid op/value: %s{op}/%s{value}"
+            | line -> failwith $"Invalid input[2]: %s{line}"
+
+        let test =
+            match lines[3] with
+            | Regex @"^Test: divisible by (\d+)$" [ UInt64 d ] -> d
+            | line -> failwith $"Invalid input[3]: %s{line}"
+
+        let onTrue =
+            match lines[4] with
+            | Regex @"^If true: throw to monkey (\d+)$" [ Int i ] -> i
+            | line -> failwith $"Invalid input[4]: %s{line}"
+
+        let onFalse =
+            match lines[5] with
+            | Regex @"^If false: throw to monkey (\d+)$" [ Int i ] -> i
+            | line -> failwith $"Invalid input[5]: %s{line}"
+
+        { items = items
+          operation = operation
+          test = test
+          onTrueFalse = onTrue, onFalse }
+
+    let parseMany input =
+        (input |> String.trim).Split("\n\n") |> Array.map parse
+
+    let playMonkey magicNumber mode index (monkeys: Monkey[]) =
+        let monkey = monkeys[index]
+
+        monkey.items
+        |> List.iter (fun item ->
+            let worryLevel =
+                match mode with
+                | WithDiv ->
+                    match monkey.operation with
+                    | Add v -> (item + v) / 3UL
+                    | Mul v -> (item * v) / 3UL
+                    | Sq -> (item * item) / 3UL
+                | WithoutDiv ->
+                    match monkey.operation with
+                    | Add v -> item + v
+                    | Mul v -> item * v
+                    | Sq -> item * item
+
+            let target =
+                monkey.onTrueFalse
+                |> match worryLevel % monkey.test with
+                   | 0UL -> fst
+                   | _ -> snd
+
+            monkeys[target] <-
+                { monkeys[target] with
+                    items =
+                        monkeys[target].items @ [ worryLevel % magicNumber ] })
+
+        monkeys[index] <- { monkey with items = [] }
+
+let monkeyBusiness mode rounds (monkeys: Monkey[]) =
+    let magicNumber = monkeys |> Array.map (fun m -> m.test) |> Array.reduce (*)
+
+    [ 0 .. rounds * monkeys.Length - 1 ]
+    |> List.fold
+        (fun (acc: _[]) i ->
+            let index = i % monkeys.Length
+
+            acc[index] <- acc[index] + uint64 monkeys[index].items.Length
+
+            Monkey.playMonkey magicNumber mode index monkeys
+            acc)
+        (monkeys |> Array.map (fun _ -> 0UL))
+    |> Array.sortDescending
+    |> Array.take 2
+    |> Array.reduce (*)
+
+let input = readInputText "11"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> Monkey.parseMany |> monkeyBusiness WithDiv 20 |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> Monkey.parseMany |> monkeyBusiness WithoutDiv 10000 |> string
