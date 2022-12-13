c672ccac78fd0006e6a3ffc642134c1de7f238c3
Arnold Filip <arnold.filip@gmail.com>
2022-12-13 12:07:54 +0100

    F# 2022-13

--- 2022/fsharp/Aoc2022.Tests/Day13.Tests.fs
+++ 2022/fsharp/Aoc2022.Tests/Day13.Tests.fs
@@ -4,10 +4,57 @@ open Xunit
 open Utils
 open Day13
 
+let input =
+    """
+    [1,1,3,1,1]
+    [1,1,5,1,1]
+
+    [[1],[2,3,4]]
+    [[1],4]
+
+    [9]
+    [[8,7,6]]
+
+    [[4,4],4,4]
+    [[4,4],4,4,4]
+
+    [7,7,7,7]
+    [7,7,7]
+
+    []
+    [3]
+
+    [[[]]]
+    [[]]
+
+    [1,[2,[3,[4,[5,6,7]]]],8,9]
+    [1,[2,[3,[4,[5,6,0]]]],8,9]
+    """
+    |> String.toLines
+    |> String.concat "\n"
+
+[<Theory>]
+[<InlineData("[]", "[]")>]
+[<InlineData("[1]", "[1]")>]
+[<InlineData("[1,[12,3]]", "[1,[12,3]]")>]
+[<InlineData("[[12,3],4]", "[[12,3],4]")>]
+[<InlineData("[[12,3],[4,56]]", "[[12,3],[4,56]]")>]
+[<InlineData("[[12,3],99,[4,56]]", "[[12,3],99,[4,56]]")>]
+let ``Day13 parse works`` expected input =
+    Assert.Equal(expected, input |> Data.parse |> Data.dump)
+
+[<Fact>]
+let ``Day13 part1 works`` () =
+    Assert.Equal(13, input |> Data.parsePairs |> part1)
+
+[<Fact>]
+let ``Day13 part2 works`` () =
+    Assert.Equal(140, input |> Data.parsePairs |> part2)
+
 [<Fact>]
 let ``Day13 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("5208", job1 ())
+        Assert.Equal("25792", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2022/fsharp/Aoc2022/Day13.fs
+++ 2022/fsharp/Aoc2022/Day13.fs
@@ -2,12 +2,129 @@ module Day13
 
 open Utils
 
-let input = readInputLines "13"
+type CompareResult =
+    | Valid
+    | Invalid
+    | Undetermined
+
+type Data =
+    | Number of int
+    | Packet of Data list
+
+module Data =
+    let parse line =
+        let rec loop stack i =
+            match String.length line = i with
+            | true ->
+                match stack with
+                | [ data ] -> data
+                | _ -> failwith "Error on parse end"
+            | false ->
+                match line[i] with
+                | '[' -> loop (Packet([]) :: stack) (i + 1)
+                | ']' ->
+                    match stack with
+                    | Packet(current) :: Packet(data) :: stack ->
+                        loop
+                            (Packet(Packet(current |> List.rev) :: data)
+                             :: stack)
+                            (i + 1)
+                    | [ Packet(current) ] ->
+                        loop [ Packet(current |> List.rev) ] (i + 1)
+                    | _ -> failwith "Error on packet close"
+                | ',' -> loop stack (i + 1)
+                | c when c >= '0' && c <= '9' ->
+                    match line.IndexOfAny([| ','; ']' |], i) with
+                    | j when j > i ->
+                        let n = line[i .. j - 1] |> int
+
+                        match stack with
+                        | Packet(data) :: stack ->
+                            loop (Packet(Number(n) :: data) :: stack) j
+                        | _ -> failwith "Error on adding number"
+                    | _ -> failwith "Error on parsing number"
+                | c -> failwith $"Invalid char: %c{c}"
+
+        loop [] 0
+
+    let parsePairs (input: string) =
+        input.Split("\n\n")
+        |> Array.map (
+            String.split '\n'
+            >> Array.map parse
+            >> function
+                | [| d1; d2 |] -> d1, d2
+                | _ -> failwith "Not a data pair"
+        )
+
+    let dump data =
+        let d = sprintf "%A" data
+        d.Replace("Packet ", "").Replace("Number ", "").Replace("; ", ",")
+
+    let rec compare d1 d2 =
+        match d1, d2 with
+        | Number d1, Number d2 ->
+            if d1 < d2 then Valid
+            else if d1 > d2 then Invalid
+            else Undetermined
+        | Number n, Packet _ -> compare (Packet [ Number n ]) d2
+        | Packet _, Number n -> compare d1 (Packet [ Number n ])
+        | Packet d1, Packet d2 -> loop d1 d2
+
+    and loop d1 d2 =
+        match d1, d2 with
+        | a :: d1, b :: d2 ->
+            match compare a b with
+            | Undetermined -> loop d1 d2
+            | result -> result
+        | [], _ :: _ -> Valid
+        | _ :: _, [] -> Invalid
+        | [], [] -> Undetermined
+
+    let sort data =
+        let rec insert p packets result =
+            match packets with
+            | [] -> (p :: result) |> List.rev
+            | p' :: packets ->
+                match compare p p' with
+                | Valid -> (result |> List.rev) @ (p :: p' :: packets)
+                | Invalid -> insert p packets (p' :: result)
+                | Undetermined -> failwith "Compare must not be Undetermined"
+
+        let rec loop result packets =
+            match packets with
+            | [] -> result |> List.toArray
+            | p :: packets -> loop (insert p result []) packets
+
+        loop [ data |> Array.head ] (data |> Array.tail |> Array.toList)
+
+let part1 pairs =
+    pairs
+    |> Array.mapi (fun i (d1, d2) ->
+        match Data.compare d1 d2 with
+        | Valid -> i + 1
+        | _ -> 0)
+    |> Array.sum
+
+let part2 pairs =
+    let p1 = Packet [ Packet [ Number 2 ] ]
+    let p2 = Packet [ Packet [ Number 6 ] ]
+
+    pairs
+    |> Array.append [| p1, p2 |]
+    |> Array.map (fun (d1, d2) -> [| d1; d2 |])
+    |> Array.concat
+    |> Data.sort
+    |> Array.mapi (fun i p ->
+        match p = p1 || p = p2 with
+        | true -> i + 1
+        | false -> 1)
+    |> Array.reduce (*)
+
+let input = readInputText "13"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> Data.parsePairs |> part1 |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> Data.parsePairs |> part2 |> string
