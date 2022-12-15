bcbfd1989afd458965e55cfc83c41dc4fc8dbb56
Arnold Filip <arnold.filip@gmail.com>
2022-12-15 17:10:04 +0100

    F# 2022-15

--- 2022/fsharp/Aoc2022.Tests/Day15.Tests.fs
+++ 2022/fsharp/Aoc2022.Tests/Day15.Tests.fs
@@ -4,10 +4,33 @@ open Xunit
 open Utils
 open Day15
 
+let input =
+    """
+    Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+    Sensor at x=9, y=16: closest beacon is at x=10, y=16
+    Sensor at x=13, y=2: closest beacon is at x=15, y=3
+    Sensor at x=12, y=14: closest beacon is at x=10, y=16
+    Sensor at x=10, y=20: closest beacon is at x=10, y=16
+    Sensor at x=14, y=17: closest beacon is at x=10, y=16
+    Sensor at x=8, y=7: closest beacon is at x=2, y=10
+    Sensor at x=2, y=0: closest beacon is at x=2, y=10
+    Sensor at x=0, y=11: closest beacon is at x=2, y=10
+    Sensor at x=20, y=14: closest beacon is at x=25, y=17
+    Sensor at x=17, y=20: closest beacon is at x=21, y=22
+    Sensor at x=16, y=7: closest beacon is at x=15, y=3
+    Sensor at x=14, y=3: closest beacon is at x=15, y=3
+    Sensor at x=20, y=1: closest beacon is at x=15, y=3
+    """
+    |> String.toLines
+
+[<Fact>]
+let ``Day15 countNonEmptyInRow works`` () =
+    Assert.Equal(26L, input |> Signal.parseMany |> Signal.countNonEmptyInRow 10)
+
 [<Fact>]
 let ``Day15 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("5832528", job1 ())
+        Assert.Equal("13360899249595", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2022/fsharp/Aoc2022/Day15.fs
+++ 2022/fsharp/Aoc2022/Day15.fs
@@ -2,12 +2,106 @@ module Day15
 
 open Utils
 
+type Pos = int * int
+let distance (x, y) (x', y') = abs (x - x') + abs (y - y')
+
+type Signal = { Sensor: Pos; Beacon: Pos }
+
+module Signal =
+    let parse line =
+        let rx =
+            @"^Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$"
+
+        match line with
+        | Regex rx [ Int sx; Int sy; Int bx; Int by ] ->
+            { Sensor = (sx, sy); Beacon = (bx, by) }
+        | _ -> failwithf "Invalid line: %s" line
+
+    let parseMany lines = lines |> Seq.map parse |> Seq.toArray
+
+    let countSenderAndBeaconsInRow y signals =
+        signals
+        |> Array.fold
+            (fun
+                xs
+                { Sensor = (x', y')
+                  Beacon = (x'', y'') } ->
+                xs
+                |> if y = y' then Set.add x' else id
+                |> if y = y'' then Set.add x'' else id)
+            Set.empty
+        |> Set.toList
+
+    let countNonEmptyInRow y signals =
+        let addFromSignal ranges s =
+            let { Sensor = (x', y') } = s
+            let d = distance s.Sensor s.Beacon
+            let dy = abs (y - y')
+
+            match d - dy with
+            | dx when dx >= 0 -> (Range.init (x' - dx) (x' + dx)) :: ranges
+            | _ -> ranges
+
+        let ranges = signals |> Array.fold addFromSignal [] |> Range.combine
+
+        let nonEmptyCount =
+            signals
+            |> countSenderAndBeaconsInRow y
+            |> List.filter (fun x -> ranges |> List.exists (Range.contains x))
+            |> List.length
+            |> int64
+
+        let emptyCount = ranges |> List.map (Range.longLength) |> List.sum
+
+        max 0L (emptyCount - nonEmptyCount)
+
+    let tryFindEmptyInRow y signals =
+        let addFromSignal ranges s =
+            let { Sensor = (x', y') } = s
+            let d = distance s.Sensor s.Beacon
+            let dy = abs (y - y')
+
+            match d - dy with
+            | dx when dx >= 0 -> (Range.init (x' - dx) (x' + dx)) :: ranges
+            | _ -> ranges
+
+        let ranges =
+            signals
+            |> Array.fold addFromSignal []
+            |> Range.combine
+            |> List.map (fun (a, b) ->
+                let a' = max 0 a
+                let b' = min 4000000 b
+                a', b')
+            |> List.sort
+
+        let length = ranges |> List.map (Range.longLength) |> List.sum
+
+        if length = 4000001L then
+            None
+        else
+            let x =
+                match ranges with
+                | [ r1; _ ] -> (r1 |> snd) + 1
+                | [ r ] -> if r |> fst = 0 then 4000000 else 0
+                | _ -> failwithf "Impossible ranges: %A" ranges
+
+            Some x
+
+
+    let findTuningFrequency signals =
+        seq {
+            for y in 0..4000000 do
+                match tryFindEmptyInRow y signals with
+                | Some x -> yield (int64 x * 4000000L + int64 y)
+                | None -> ()
+        }
+        |> Seq.head
+
 let input = readInputLines "15"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> Signal.parseMany |> Signal.countNonEmptyInRow 2000000 |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> Signal.parseMany |> Signal.findTuningFrequency |> string
