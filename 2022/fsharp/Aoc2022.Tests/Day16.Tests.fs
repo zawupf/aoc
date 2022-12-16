fc754c02becc4baf94623a7d9ef0291f3132081d
Arnold Filip <arnold.filip@gmail.com>
2022-12-16 14:41:06 +0100

    F# 2022-16

--- 2022/fsharp/Aoc2022.Tests/Day16.Tests.fs
+++ 2022/fsharp/Aoc2022.Tests/Day16.Tests.fs
@@ -4,10 +4,32 @@ open Xunit
 open Utils
 open Day16
 
+let input =
+    """
+    Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+    Valve BB has flow rate=13; tunnels lead to valves CC, AA
+    Valve CC has flow rate=2; tunnels lead to valves DD, BB
+    Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+    Valve EE has flow rate=3; tunnels lead to valves FF, DD
+    Valve FF has flow rate=0; tunnels lead to valves EE, GG
+    Valve GG has flow rate=0; tunnels lead to valves FF, HH
+    Valve HH has flow rate=22; tunnel leads to valve GG
+    Valve II has flow rate=0; tunnels lead to valves AA, JJ
+    Valve JJ has flow rate=21; tunnel leads to valve II
+    """
+    |> String.toLines
+
+[<Theory>]
+[<InlineData(1651, 1, 30)>]
+[<InlineData(1707, 2, 26)>]
+let ``Day16 maxPressure works`` expected numRunners totalTime =
+    Assert.Equal(expected, input |> maxPressure numRunners totalTime)
+
 [<Fact>]
 let ``Day16 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("2183", job1 ())
+        // Assert.Equal("2911", job2 ()) // FIXME: performance (>1.5h)
+        ()
     with :? System.NotImplementedException ->
         ()
--- 2022/fsharp/Aoc2022/Day16.fs
+++ 2022/fsharp/Aoc2022/Day16.fs
@@ -4,10 +4,192 @@ open Utils
 
 let input = readInputLines "16"
 
-let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+type Valve =
+    { Name: string
+      Rate: int
+      Open: bool
+      Valves: int array }
 
-let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+type Runner = { Current: int; RemainingTime: int }
+
+type Waypoint =
+    { Runners: Runner list
+      Pressure: int
+      Targets: int list }
+
+let parseLine line =
+    let rx =
+        @"^Valve (\w{2}) has flow rate=(\d+); tunnels? leads? to valves? (\w{2}(?:, \w{2})*)$"
+
+    match line with
+    | Regex rx [ name; Int rate; valves ] ->
+        { Name = name
+          Rate = rate
+          Open = false
+          Valves = Array.empty },
+        valves.Split(", ")
+    | _ -> failwith $"Invalid line: %s{line}"
+
+let mapValveIndices (valves: _[], tunnels) =
+    tunnels
+    |> Array.mapi (fun i tunnels ->
+        let valve = valves[i]
+
+        let valves' =
+            tunnels
+            |> Array.map (fun name ->
+                valves |> Array.findIndex (fun valve -> valve.Name = name))
+
+        { valve with Valves = valves' })
+
+let parse lines =
+    lines |> Array.map parseLine |> Array.unzip |> mapValveIndices
+
+let buildGraph valves =
+    valves
+    |> Seq.indexed
+    |> Seq.fold
+        (fun graph (i, { Valves = valves }) ->
+            valves |> Array.fold (fun graph j -> ((i, j), 1) :: graph) graph)
+        List.empty
+    |> Map.ofSeq
+
+let shortestPathLength graph i j =
+    let rec loop result pathes =
+        match pathes with
+        | [] -> result
+        | (i, len, visited) :: pathes ->
+            match i = j with
+            | true ->
+                loop
+                    (result |> Option.map (min len) |> Option.orElse (Some len))
+                    pathes
+            | false ->
+                let pathes' =
+                    graph
+                    |> Map.fold
+                        (fun pathes (i', j') l ->
+                            let len' = len + l
+
+                            if
+                                i = i'
+                                && visited |> Set.contains j' |> not
+                                && result
+                                   |> Option.map (fun r -> len' < r)
+                                   |> Option.defaultValue true
+                            then
+                                (j', len', visited |> Set.add j') :: pathes
+                            else
+                                pathes)
+                        pathes
+
+                loop result pathes'
+
+    loop None [ i, 0, Set.ofList [ i ] ]
+
+let buildDistanceMap (valves: _[]) =
+    let graph = buildGraph valves
+
+    let rec loop result valves =
+        match valves with
+        | [] -> result |> Map.ofList
+        | i :: js ->
+            let paths = shortestPathLength graph i
+
+            let len j =
+                match paths j with
+                | Some len -> len
+                | None -> failwith $"Invalid path: %d{i} -> %d{j}"
+
+            let result' =
+                js
+                |> List.fold
+                    (fun result j ->
+                        let l = len j
+                        ((i, j), l) :: ((j, i), l) :: result)
+                    result
+
+            loop result' js
+
+    loop [] [ 0 .. valves.Length - 1 ]
+
+let maxPressure numRunners totalTime lines =
+    let valves = lines |> parse
+    let distances = valves |> buildDistanceMap
+
+    let start = valves |> Array.findIndex (fun valve -> valve.Name = "AA")
+
+    let targets =
+        [ 0 .. valves.Length - 1 ] |> List.filter (fun i -> valves[i].Rate <> 0)
+
+    let rec loop result ways =
+        match ways with
+        | [] -> result
+        | waypoint :: ways ->
+            let { Runners = allRunners
+                  Pressure = p
+                  Targets = js } =
+                waypoint
+
+            let runners =
+                allRunners
+                |> List.maxBy (fun r -> r.RemainingTime)
+                |> List.singleton
+
+            let result', ways' =
+                js
+                |> List.fold
+                    (fun (r, ways) j ->
+                        runners
+                        |> List.fold
+                            (fun (r, ways) runner ->
+                                let { Current = i; RemainingTime = t } =
+                                    runner
+
+                                let others =
+                                    allRunners
+                                    |> List.removeAt (
+                                        allRunners
+                                        |> List.findIndex (fun r ->
+                                            r = runner)
+                                    )
+
+                                let t' = t - Map.find (i, j) distances - 1
+
+                                if t' <= 0 then
+                                    if others |> List.isEmpty then
+                                        r, ways
+                                    else
+                                        r,
+                                        { waypoint with Runners = others }
+                                        :: ways
+                                else
+                                    let p' = p + t' * valves[j].Rate
+                                    let r' = max p' r
+                                    let js' = js |> List.except [ j ]
+
+                                    r',
+                                    { Runners =
+                                        { Current = j; RemainingTime = t' }
+                                        :: others
+                                      Pressure = p'
+                                      Targets = js' }
+                                    :: ways)
+                            (r, ways))
+                    (result, ways)
+
+            loop result' ways'
+
+    loop
+        0
+        [ { Runners =
+              List.replicate
+                  numRunners
+                  { Current = start
+                    RemainingTime = totalTime }
+            Pressure = 0
+            Targets = targets } ]
+
+let job1 () = input |> maxPressure 1 30 |> string
+
+let job2 () = input |> maxPressure 2 26 |> string
