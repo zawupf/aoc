c5e7ddc9d4aaf37725bd5a0636932a2a92536a54
Arnold Filip <arnold.filip@gmail.com>
2022-12-21 15:54:32 +0100

    F# 2022-21

--- 2022/fsharp/Aoc2022.Tests/Day21.Tests.fs
+++ 2022/fsharp/Aoc2022.Tests/Day21.Tests.fs
@@ -4,10 +4,38 @@ open Xunit
 open Utils
 open Day21
 
+let input =
+    """
+    root: pppw + sjmn
+    dbpl: 5
+    cczh: sllz + lgvd
+    zczc: 2
+    ptdq: humn - dvpt
+    dvpt: 3
+    lfqf: 4
+    humn: 5
+    ljgn: 2
+    sjmn: drzm * dbpl
+    sllz: 4
+    pppw: cczh / lfqf
+    lgvd: ljgn * ptdq
+    drzm: hmdt - zczc
+    hmdt: 32
+    """
+    |> String.toLines
+
+[<Fact>]
+let ``Day21 eval works`` () =
+    Assert.Equal(152L, input |> parse |> eval "root" |> fst)
+
+[<Fact>]
+let ``Day21 guessNumber works`` () =
+    Assert.Equal(301L, input |> parse |> guessNumber)
+
 [<Fact>]
 let ``Day21 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("291425799367130", job1 ())
+        Assert.Equal("3219579395609", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2022/fsharp/Aoc2022/Day21.fs
+++ 2022/fsharp/Aoc2022/Day21.fs
@@ -2,12 +2,169 @@ module Day21
 
 open Utils
 
+type Operand =
+    | Add
+    | Sub
+    | Mul
+    | Div
+    | Match
+
+let (|Op|_|) (str: string) =
+    match str with
+    | "+" -> Some Add
+    | "-" -> Some Sub
+    | "*" -> Some Mul
+    | "/" -> Some Div
+    | "=" -> Some Match
+    | _ -> None
+
+type Expr =
+    | Value of int64
+    | Operation of Operand * string * string
+    | MagicNumber
+
+let parseLine line =
+    match line with
+    | Regex @"^(\w+): (-?\d+)$" [ name; Int64 value ] -> name, Value(value)
+    | Regex @"^(\w+): (\w+) ([*/+-]) (\w+)$" [ name; left; Op op; right ] ->
+        name, Operation(op, left, right)
+    | _ -> failwithf "Invalid line: %s" line
+
+let parse lines = lines |> Seq.map parseLine |> Map.ofSeq
+
+let calc op =
+    match op with
+    | Add -> (+)
+    | Sub -> (-)
+    | Mul -> (*)
+    | Div -> (/)
+    | Match -> (fun left right -> (left - right) |> sign |> int64)
+
+let calcLeft op right result =
+    match op with
+    | Add -> result - right
+    | Sub -> result + right
+    | Mul -> result / right
+    | Div -> result * right
+    | Match -> right
+
+let calcRight op left result =
+    match op with
+    | Add -> result - left
+    | Sub -> left - result
+    | Mul -> result / left
+    | Div -> left / result
+    | Match -> left
+
+let eval name exprMap =
+    let resolveMagicNumbers name value map magicMap =
+        let rec loop name value map magicMap =
+            let expr = magicMap |> Map.find name
+
+            let map = map |> Map.add name (Value value)
+            let magicMap = magicMap |> Map.remove name
+
+            let next =
+                match expr with
+                | Operation(op, left, right) when
+                    magicMap |> Map.containsKey left
+                    ->
+                    match map |> Map.find right with
+                    | Value r -> Some(left, calcLeft op r value)
+                    | _ -> failwith "Logic error 🤯"
+                | Operation(op, left, right) when
+                    magicMap |> Map.containsKey right
+                    ->
+                    match map |> Map.find left with
+                    | Value l -> Some(right, calcRight op l value)
+                    | _ -> failwith "Logic error 🤯"
+                | MagicNumber -> None
+                | _ -> failwith "Logic error 🤯"
+
+            match next with
+            | Some(name, value) -> loop name value map magicMap
+            | None -> map, magicMap
+
+        loop name value map magicMap
+
+    let rec loop names map magicMap =
+        match names with
+        | [] ->
+            match map |> Map.find name with
+            | Value v -> v, map
+            | MagicNumber
+            | Operation _ -> failwithf "Could not resolve value of: %s" name
+        | name :: names ->
+            match map |> Map.find name with
+            | Value _
+            | MagicNumber -> loop names map magicMap
+            | Operation(op, left, right) as operation ->
+                match Map.find left map, Map.find right map with
+                | Value l, Value r ->
+                    loop
+                        names
+                        (map |> Map.add name (Value(calc op l r)))
+                        magicMap
+                | Value _, Operation _ ->
+                    loop (right :: name :: names) map magicMap
+                | Operation _, Value _ ->
+                    loop (left :: name :: names) map magicMap
+                | Operation _, Operation _ ->
+                    loop (left :: right :: name :: names) map magicMap
+                | MagicNumber, Value value when op = Match ->
+                    let map, magicMap =
+                        resolveMagicNumbers left value map magicMap
+
+                    loop
+                        names
+                        (map |> Map.add name (Value(calc op value value)))
+                        magicMap
+                | Value value, MagicNumber when op = Match ->
+                    let map, magicMap =
+                        resolveMagicNumbers right value map magicMap
+
+                    loop
+                        names
+                        (map |> Map.add name (Value(calc op value value)))
+                        magicMap
+                | MagicNumber, Value _
+                | MagicNumber, Operation _ ->
+                    let magicMap = magicMap |> Map.add name operation
+
+                    let map = map |> Map.add name MagicNumber
+                    loop (right :: name :: names) map magicMap
+                | Value _, MagicNumber
+                | Operation _, MagicNumber ->
+                    let magicMap = magicMap |> Map.add name operation
+
+                    let map = map |> Map.add name MagicNumber
+                    loop (right :: name :: names) map magicMap
+                | MagicNumber, MagicNumber -> failwithf "I'm screwed 🤪"
+
+    let magicMap =
+        exprMap
+        |> Map.filter (fun _ expr ->
+            match expr with
+            | MagicNumber -> true
+            | _ -> false)
+
+    loop [ name ] exprMap magicMap
+
+let guessNumber exprMap =
+    let map =
+        match exprMap |> Map.find "root" with
+        | Operation(_, l, r) ->
+            exprMap |> Map.add "root" (Operation(Match, l, r))
+        | _ -> failwith "Invlalid input"
+        |> Map.add "humn" MagicNumber
+
+    match eval "root" map |> snd |> Map.find "humn" with
+    | Value v -> v
+    | _ -> failwith "Eval magic number failed"
+
 let input = readInputLines "21"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> parse |> eval "root" |> fst |> string
 
-let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+let job2 () = input |> parse |> guessNumber |> string
