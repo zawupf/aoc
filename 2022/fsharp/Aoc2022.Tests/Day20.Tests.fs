ec0c1d8f4908f3e2814ae843660eb7510f38299a
Arnold Filip <arnold.filip@gmail.com>
2022-12-22 17:32:04 +0100

    F# 2022-20
    Thanks Annika! 🎉🥳🤗
    I would have never guessed the correct modulo length myself 🤷‍♂️😊

--- 2022/fsharp/Aoc2022.Tests/Day20.Tests.fs
+++ 2022/fsharp/Aoc2022.Tests/Day20.Tests.fs
@@ -4,10 +4,56 @@ open Xunit
 open Utils
 open Day20
 
+let input =
+    """
+    1
+    2
+    -3
+    3
+    -2
+    0
+    4
+    """
+    |> String.toLines
+
+[<Theory>]
+[<InlineData("1, 2, -3, 3, -2, 0, 4", 0, 0<nid>)>]
+[<InlineData("2, 1, -3, 3, -2, 0, 4", 1, 1<nid>)>]
+[<InlineData("1, -3, 2, 3, -2, 0, 4", 2, 0<nid>)>]
+[<InlineData("1, 2, 3, -2, -3, 0, 4", 3, 0<nid>)>]
+[<InlineData("1, 2, -2, -3, 0, 3, 4", 4, 0<nid>)>]
+[<InlineData("1, 2, -3, 0, 3, 4, -2", 5, 0<nid>)>]
+[<InlineData("1, 2, -3, 0, 3, 4, -2", 6, 0<nid>)>]
+[<InlineData("1, 2, -3, 4, 0, 3, -2", 7, 0<nid>)>]
+let ``Day20 mix_n works`` expected n id' =
+    Assert.Equal(expected, input |> parse |> mix_n n |> dump id')
+
+[<Fact>]
+let ``Day20 groveCoordinates works`` () =
+    Assert.Equal<int64 list>(
+        [ 4L; -3L; 2L ],
+        input |> parse |> groveCoordinates 1L 1
+    )
+
+    Assert.Equal<int64 list>(
+        [ 811589153L; 2434767459L; -1623178306L ],
+        input |> parse |> groveCoordinates 811589153L 10
+    )
+
+[<Fact>]
+let ``Day20 groveCoordinatesSum works`` () =
+    Assert.Equal(3L, input |> parse |> groveCoordinatesSum 1L 1)
+
+    Assert.Equal(
+        1623178306L,
+        input |> parse |> groveCoordinatesSum 811589153L 10
+    )
+
 [<Fact>]
 let ``Day20 Stars`` () =
     try
-        Assert.Equal("", job1 ())
-        Assert.Equal("", job2 ())
+        Assert.Equal("4914", job1 ())
+        Assert.Equal("7973051839072", job2 ())
+        ()
     with :? System.NotImplementedException ->
         ()
--- 2022/fsharp/Aoc2022/Day20.fs
+++ 2022/fsharp/Aoc2022/Day20.fs
@@ -2,12 +2,73 @@ module Day20
 
 open Utils
 
+[<Measure>]
+type nid
+
+let parse (lines: string seq) =
+    lines |> Seq.mapi (fun i v -> i * 1<nid>, int64 v) |> Seq.toArray
+
+let indexById nodeId data =
+    data |> Array.findIndex (fun (nodeId', _) -> nodeId = nodeId')
+
+let indexByValue v data =
+    data |> Array.findIndex (fun (_, value) -> v = value)
+
+let valueAt i data = data |> Array.item (i % data.Length)
+
+let dump nodeId data =
+    let i = data |> indexById nodeId
+    let left, right = data |> Array.splitAt i
+    let data = Array.concat [ right; left ]
+    data |> Array.map (snd >> string) |> String.concat ", "
+
+let mix_n n data =
+    let len = (data |> Array.length |> int64) - 1L
+
+    let move data nodeId =
+        let i = data |> indexById nodeId
+        let node = data |> Array.item i
+        let v = node |> snd
+
+        // Thanks Annika! 🎉🥳🤗
+        // I would have never guessed the correct modulo length myself 🤷‍♂️😊
+        let j =
+            match ((int64 i) + v) % len |> int with
+            | j when j >= 0 -> j
+            | j -> (int len) + j
+
+        // data |> Array.removeAt i |> Array.insertAt j node
+        if i < j then
+            Array.blit data (i + 1) data i (j - i + 1)
+            data[j] <- node
+        else if i > j then
+            Array.blit data j data (j + 1) (i - j)
+            data[j] <- node
+
+        data
+
+
+    [ 0 .. n - 1 ] |> Seq.map ((*) 1<nid>) |> Seq.fold move data
+
+let mix data = mix_n (Array.length data) data
+
+let groveCoordinates key n data =
+    let data = data |> Array.map (fun (nodeId, value) -> nodeId, value * key)
+    let data = [ 1..n ] |> List.fold (fun data _ -> mix data) data
+    let i = data |> indexByValue 0L
+
+    [ data |> valueAt (i + 1000)
+      data |> valueAt (i + 2000)
+      data |> valueAt (i + 3000) ]
+    |> List.map snd
+
+let groveCoordinatesSum key n data =
+    data |> groveCoordinates key n |> List.sum
+
 let input = readInputLines "20"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> parse |> groveCoordinatesSum 1L 1 |> string
 
 let job2 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> parse |> groveCoordinatesSum 811589153L 10 |> string
