34ee090df88147115a3c2aa7f40952d2f42bf39b
Arnold Filip <arnold.filip@gmail.com>
2022-12-27 09:40:35 +0100

    F# 2022-25

--- 2022/fsharp/Aoc2022.Tests/Day25.Tests.fs
+++ 2022/fsharp/Aoc2022.Tests/Day25.Tests.fs
@@ -4,10 +4,48 @@ open Xunit
 open Utils
 open Day25
 
+[<Theory>]
+[<InlineData(1, "1")>]
+[<InlineData(2, "2")>]
+[<InlineData(3, "1=")>]
+[<InlineData(4, "1-")>]
+[<InlineData(5, "10")>]
+[<InlineData(6, "11")>]
+[<InlineData(7, "12")>]
+[<InlineData(8, "2=")>]
+[<InlineData(9, "2-")>]
+[<InlineData(10, "20")>]
+[<InlineData(15, "1=0")>]
+[<InlineData(20, "1-0")>]
+[<InlineData(2022, "1=11-2")>]
+[<InlineData(12345, "1-0---0")>]
+[<InlineData(314159265, "1121-1110-1=0")>]
+let ``Day25 Snafu.toInt works`` expected snafu =
+    Assert.Equal(expected, snafu |> Snafu.toInt)
+
+[<Theory>]
+[<InlineData(1, "1")>]
+[<InlineData(2, "2")>]
+[<InlineData(3, "1=")>]
+[<InlineData(4, "1-")>]
+[<InlineData(5, "10")>]
+[<InlineData(6, "11")>]
+[<InlineData(7, "12")>]
+[<InlineData(8, "2=")>]
+[<InlineData(9, "2-")>]
+[<InlineData(10, "20")>]
+[<InlineData(15, "1=0")>]
+[<InlineData(20, "1-0")>]
+[<InlineData(2022, "1=11-2")>]
+[<InlineData(12345, "1-0---0")>]
+[<InlineData(314159265, "1121-1110-1=0")>]
+let ``Day25 Snafu.ofInt works`` number expected =
+    Assert.Equal(expected, number |> Snafu.ofInt)
+
 [<Fact>]
 let ``Day25 Stars`` () =
     try
-        Assert.Equal("", job1 ())
+        Assert.Equal("2=1-=02-21===-21=200", job1 ())
         Assert.Equal("", job2 ())
     with :? System.NotImplementedException ->
         ()
--- 2022/fsharp/Aoc2022/Day25.fs
+++ 2022/fsharp/Aoc2022/Day25.fs
@@ -2,11 +2,45 @@ module Day25
 
 open Utils
 
+type Snafu = string
+
+module Snafu =
+    let toInt input =
+        Seq.foldBack
+            (fun c (sum, prod) ->
+                let n =
+                    match c with
+                    | '0' -> 0L
+                    | '1' -> 1L
+                    | '2' -> 2L
+                    | '-' -> -1L
+                    | '=' -> -2L
+                    | _ -> failwith "Invalid input"
+
+                sum + prod * n, prod * 5L)
+            input
+            (0L, 1L)
+        |> fst
+
+    let ofInt n =
+        n
+        |> Seq.unfold (fun n ->
+            match n / 5L, n % 5L with
+            | 0L, 0L -> None
+            | n, d when d < 3L -> Some(char (int64 '0' + d), n)
+            | n, 3L -> Some('=', n + 1L)
+            | n, 4L -> Some('-', n + 1L)
+            | _ -> failwith "Internal error")
+        |> Seq.rev
+        |> String.ofChars
+        |> function
+            | "" -> "0"
+            | s -> s
+
 let input = readInputLines "25"
 
 let job1 () =
-    // input |> String.join "" |> string
-    raise (System.NotImplementedException())
+    input |> Seq.sumBy Snafu.toInt |> Snafu.ofInt
 
 let job2 () =
     // input |> String.join "" |> string
