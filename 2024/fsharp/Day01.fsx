39fd19805e7e66bbe8c6844a62bb199978dba6e5
Arnold Filip <arnold.filip@gmail.com>
2024-12-01 16:45:09 +0100

    F# 2024-01

--- 2024/fsharp/Day01.fsx
+++ 2024/fsharp/Day01.fsx
@@ -1,11 +1,36 @@
 #load "Utils.fsx"
-open System.Text.RegularExpressions
+open Utils.FancyPatterns
 
-let input = Utils.readInputLines "01"
+let day = __SOURCE_FILE__[3..4]
 
-let getDay01_1 () = input |> Utils.notImplemented ()
+let input = Utils.readInputLines day
 
-let getDay01_2 () = input |> Utils.notImplemented ()
+let parseInput (lines: string array) =
+    lines
+    |> Array.map (fun line ->
+        match line with
+        | Regex @"(\d+)\s+(\d+)" [ Int l; Int r ] -> l, r
+        | _ -> failwithf "Invalid line: %A" line)
+    |> Array.unzip
 
-Utils.Test.run "Part 1" nan getDay01_1
-Utils.Test.run "Part 2" nan getDay01_2
+let part1 input =
+    let left, right = input |> parseInput
+    Array.sortInPlace left
+    Array.sortInPlace right
+
+    Array.zip left right |> Array.sumBy (fun (l, r) -> abs (l - r))
+
+let part2 input =
+    let left, right = input |> parseInput
+    let left = Array.countBy id left
+    let right = Array.countBy id right |> Map.ofArray
+
+    left |> Array.sumBy (fun (k, v) ->
+        v * k * (right |> Map.tryFind k |> Option.defaultValue 0))
+
+let solution1 () = part1 input
+
+let solution2 () = part2 input
+
+Utils.Test.run "Part 1" 1651298 solution1
+Utils.Test.run "Part 2" 21306195 solution2
