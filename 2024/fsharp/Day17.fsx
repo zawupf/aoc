31efa0fd72a398dfc308810aed6dabcd123a9bd3
Arnold Filip <arnold.filip@gmail.com>
2024-12-18 12:24:00 +0100

    F# 2024-17 (placeholder)

---
+++ 2024/fsharp/Day17.fsx
@@ -0,0 +1,14 @@
+#load "Utils.fsx"
+open Utils.FancyPatterns
+
+let part1 input = input |> Utils.notImplemented ()
+
+let part2 input = input |> Utils.notImplemented ()
+
+let day = __SOURCE_FILE__[3..4]
+let input = Utils.readInputLines day
+let solution1 () = part1 input
+let solution2 () = part2 input
+
+Utils.Test.run "Part 1" nan solution1
+Utils.Test.run "Part 2" nan solution2
