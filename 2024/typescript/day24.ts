e0e4733825bda3e3d3bd9586e8a65aeb14ef3f8b
Arnold Filip <arnold.filip@gmail.com>
2024-12-29 09:46:02 +0100

    TS 2024-24 (part 2)
    See: https://www.reddit.com/r/adventofcode/comments/1hla5ql/2024_day_24_part_2_a_guide_on_the_idea_behind_the/

--- 2024/typescript/day24.ts
+++ 2024/typescript/day24.ts
@@ -1,7 +1,8 @@
 import { type DayModule, type SolutionFactory } from './types'
 import * as utils from './utils'
 
-type Wires = Map<string, number | null>
+type Wires = Map<string, number>
+type Gates = Map<string, Gate>
 type Operation = 'AND' | 'OR' | 'XOR'
 type Gate = {
     op: Operation
@@ -11,76 +12,210 @@ type Gate = {
 
 type Circuit = {
     wires: Wires
-    gates: Gate[]
+    gates: Gates
+    bits: number
 }
 
 function parse(input: string): Circuit {
     const [wiresChunk, gatesChunk] = utils
         .split_sections(input)
         .map(utils.split_lines)
+
     const wires = new Map(
         wiresChunk.map(line => {
             const [name, value] = line.split(': ')
-            return [name, parseInt(value)]
+            return [name, utils.parseInt(value)]
         }),
     )
-    const gates = gatesChunk.map(line => {
-        const [in1, op, in2, _, output] = line.split(' ')
-        ;[in1, in2, output].forEach(wire => {
-            if (!wires.has(wire)) {
-                wires.set(wire, null as unknown as number)
-            }
-        })
-        return { op, inputs: [in1, in2], output } as Gate
-    })
-    return { wires, gates }
+
+    const gates = new Map(
+        gatesChunk.map(line => {
+            const [in1, op, in2, _, output] = line.split(' ')
+            return [output, { op, inputs: [in1, in2], output } as Gate]
+        }),
+    )
+
+    const bits = gates
+        .keys()
+        .filter(wire => /^z\d\d$/.test(wire))
+        .reduce((count, _) => count + 1, 0)
+
+    return { wires, gates, bits }
+}
+
+const calculate = {
+    AND: (a: number, b: number) => a & b,
+    OR: (a: number, b: number) => a | b,
+    XOR: (a: number, b: number) => a ^ b,
 }
 
 function evaluate(wire: string, circuit: Circuit): number {
     const { wires, gates } = circuit
     const value = wires.get(wire)
-    if (value != null) {
+    if (value !== undefined) {
         return value
     }
 
-    const gate = gates.find(gate => gate.output === wire)!
+    const gate = gates.get(wire)!
     const [value1, value2] = gate.inputs.map(w => evaluate(w, circuit))
-    const result = {
-        AND: value1 & value2,
-        OR: value1 | value2,
-        XOR: value1 ^ value2,
-    }[gate.op]
+
+    const result = calculate[gate.op](value1, value2)
     wires.set(wire, result)
     return result
 }
 
-export const part1: Part = input => () => {
-    const circuit = parse(input)
-    const zs = circuit.wires
-        .keys()
-        .filter(wire => wire.startsWith('z'))
-        .toArray()
-        .sort()
-    const result = zs
+type Prefix = 'x' | 'y' | 'z'
+
+function bitsOf(name: Prefix, circuit: Circuit): number {
+    return name === 'z' ? circuit.bits : circuit.bits - 1
+}
+
+function wiresOf(name: Prefix, circuit: Circuit): string[] {
+    return Array.from({ length: bitsOf(name, circuit) }).map(
+        (_, i) => name + i.toString().padStart(2, '0'),
+    )
+}
+
+function get(name: Prefix, circuit: Circuit): number | null {
+    return wiresOf(name, circuit)
         .map(wire => evaluate(wire, circuit))
         .reduce((sum, bit, i) => sum + bit * 2 ** i, 0)
+}
+
+function swapOutput(gate1: Gate, gate2: Gate, circuit: Circuit) {
+    ;[gate1.output, gate2.output] = [gate2.output, gate1.output]
+    circuit.gates.set(gate1.output, gate1)
+    circuit.gates.set(gate2.output, gate2)
+}
+
+function isInvalidGate(gate: Gate, circuit: Circuit): boolean {
+    const isZOutput = gate.output.startsWith('z')
+    const isLastZBit =
+        isZOutput && parseInt(gate.output.slice(1), 10) === circuit.bits - 1
+    const isXYInput = gate.inputs.every(
+        wire => wire.startsWith('x') || wire.startsWith('y'),
+    )
+
+    return (
+        (isZOutput && !isLastZBit && gate.op !== 'XOR') ||
+        (!isZOutput && !isXYInput && gate.op === 'XOR')
+    )
+}
+
+function resolveOutputs(gate: Gate, circuit: Circuit): Gate[] {
+    if (gate.output.startsWith('z')) {
+        return [gate]
+    }
+
+    const nextGates = circuit.gates
+        .values()
+        .filter(g => g.inputs.includes(gate.output))
+        .toArray()
+    return nextGates.flatMap(g => resolveOutputs(g, circuit))
+}
+
+export const part1: Part = input => () => {
+    const circuit = parse(input)
+    const result = get('z', circuit)!
     return result.toString()
 }
 
 export const part2: Part = input => () => {
     const circuit = parse(input)
-    const bits =
-        circuit.wires
-            .keys()
-            .filter(wire => wire.startsWith('z'))
-            .toArray().length - 1
-    return 'WIP'
+    const initialWires = new Map(circuit.wires)
+
+    const invalidGates = circuit.gates
+        .values()
+        .filter(g => isInvalidGate(g, circuit))
+        .toArray()
+    const invalidOutputGates = invalidGates.filter(gate =>
+        gate.output.startsWith('z'),
+    )
+    const invalidInnerGates = invalidGates.filter(
+        gate => !gate.output.startsWith('z'),
+    )
+    const resolvedGates = invalidGates
+        .filter(gate => !gate.output.startsWith('z'))
+        .map(g => resolveOutputs(g, circuit))
+
+    const mapToInvalidOutputs = (
+        gates: Gate[],
+        i: number,
+    ): [number, Gate[]] => {
+        const gatesOutputId = gates.map(gate => parseInt(gate.output.slice(1)))
+        return [
+            i,
+            invalidOutputGates.filter(gate =>
+                gatesOutputId.includes(parseInt(gate.output.slice(1)) + 1),
+            ),
+        ]
+    }
+
+    const gatesMapping = resolvedGates
+        .map(mapToInvalidOutputs)
+        .sort(([_i, a], [_j, b]) => a.length - b.length)
+        .reduce(
+            (
+                [map, used]: [[Gate, Gate][], Set<string>],
+                [i, gates]: [number, Gate[]],
+            ) => {
+                const outputs = new Set(
+                    gates.map(gate => gate.output),
+                ).difference(used)
+                console.assert(
+                    outputs.size === 1,
+                    `Invalid outputs: ${outputs}`,
+                )
+                const output = outputs.values().next().value!
+                used.add(output)
+                map.push([invalidInnerGates[i], circuit.gates.get(output)!])
+                return [map, used]
+            },
+            [[], new Set()] as [[Gate, Gate][], Set<string>],
+        )[0]
+
+    gatesMapping.forEach(([gate1, gate2]) => swapOutput(gate1, gate2, circuit))
+
+    const xor = (a: number, b: number) =>
+        parseInt(
+            utils
+                .zip(
+                    a.toString(2).padStart(circuit.bits, '0').split(''),
+                    b.toString(2).padStart(circuit.bits, '0').split(''),
+                )
+                .map(([x, y]) => (x === y ? '0' : '1'))
+                .join(''),
+            2,
+        )
+
+    const z = get('z', circuit)!
+    const expected = get('x', circuit)! + get('y', circuit)!
+    const okBits = xor(z, expected).toString(2).replace(/1/g, '').length
+    const xFail = 'x' + okBits.toString().padStart(2, '0')
+    const yFail = 'y' + okBits.toString().padStart(2, '0')
+    const gatesFail = circuit.gates
+        .values()
+        .filter(g => g.inputs.includes(xFail) && g.inputs.includes(yFail))
+        .toArray()
+    console.assert(gatesFail.length === 2, `Invalid gates: ${gatesFail}`)
+
+    swapOutput(gatesFail[0], gatesFail[1], circuit)
+    circuit.wires = new Map(initialWires)
+    const z2 = get('z', circuit)!
+    console.assert(z2 === expected, `Invalid output: ${z2} !== ${expected}`)
+
+    const result = [gatesFail, ...gatesMapping]
+        .flatMap(gs => gs.map(g => g.output))
+        .sort()
+        .join(',')
+
+    return result
 }
 
 export const day = import.meta.file.match(/day(\d+)/)![1]
 export const input = await utils.readInputText(day)
 part1.solution = '49430469426918'
-part2.solution = 'TODO'
+part2.solution = 'fbq,pbv,qff,qnw,qqp,z16,z23,z36'
 
 export const main = import.meta.main
 if (main) {
