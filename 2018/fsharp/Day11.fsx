5cd1d3e82733e4be67e08184af44399bab039435
Arnold Filip <arnold.filip@gmail.com>
2025-01-20 21:29:12 +0100

    F# 2018-11 (much faster)
    at the cost of readability

--- 2018/fsharp/Day11.fsx
+++ 2018/fsharp/Day11.fsx
@@ -1,44 +1,74 @@
 #load "Utils.fsx"
 open Utils
 
+let SIZE = 300
+
 let initGrid serialNumber =
-    Array2D.initBased 1 1 300 300 (fun x y ->
+    Array2D.initBased 1 1 SIZE SIZE (fun x y ->
         let rackId = x + 10
         (rackId * y + serialNumber) * rackId / 100 % 10 - 5)
 
-let part1 input =
-    let grid = input |> int |> initGrid
+let maxTotalPower (grid: int array2d) size =
+    let sumOf_x_y1_y2 x y1 y2 =
+        useCache<int * int * int, int> () (x, y1, y2) (fun _ ->
+            Array.sum grid[x, y1..y2])
 
-    [
-        for x = 1 to 298 do
-            for y = 1 to 298 do
-                yield x, y
-    ]
-    |> List.maxBy (fun (x, y) ->
-        [
-            for x = x to x + 2 do
-                for y = y to y + 2 do
-                    grid[x, y]
-        ]
-        |> List.sum)
+    let power11 = [| 1..size |] |> Array.sumBy (fun x -> sumOf_x_y1_y2 x 1 size)
+
+    let row1 =
+        Array.unfold
+            (fun (power, x) ->
+                if x = 0 then
+                    None
+                elif x + size > SIZE then
+                    Some((power, (x, 1, size)), (0, 0))
+                else
+                    let power' =
+                        power - sumOf_x_y1_y2 x 1 size
+                        + sumOf_x_y1_y2 (x + size) 1 size
+
+                    Some((power, (x, 1, size)), (power', x + 1)))
+            (power11, 1)
+
+    let rowDeltas y =
+        Array.unfold
+            (fun (d, x) ->
+                if x = 0 then None
+                elif x + size > SIZE then Some(d, (0, 0))
+                else Some(d, (d - grid[x, y] + grid[x + size, y], x + 1)))
+            (Array.sum grid[1..size, y], 1)
+
+    Array.unfold
+        (fun (row, y) ->
+            if y = 0 then
+                None
+            elif y + size > SIZE then
+                Some(Array.maxBy fst row, (Array.empty, 0))
+            else
+                let dSub = rowDeltas y
+                let dAdd = rowDeltas (y + size)
+
+                let row' =
+                    Array.zip3 row dSub dAdd
+                    |> Array.map (fun ((power, (x, y, s)), dSub, dAdd) ->
+                        power - dSub + dAdd, (x, y + 1, s))
+
+                Some(Array.maxBy fst row, (row', y + 1)))
+        (row1, 1)
+    |> Array.maxBy fst
+
+let part1 input =
+    maxTotalPower (input |> int |> initGrid) 3
+    |> (fun (_, (x, y, _)) -> x, y)
     ||> sprintf "%d,%d"
 
 let part2 input =
     let grid = input |> int |> initGrid
 
-    [
-        for s = 1 to 300 do
-            for x = 1 to 301 - s do
-                for y = 1 to 301 - s do
-                    yield x, y, s
-    ]
-    |> List.maxBy (fun (x, y, s) ->
-        [
-            for x = x to x + s - 1 do
-                for y = y to y + s - 1 do
-                    grid[x, y]
-        ]
-        |> List.sum)
+    [ 1..SIZE ]
+    |> List.map (maxTotalPower grid)
+    |> List.maxBy fst
+    |> snd
     |> fun (x, y, s) -> sprintf "%d,%d,%d" x y s
 
 let day = __SOURCE_FILE__[3..4]
